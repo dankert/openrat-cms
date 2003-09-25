9277714e8e5d06d0e4bcaebf04200648a101a9e3
dankert <devnull@localhost>
2003-09-25 21:15:02 +0200

    *** empty log message ***

---
+++ themes/default/pages/html/copyright.tpl.php
@@ -0,0 +1,8 @@
+<!-- $Id$ -->
+<!-- The GPL licence requires this text, so NEVER remove nor change it. -->
+
+<br><br>
+<p class="copyright">
+Copyright (C) <a href="mailto:cms-author@jandankert.de">Jan Dankert</a><br>
+This program comes with ABSOLUTELY NO WARRANTY.<br>
+This is free software, and you are welcome to redistribute it under certain conditions; <a href="http://www.gnu.org/copyleft/gpl.html" target="_top">details</a>.</p>
