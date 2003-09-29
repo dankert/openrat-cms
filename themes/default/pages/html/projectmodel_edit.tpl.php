50215e7e59ba9d29cb4558bec7dfcfe866dc36d2
dankert <devnull@localhost>
2003-09-29 20:38:22 +0200

    neuer Dateiname

---
+++ themes/default/pages/html/projectmodel_edit.tpl.php
@@ -0,0 +1,22 @@
+<?php include( $tpl_dir.'header.tpl.php') ?>
+
+<br><br><br>
+<center>
+
+<table width="80%">
+
+<tr>
+  <th colspan="2"><?php echo lang('VARIANT') ?></a></th>
+</tr>
+
+<tr>
+<td width="50%" class="f1"><?php echo lang('NAME') ?></a></td>
+<td width="50%" class="f1"><?php echo $name ?></td>
+</tr>
+
+</table>
+
+<br>
+
+</center>
+<?php include( $tpl_dir.'footer.tpl.php') ?>
