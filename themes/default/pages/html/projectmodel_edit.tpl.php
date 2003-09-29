e9f476571aa927e7627c19d0b5cc71aec672c92c
dankert <devnull@localhost>
2003-09-29 20:58:32 +0200

    *** empty log message ***

--- themes/default/pages/html/projectmodel_edit.tpl.php
+++ themes/default/pages/html/projectmodel_edit.tpl.php
@@ -1,9 +1,11 @@
 <?php include( $tpl_dir.'header.tpl.php') ?>
 
-<br><br><br>
+<!-- $Id$ -->
 <center>
 
-<table width="80%">
+<form action="<?php echo $form_action ?>" method="post" target="_self">
+
+<table class="main" width="90%" cellspacing="0" cellpadding="4">
 
 <tr>
   <th colspan="2"><?php echo lang('VARIANT') ?></a></th>
@@ -13,10 +15,18 @@
 <td width="50%" class="f1"><?php echo lang('NAME') ?></a></td>
 <td width="50%" class="f1"><?php echo $name ?></td>
 </tr>
+<tr>
+<td width="50%" class="f2"><?php echo lang('DELETE') ?></a></td>
+<td width="50%" class="f2"><input type="checkbox" value="1"></td>
+</tr>
+<tr>
+<td colspan="2" class="act"><input type="submit" class="submit" value="<?php echo lang('SAVE') ?>"></a></td>
+</tr>
 
 </table>
 
-<br>
+</form>
 
 </center>
+
 <?php include( $tpl_dir.'footer.tpl.php') ?>
