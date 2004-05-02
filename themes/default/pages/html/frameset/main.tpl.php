24c768614780df7afda7fcf5f31e30a9980f66e0
dankert <devnull@localhost>
2004-05-02 20:55:18 +0200

    Hoehe 75

--- themes/default/pages/html/frameset/main.tpl.php
+++ themes/default/pages/html/frameset/main.tpl.php
@@ -4,8 +4,8 @@
 <head>
 <title></title>
 </head>
-     <frameset rows="70,*" border="0" frameborder="0" framespacing="0">
-          <frame src="<?php echo sid($frame_src_main_menu) ?>" name="cms_main_menu" marginheight="0" marginwidth="0" scrolling="no">
-          <frame src="<?php echo sid($frame_src_main_main) ?>" name="cms_main_main" marginheight="0" marginwidth="0" scrolling="auto">
+     <frameset rows="75,*" border="0" frameborder="0" framespacing="0">
+          <frame src="<?php echo $frame_src_main_menu ?>" name="cms_main_menu" marginheight="0" marginwidth="0" scrolling="no">
+          <frame src="<?php echo $frame_src_main_main ?>" name="cms_main_main" marginheight="0" marginwidth="0" scrolling="auto">
      </frameset>
 </html>
