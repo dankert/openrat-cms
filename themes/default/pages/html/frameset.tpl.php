ad222347cce6ba36d082a2303a3bcd5808b4fcef
dankert <devnull@localhost>
2004-05-02 20:55:41 +0200

    Ausbau sid()

--- themes/default/pages/html/frameset.tpl.php
+++ themes/default/pages/html/frameset.tpl.php
@@ -3,18 +3,18 @@
 <!-- $Id$ -->
 <head>
 <title><?php echo $title ?></title>
-<link rel="shortcut icon" href="<?php echo $favicon_url ?>">
+<link rel="shortcut icon" href="<?php echo $image_dir.'favicon.ico' ?>">
 </head>
 
 <frameset rows="20,*" border="0" frameborder="5" framespacing="0" bordercolor="#000000">
-     <frame src="<?php echo sid($frame_src_title) ?>" name="cms_title" marginheight="0" marginwidth="0" scrolling="no">
+     <frame src="<?php echo $frame_src_title ?>" name="cms_title" marginheight="0" marginwidth="0" scrolling="no">
      <frameset cols="<?php echo $tree_width ?>,*" border="0" frameborder="0" framespacing="0" bordercolor="grey">
-          <frameset rows="70,*"  border="0" frameborder="0" framespacing="0" bordercolor="#000000">
-               <frame src="<?php echo sid($frame_src_treemenu) ?>" name="cms_treemenu" marginheight="0" marginwidth="0" scrolling="no">
+          <frameset rows="75,*"  border="0" frameborder="0" framespacing="0" bordercolor="#000000">
+               <frame src="<?php echo $frame_src_treemenu ?>" name="cms_treemenu" marginheight="0" marginwidth="0" scrolling="no">
 
-               <frame src="<?php echo sid($frame_src_tree) ?>" name="cms_tree" marginheight="0" marginwidth="0" scrolling="auto">
+               <frame src="<?php echo $frame_src_tree ?>" name="cms_tree" marginheight="0" marginwidth="0" scrolling="auto">
           </frameset>
-          <frame src="<?php echo sid($frame_src_main) ?>" name="cms_main" marginheight="0" marginwidth="0" frameborder="0" scrolling="no">
+          <frame src="<?php echo $frame_src_main ?>" name="cms_main" marginheight="0" marginwidth="0" frameborder="0" scrolling="no">
      </frameset>
 </frameset>
 
