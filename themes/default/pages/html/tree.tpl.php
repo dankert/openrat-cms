63a3b1d6b41131a8aa05b0f10dbf584aa2be460b
dankert <devnull@localhost>
2004-05-08 19:59:36 +0200

    alt="" bei img-Tags nachgetragen

--- themes/default/pages/html/tree.tpl.php
+++ themes/default/pages/html/tree.tpl.php
@@ -10,16 +10,16 @@
 
 <?php if   (is_array($z['cols'])) foreach( $z['cols'] as $i )
       { ?>
-<td width="1%"><img src="<?php echo $image_dir.'tree_'.$i ?>.gif" border="0"></td>
+<td width="1%"><img src="<?php echo $image_dir.'tree_'.$i ?>.gif" border="0" alt=""></td>
 <?php } ?>
 
 <td width="1%">
-<?php if (isset($z['image_url'])) { ?><a href="<?php echo $z['image_url'].'#'.$z['name'] ?>" class="tree" target="_self"><?php } ?><img src="<?php echo $image_dir.'tree_'.$z['image'] ?>.gif" border=""><?php if (isset($z['image_url'])) { ?></a><?php } ?>
+<?php if (isset($z['image_url'])) { ?><a href="<?php echo $z['image_url'].'#'.$z['name'] ?>" class="tree" target="_self"><?php } ?><img src="<?php echo $image_dir.'tree_'.$z['image'] ?>.gif" alt="" border="0"><?php if (isset($z['image_url'])) { ?></a><?php } ?>
 </td>
 
 <td colspan="20" id="<?php echo $z['name'] ?>" valign="middle" style="white-space:nowrap;">
 <a name="<?php echo $z['name'] ?>"></a>
-<?php if (isset($z['url'])) { ?><a href="<?php echo $z['url'] ?>" <?php if($z['desc']!='') echo 'title="'.$z['desc'].'" ' ?>class="tree" target="<?php echo $z['target'] ?>"><?php } ?><img src="<?php echo $image_dir.'icon_'.$z['icon'] ?>.png" border="0" align="left"><?php echo $z['text'] ?><?php if (isset($z['url'])) { ?></a><?php if(isset($z['add'])) echo '&nbsp;<small>'.$z['add'].'<small>' ?><?php } ?>
+<?php if (isset($z['url'])) { ?><a href="<?php echo $z['url'] ?>" <?php if($z['desc']!='') echo 'title="'.$z['desc'].'" ' ?>class="tree" target="<?php echo $z['target'] ?>"><?php } ?><img src="<?php echo $image_dir.'icon_'.$z['icon'] ?>.png" border="0" alt="" align="left"><?php echo $z['text'] ?><?php if (isset($z['url'])) { ?></a><?php if(isset($z['add'])) echo '&nbsp;<small>'.$z['add'].'<small>' ?><?php } ?>
 </td>
 
 </tr>
