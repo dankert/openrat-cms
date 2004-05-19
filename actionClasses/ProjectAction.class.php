bacdb78e847f15a6cc26cefda39017cc5ec60d50
dankert <devnull@localhost>
2004-05-19 23:12:49 +0200

    Korrektur listing()

--- actionClasses/ProjectAction.class.php
+++ actionClasses/ProjectAction.class.php
@@ -20,7 +20,10 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ---------------------------------------------------------------------------
 // $Log$
-// Revision 1.2  2004-05-02 14:49:37  dankert
+// Revision 1.3  2004-05-19 21:12:49  dankert
+// Korrektur listing()
+//
+// Revision 1.2  2004/05/02 14:49:37  dankert
 // Einfügen package-name (@package)
 //
 // Revision 1.1  2004/04/24 15:14:52  dankert
@@ -91,7 +94,7 @@ class ProjectAction extends Action
 		global $conf_php;
 
 		// Projekte ermitteln
-		$listl = array();
+		$list = array();
 
 		foreach( $this->project->getAll() as $id=>$name )
 		{
