30ebc8016351374bc99992ad314597096cf7cdf8
dankert <devnull@localhost>
2004-05-19 21:56:06 +0200

    subaction statt useraction

--- themes/default/pages/html/user/pw.tpl.php
+++ themes/default/pages/html/user/pw.tpl.php
@@ -5,8 +5,9 @@
 <center>
 
 <form action="<?php echo $self ?>" method="post" target="_self">
-<input type="hidden" name="<?php echo session_name() ?>" value="<?php echo session_id() ?>">
-<input type="hidden" name="useraction" value="pwchange">
+
+<input type="hidden" name="action"    value="user"    />
+<input type="hidden" name="subaction" value="pwchange"/>
 
 <table class="main" width="90%" cellspacing="0" cellpadding="4">
 <tr>
