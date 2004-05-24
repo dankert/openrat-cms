0e85928acbbec0818f58678f53e9cf054910fcf9
dankert <devnull@localhost>
2004-05-24 20:17:20 +0200

    meta-Tags geschlossen und eingerueckt

--- themes/default/pages/html/header.tpl.php
+++ themes/default/pages/html/header.tpl.php
@@ -2,17 +2,17 @@
 <html>
 <!-- $Id$ -->
 <head>
-<title><?php echo $cms_title ?></title>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
-<meta name="MSSmartTagsPreventParsing" content="TRUE">
-<meta name="robots" content="noindex,nofollow">
-<meta http-equiv="expires" content="0">
-<meta http-equiv="cache-control" content="no-cache">
-<meta http-equiv="pragma" content="no-cache">
-<link rel="stylesheet" type="text/css" href="<?php echo $stylesheet ?>">
+  <title><?php echo $cms_title ?></title>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+  <meta name="MSSmartTagsPreventParsing" content="true" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta http-equiv="expires" content="0" />
+  <meta http-equiv="cache-control" content="no-cache" />
+  <meta http-equiv="pragma" content="no-cache" />
+  <link rel="stylesheet" type="text/css" href="<?php echo $stylesheet ?>" />
 <?php if (isset($request_timeout))
       { ?>
-<meta http-equiv="refresh" content="<?php echo $refresh_timeout ?>;url=<?php echo $refresh_url ?>">
+  <meta http-equiv="refresh" content="<?php echo $refresh_timeout ?>;url=<?php echo $refresh_url ?>">
 <?php } ?>
 
 <?php if (isset($id))
