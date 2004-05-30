968e5f1d69846383e96e205a986dd4dac6178643
dankert <devnull@localhost>
2004-05-31 01:09:54 +0200

    Korrektur "colspan"-Angabe

--- themes/default/pages/html/tree.tpl.php
+++ themes/default/pages/html/tree.tpl.php
@@ -17,7 +17,7 @@
 <?php if (isset($z['image_url'])) { ?><a href="<?php echo $z['image_url'].'#'.$z['name'] ?>" class="tree" target="_self"><?php } ?><img src="<?php echo $image_dir.'tree_'.$z['image'] ?>.gif" alt="" border="0"><?php if (isset($z['image_url'])) { ?></a><?php } ?>
 </td>
 
-<td colspan="20" id="<?php echo $z['name'] ?>" valign="middle" style="white-space:nowrap;">
+<td colspan="<?php echo intval(20-count($z['cols'])) ?>" id="<?php echo $z['name'] ?>" valign="middle" style="white-space:nowrap;">
 <a name="<?php echo $z['name'] ?>"></a>
 <?php if (isset($z['url'])) { ?><a href="<?php echo $z['url'] ?>" <?php if($z['desc']!='') echo 'title="'.$z['desc'].'" ' ?>class="tree" target="<?php echo $z['target'] ?>"><?php } ?><img src="<?php echo $image_dir.'icon_'.$z['icon'] ?>.png" border="0" alt="" align="left"><?php echo $z['text'] ?><?php if (isset($z['url'])) { ?></a><?php if(isset($z['add'])) echo '&nbsp;<small>'.$z['add'].'<small>' ?><?php } ?>
 </td>
