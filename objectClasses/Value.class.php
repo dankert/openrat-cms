ec375bda021342541a27a20a4cb753de4b22d5f0
dankert <devnull@localhost>
2004-07-07 22:48:33 +0200

    Neuer Elementtyp: select

--- objectClasses/Value.class.php
+++ objectClasses/Value.class.php
@@ -20,7 +20,10 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ---------------------------------------------------------------------------
 // $Log$
-// Revision 1.8  2004-05-03 21:15:30  dankert
+// Revision 1.9  2004-07-07 20:48:33  dankert
+// Neuer Elementtyp: select
+//
+// Revision 1.8  2004/05/03 21:15:30  dankert
 // Umstellung auf dezimale ASCII-Werte
 //
 // Revision 1.7  2004/05/03 20:21:49  dankert
@@ -161,30 +164,86 @@ class Value
 		global $conf_languagedir,
 		       $conf_php;
 
+		$ini_chars = parse_ini_file( $conf_languagedir.'/specialchars.ini.'.$conf_php );
+
 		$neu = array();
+
+		$toc = array();
+		$tocid = 0;
 	
 		$pre   = false;
+		$quote = false;
 		$br    = false;
 		$ul    = false;
 		$ol    = false;
+		$li    = true;
+		$level = 0;
+		$lis   = array();
 		$table = false;
 		$p     = false;
-		
-		$text = str_replace( "\n===",'H1H1H1',$text );
-		$text = str_replace( "\n---",'H2H2H2',$text );
-		$text = str_replace( "\n...",'H3H3H3',$text );
 
+		// Links
+		if   ( $this->element->html )
+			$pf = '>';
+		else $pf = '&gt;';
+
+		if   ( $this->element->html )
+			$pf = '>';
+		else $pf = '&gt;';
+		
 		// Zeichenkette in die einzelnen Zeilen zerlegen
 		$zeilen = explode("\n",$text);
 		
 		foreach( $zeilen as $zeile )
 		{
-			# Leerzeichen und sonstige Sonderzeichen am Zeilenende entfernen
+			# Leer- und Sonderzeichen am Zeilenende entfernen
 			$zeile = chop( $zeile );
+			
+			// Backtick am Zeilenbeginn schaltet Wikiauszeichnungen aus
+			if	( substr($zeile,0,1) == '`' && !$pre )
+			{
+				$zeile = substr($zeile,1);
+				$nowiki = true;
+			}
+			else
+			{
+				$nowiki = false;
+			}
+
+			// Zitat Anfang
+			if   ( $zeile == $pf  &&  !$quote && !$pre )
+			{
+				if	( $p )
+				{
+					$neu[] = '</p>';
+					$p = false;
+				}
+
+				$zeile = '<blockquote>';
+				$quote = true;
+			}
+	
+			// Zitat Ende
+			if   ( $zeile == $pf &&  $quote  && !$pre )
+			{
+				if	( $p )
+				{
+					$neu[] = '</p>';
+					$p = false;
+				}
+				$zeile = '</blockquote>';
+				$quote = false;
+			}
 		
 			// Präformatierter Text Anfang
 			if   ( $zeile == '='  &&  !$pre )
 			{
+				if	( $p )
+				{
+					$neu[] = '</p>';
+					$p = false;
+				}
+
 				$zeile = '<pre>';
 				$pre = true;
 			}
@@ -197,51 +256,24 @@ class Value
 			}
 		
 
+			// Überschriften
+			if	( preg_match('/^([+]{1,}) ?(.*)/',$zeile,$match) && !$nowiki && !$pre && !$quote )
+			{
+				if	( $p )
+				{
+					$neu[] = '</p>';
+					$p = false;
+				}
+
+				$tocid++;
+				$hlev = strlen($match[1]);
+				$toc[] = array('level'=>$hlev,'id'=>$tocid,'text'=>$match[2]);
+				$zeile = '<h'.$hlev.'><a name="toc'.$tocid.'"></a>'.$match[2].'</h'.$hlev.'>';
+			}
+
 			// Bei präformatierem Text keine weiteren Formatierungen durchführen	
 			if   ( !$pre )
 			{
-				// Überschrift 1. Ordnung	
-				if   ( substr($zeile,0,3) == '!!!' )
-				{
-					$zeile = '<h1>'.substr($zeile,3).'</h1>';
-				}
-				
-				if   ( ereg( 'H1H1H1.*$',$zeile ) )
-				{
-					$zeile = eregi_replace( 'H1H1H1.*$','',$zeile );
-					$zeile = chop( $zeile );
-					$zeile = '<h1>'.$zeile.'</h1>';
-				}
-				
-
-				// Überschrift 2. Ordnung	
-				if   ( substr($zeile,0,2) == '!!' )
-				{
-					$zeile = '<h2>'.substr($zeile,2).'</h2>';
-				}
-
-				if   ( ereg( 'H2H2H2.*$',$zeile ) )
-				{
-					$zeile = eregi_replace( 'H2H2H2.*$','',$zeile );
-					$zeile = chop( $zeile );
-					$zeile = '<h2>'.$zeile.'</h2>';
-				}
-		
-
-				// Überschrift 3. Ordnung	
-				if   ( substr($zeile,0,1) == '!' )
-				{
-					$zeile = '<h3>'.substr($zeile,1).'</h3>';
-				}
-
-				if   ( ereg( 'H3H3H3.*$',$zeile ) )
-				{
-					$zeile = eregi_replace( 'H3H3H3.*$','',$zeile );
-					$zeile = chop( $zeile );
-					$zeile = '<h3>'.$zeile.'</h3>';
-				}
-	
-	
 				// Tabellen
 				$beg = substr($zeile,0,1);
 				
@@ -249,18 +281,30 @@ class Value
 				{
 					if   ( !$table )
 					{
+						if	( $p )
+						{
+							$neu[] = '</p>';
+							$p = false;
+						}
+	
 						$neu[] = '<table>';
 						$table = true;
 					}
 					
 					$zeile = ereg_replace( '^\|','<tr><td>',$zeile );
-					$zeile = ereg_replace( '\|$','</td></tr>',$zeile );
+					$zeile = ereg_replace( '\|?$','</td></tr>',$zeile );
 					$zeile = str_replace( '|','</td><td>',$zeile );
 
-					$zeile = eregi_replace( '<td>!([^<]+)</td>','<th>\\1</th>',$zeile );
+					// Spaltenübergreifende Zellen
+					$zeile = str_replace('<td></td><td>','<td colspan\="2">',$zeile);
+					for( $i=2; $i<=10; $i++)
+						$zeile = str_replace('</td><td colspan\="'.$i.'"></td><td>','</td><td colspan\="'.($i+1).'">',$zeile);
 
-					$zeile = eregi_replace( '<td>\(([a-zA-Z0-9]+)\)([^<]+)</td>','<td class="\\1">\\2</td>',$zeile );
-					$zeile = eregi_replace( '<th>\(([a-zA-Z0-9]+)\)([^<]+)</th>','<th class="\\1">\\2</th>',$zeile );
+					// Spalten-Überschriften <th>
+					$zeile = eregi_replace( '<td([^<]*)>!([^<]+)</td>','<th\\1>\\2</th>',$zeile );
+
+					// CSS-Klassen
+					$zeile = eregi_replace( '<t([dh][^<]*)>\(([a-zA-Z0-9]+)\)([^<]+)</td>','<t\\1 class\="\\2">\\3</td>',$zeile );
 				}
 				else
 				{
@@ -270,45 +314,54 @@ class Value
 						$neu[] = '</table>';
 					}
 				}
-				
-				$beg = substr($zeile,0,2);
-			
-				// numerierte Aufzaehlungen
-				if   ( $beg == '# ' )
+
+
+				// Aufzählungen		
+				if	( preg_match('/^( *)([\*#-]) (.*)/',$zeile,$match) && !$nowiki )
 				{
-					if   ( !$ol )
+					if	( $p )
 					{
-						$neu[] = '<ol>';
-						$ol = true;
+						$neu[] = '</p>';
+						$p = false;
 					}
-					$zeile = '<li>'.substr($zeile,2).'</li>';
+
+					$lev  = strlen($match[1])+1;
+					$type = $match[2];
+					$text = $match[3];
+					
+					if	( $level == $lev ) $neu[] = '</li>';
+
+					while( $level < $lev )
+					{
+						$level++;
+
+						if	( $match[2]=='#')
+							$neu[] = '<ol class="level'.$level.'">';
+						else	$neu[] = '<ul class="level'.$level.'">';
+
+						$lis[$level] = $match[2]; 
+					}
+
+					while( $level > $lev )
+					{
+						$neu[] = '</li>';
+						if	( $lis[$level]=='#')
+							$neu[] = '</ol>';
+						else	$neu[] = '</ul>';
+
+						$level--;
+					}
+
+					$zeile = '<li>'.$text;
 				}
 				else
 				{
-					if ( $ol )
+					while( $level > 0 )
 					{
-						$ol = false;
-						$neu[] = '</ol>';
-					}
-				}
-		
-		
-				// einfache Aufzaehlungen
-				if   ( $beg == '- ' || $beg == '* ' || $beg == 'o ' )
-				{
-					if   ( !$ul )
-					{
-						$neu[] = '<ul>';
-						$ul = true;
-					}
-					$zeile = '<li>'.substr($zeile,2).'</li>';
-				}
-				else
-				{
-					if ( $ul )
-					{
-						$ul = false;
-						$neu[] = '</ul>';
+						if	( $lis[$level]=='#')
+							$neu[] = '</ol>';
+						else	$neu[] = '</ul>';
+						$level--;
 					}
 				}
 			}
@@ -334,29 +387,21 @@ class Value
 					$p = true;
 				}
 			}
+		
 	
 	
 			// Textauszeichnungen fett,kursiv,fest		
-			if   ( !$pre )  // nicht bei präformatiertem Text
+			if   ( !$pre && !$nowiki )  // nicht bei präformatiertem Text
 			{
 				// *Fett*	
-				$zeile = ereg_replace( '\*([^*]+[^\\])\*' , '<strong>\\1</strong>' , $zeile );
+				//$zeile = ereg_replace( '\*([^*]+[^\\])\*' , '<strong>\\1</strong>' , $zeile );
+				$zeile = preg_replace( '/\*([^*]+[^\\\\]+)\*/' , '<strong>\\1</strong>' , $zeile );
 	
 				// kursiv
-				$zeile = ereg_replace( '_([^_]+[^\\])_'    , ' <em>\\1</em> ' , $zeile );
-				//$zeile = ereg_replace( '\/([^\/:]+)\/', ' <em>\\1</em> ' , $zeile );
+				$zeile = ereg_replace( '_([^_]+[^\\])_'        , '<em>\\1</em>' , $zeile );
 	
 				// feste Breite
-				$zeile = ereg_replace( '=([^=]+[^\\])=' , ' <tt>\\1</tt> ' , $zeile );
-
-				$zeile = str_replace( '\*','*',$zeile );
-				$zeile = str_replace( '\_','_',$zeile );
-				$zeile = str_replace( '\=','=',$zeile );
-	
-				// Links
-				if   ( $this->element->html )
-					$pf = '>';
-				else $pf = '&gt;';
+				$zeile = ereg_replace( '=([^=]+[^\\])='        , '<tt>\\1</tt>' , $zeile );
 
 				// Text->... umsetzen nach "Text"->... (Anfuehrungszeichen ergaenzen)
 				$zeile = ereg_replace( '([A-Za-z0-9._?äöüÄÖÜß-]+)-'.$pf, '"\\1"-'.$pf, $zeile );
@@ -385,7 +430,7 @@ class Value
 				$zeile = ereg_replace( '^((https?|ftps?|news|gopher):\/\/([A-Za-z0-9._\/\,-]*))', '<a href="\\1">\\3</a>', $zeile );
 				
 				// Einbinden von Bildern
-				$zeile = ereg_replace( '(ima?ge?):\/?\/?(([0-9]+))(\{.*\})?', '<img src="object:\\2" />', $zeile );
+				$zeile = ereg_replace( '(ima?ge?):\/?\/?(([0-9]+))(\{.*\})?', '<img src="object:\\2" alt="" />', $zeile );
 		
 				# mailto:...-Links
 				$zeile = ereg_replace( '([A-Za-z0-9._-]+@[A-Za-z0-9._-]+)', '<a href="mailto:\\1">\\1</a>', $zeile );
@@ -397,6 +442,8 @@ class Value
 					$var = $this->page->path_to_object( $object[1] );
 					$zeile = str_replace( $object[0],$var,$zeile );
 				}
+
+				$zeile = ereg_replace( '([^\\\\])\\\\','\\1', $zeile );
 			}
 			$neu[] = $zeile;
 		}
@@ -410,11 +457,21 @@ class Value
 	
 		$text = implode("\n",$neu);
 		
-		$ini_chars = parse_ini_file( $conf_languagedir.'/specialchars.ini.'.$conf_php );
+		// Inhaltsverzeichnis
+
+		$toctext = array();
+		foreach( $toc as $t )
+		{
+			if	($t['level'] == 1 && count($toctext)>0) $toctext[] = '';
+			$toctext[] = str_repeat('&nbsp;',$t['level']*2).'<a href="#toc'.$t['id'].'">'.$t['text'].'</a>';
+		}
+		$text = str_replace( '##TOC##',implode("<br/>\n",$toctext),$text ); // Inhaltsverzeichnis einfügen
+		
 		foreach( $ini_chars  as $key=>$val)
 		{
 			$text = str_replace( chr($key),$val,$text );
 		}
+		
 		return $text;
 	}
 
@@ -750,6 +807,7 @@ class Value
 
 			case 'longtext':
 			case 'text':
+			case 'select':
 
 				$inhalt = $this->text;
 	
@@ -759,8 +817,11 @@ class Value
 				// Wenn HTML nicht erlaubt ist, dann die HTML-Tags ersetzen
 				if   ( !$this->element->html )
 				{
-					$inhalt = str_replace('<','&lt;',$inhalt);
-					$inhalt = str_replace('>','&gt;',$inhalt);
+					//$inhalt = htmlentities($inhalt);
+					
+					$inhalt = str_replace('<','&lt;' ,$inhalt);
+					$inhalt = str_replace('>','&gt;' ,$inhalt);
+					$inhalt = str_replace('>','&amp;',$inhalt);
 				}
 	
 				// Schnellformatierung ('Wiki') durchführen
