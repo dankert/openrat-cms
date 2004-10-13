1b16159594e3f82561c997cd0c4e0fe1c0adcc3a
dankert <devnull@localhost>
2004-10-14 00:13:24 +0200

    Auswerten der Berechtigungen

--- actionClasses/TreemenuAction.class.php
+++ actionClasses/TreemenuAction.class.php
@@ -20,8 +20,11 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ---------------------------------------------------------------------------
 // $Log$
-// Revision 1.3  2004-05-02 14:49:37  dankert
-// Einfügen package-name (@package)
+// Revision 1.4  2004-10-13 22:13:24  dankert
+// Auswerten der Berechtigungen
+//
+// Revision 1.3  2004/05/02 14:49:37  dankert
+// Einf?gen package-name (@package)
 //
 // Revision 1.2  2004/04/25 17:35:42  dankert
 // openall_url setzen
@@ -50,47 +53,26 @@ class TreemenuAction extends Action
 		
 		$projects = array();
 
-		if   (isset($SESS['user']))
+		$user = $this->getUserFromSession();
+		
+		if	( isset($user) )
 		{
-			// Lesen der verfügbaren Projekte
-			$projekte = Project::getAll();
+			// Lesen aller Projekte, fuer die der Benutzer berechtigt ist.
+			$projects = $user->getReadableProjects();
 
 			$projects[0] = lang('SELECT');
 		
 			// Unterscheidung Administrator/Benutzer
-			if   ( $SESS['user']['is_admin'] == '1' )
+			if   ( $user->isAdmin )
 			{
 				// Administrator sieht Administrationsbereich
 				$projects[-1] = lang('ADMINISTRATION');
-				
-				// Administrator sieht alle Projekte
-				foreach( $projekte as $projectid=>$name )
-				{
-					$projects[$projectid] = $name;
-				}
 			}
-			else
-			{
-				// Bereitstellen der Projekte, für die der Benutzer berechtigt ist
-				foreach( $projekte as $projectid=>$projectname )
-				{
-					$project = new Project( $projectid );
-					 
-					$rootObject = new Object( $project->getRootObjectId() );
-					$rootObject->load();
-					 
-					if   ( $rootObject->hasRight('read') )
-						$projects[$projectid] = $projectname;
-				}
-			}
+			ksort( $projects );
+
 			$this->setTemplateVar( 'act_projectid',intval($this->getSessionVar('projectid')) );
 		}
 		$this->setTemplateVar('projects',$projects);
-
-		$this->setTemplateVar('reload_url'  ,Html::url( array('action'   =>'tree',
-		                                                      'subaction'=>'reload') ));
-		$this->setTemplateVar('openall_url' ,Html::url( array('action'   =>'tree',
-		                                                      'subaction'=>'openall') ));
 		
 		// Ausgabe des Templates
 		$this->forward('tree_menu');
