f2e87144535ab500730e49d5973f6211e06efb72
dankert <devnull@localhost>
2004-11-07 20:23:07 +0100

    Titel immer anzeigen (Abfrage auf Anmeldung entfernt)

--- themes/default/pages/html/title.tpl.php
+++ themes/default/pages/html/title.tpl.php
@@ -1,8 +1,6 @@
 <?php include($tpl_dir.'header.tpl.php') ?>
 
 <!-- $Id$ -->
-<?php if (isset($username))
-      { ?>
 
 <table cellpadding="5" cellspacing="0" width="100%">
   <tr>
@@ -10,7 +8,5 @@
     <td class="title" style="text-align:right;"><a href="<?php echo $profile_url ?>" title="<?php echo lang('HELP_PROFILE'); ?>" target="cms_main_main"><?php echo lang('YOURPROFILE'); ?></a>&nbsp;|&nbsp;<a href="<?php echo $logout_url ?>" title="<?php echo lang('HELP_LOGOUT'); ?>" target="_top"><?php echo lang('LOGOUT'); ?></a></td>
   </tr>
 </table>
-
-<?php } ?>
 
 <?php include($tpl_dir.'footer.tpl.php') ?>
