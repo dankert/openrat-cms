d52a93ce1dc8f0a802745754459d41b0ca344da5
dankert <devnull@localhost>
2004-11-10 23:56:47 +0100

    Verlinkung der GPL-Lizenz

--- themes/default/pages/html/copyright.tpl.php
+++ themes/default/pages/html/copyright.tpl.php
@@ -2,10 +2,4 @@
 <!-- The GPL licence requires this text, so NEVER remove nor change it. -->
 
 <br><br>
-<p class="copyright">
-<br><br>
-<p class="copyright">
-<?php echo lang('help_copyright') ?><br><br>
-<?php echo lang('help_gpl') ?>
-</p>
-<a href="http://www.gnu.org/copyleft/gpl.html" target="_top">details</a>
+<p class="copyright"><a href="http://www.gnu.org/copyleft/gpl.html" target="_top"><?php echo lang('login_gpl') ?></a></p>
