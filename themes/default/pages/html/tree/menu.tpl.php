4e312b1ef898ad47f63db9626b08be2ed2684927
dankert <devnull@localhost>
2004-11-10 23:53:28 +0100

    Wenn nur 1 Projekt zur Auswahl, dann Menue nicht zeigen

--- themes/default/pages/html/tree/menu.tpl.php
+++ themes/default/pages/html/tree/menu.tpl.php
@@ -3,26 +3,43 @@
 <!-- $Id$ -->
 
 <table cellpadding="5" cellspacing="0" width="100%" height="100%">
-<tr>
+  <tr>
     <td class="menu">
 
-      <form style="margin:0px;" action="<?php echo $self ?>" method="get" target="cms_tree">
+    <?php if (isset($act_projectid))
+	      { ?>
+      <?php if (count($projects)>1)
+            { ?>
+      <form style="margin:0px;" action="<?php echo $self ?>" method="get" target="_parent">
       <table>
       <tr>
       <td>
-        <input type="hidden" name="action" value="tree">
-        <input type="hidden" name="subaction" value="reload">
+        <input type="hidden" name="action"    value="index">
+        <input type="hidden" name="subaction" value="show">
         <?php echo Html::selectBox( 'projectid',$projects,$act_projectid,array('onchange'=>'submit();') ) ?></td><td><noscript>&nbsp;<input type="submit" class="submit" value="&raquo;"></noscript>
       </td>
       </tr>
       </table>
       </form>
+      <?php } else
+            { ?>
+      <table cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+          <td><span class="mainmenu_headline"><?php echo lang('GLOBAL_PROJECT') ?></span></td>
+        </tr>
+        <tr>
+          <td class="menu" colspan="3"><span class="mainmenu_name"><?php echo $projects[$act_projectid] ?></span></td>          
+        </tr>
+      </table>
+	<?php } ?>
+	<?php } else
+	      { ?>
+	      &nbsp;
+	<?php } ?>
     </td>
   </tr>
   <tr>
-    <td class="submenu" height="20">&nbsp;<!--
-    <a accesskey="r" href="<?php echo $reload_url ?>" target="cms_tree"><?php echo lang('REFRESH') ?></a> | <a href="<?php echo $openall_url ?>" target="cms_tree">++</a>-->
-    </td>
+    <td class="submenu" height="20">&nbsp;</td>
   </tr>
 </table>
 
