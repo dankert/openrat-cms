e5bad90cb97fcb49c7b42340b05273d7a9eae38a
dankert <devnull@localhost>
2004-11-10 23:52:06 +0100

    Auswahl eines Projektes

---
+++ themes/default/pages/html/project/select.tpl.php
@@ -0,0 +1,24 @@
+<?php include( $tpl_dir.'header.tpl.php') ?>
+
+<!-- $Id$ -->
+<center>
+
+<table class="main" width="50%" cellspacing="0" cellpadding="4">
+
+<tr>
+	<th><?php echo lang('PROJECTS') ?></th>
+</tr>
+
+<?php $f1=true;
+      foreach( $el as $id=>$e )
+      { ?>
+<tr>
+<td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $e['url'] ?>" target="_top"><img src="<?php echo $image_dir ?>icon_project.png" border="0" align="left"><?php echo $e['name'] ?></a></td>
+</tr>
+<?php } ?>
+
+
+</table>
+
+</center>
+<?php include( $tpl_dir.'footer.tpl.php') ?>
