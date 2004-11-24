be5d90f26ef9b08f419862ae61eea358be403031
dankert <devnull@localhost>
2004-11-24 23:07:02 +0100

    Menueleiste verkleinert

--- themes/default/pages/html/frameset/main.tpl.php
+++ themes/default/pages/html/frameset/main.tpl.php
@@ -4,7 +4,7 @@
 <head>
 <title></title>
 </head>
-     <frameset rows="75,*" border="0" frameborder="0" framespacing="0">
+     <frameset rows="65,*" border="0" frameborder="0" framespacing="0">
           <frame src="<?php echo $frame_src_main_menu ?>" name="cms_main_menu" marginheight="0" marginwidth="0" scrolling="no">
           <frame src="<?php echo $frame_src_main_main ?>" name="cms_main_main" marginheight="0" marginwidth="0" scrolling="auto">
      </frameset>
