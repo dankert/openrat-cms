decbcfbc352f219659cc4542ed20d97aa8bd1844
dankert <devnull@localhost>
2004-11-28 20:13:39 +0100

    Anpassen an neue Sprachdatei-Konventionen

--- themes/default/pages/html/page/prop.tpl.php
+++ themes/default/pages/html/page/prop.tpl.php
@@ -11,29 +11,29 @@
 <table class="main" width="90%" cellspacing="0" cellpadding="4">
 
   <tr>
-    <th colspan="2"><?php echo lang('PROP') ?></th>
+    <th colspan="2"><?php echo lang('GLOBAL_PROP') ?></th>
   </tr>
 
   <tr>
-    <td width="50%" rowspan="2" class="f1"><?php echo lang('name') ?></a></td>
+    <td width="50%" rowspan="2" class="f1"><?php echo lang('GLOBAL_NAME') ?></a></td>
     <td width="50%"             class="f1"><input type="text" class="name" name="name" size="50" value="<?php echo $name ?>"></td>
   </tr>
   <tr>
-  <td width="50%" class="help"><?php echo lang('HELP_NAME') ?></td>
+  <td width="50%" class="help"><?php echo lang('GLOBAL_NAME_DESC') ?></td>
   </tr>
   <tr>
-    <td width="50%" rowspan="2" class="f1"><?php echo lang('filename') ?></a></td>
+    <td width="50%" rowspan="2" class="f1"><?php echo lang('GLOBAL_FILENAME') ?></a></td>
     <td width="50%"             class="f1"><input type="text" class="filename" name="filename" size="50" value="<?php echo $filename ?>"></td>
   </tr>
   <tr>
-	<td width="50%" class="help"><?php echo lang('HELP_FILENAME') ?></td>
+	<td width="50%" class="help"><?php echo lang('GLOBAL_FILENAME_DESC') ?></td>
   </tr>
   <tr>
-    <td width="50%" class="f2"><?php echo lang('description') ?></a></td>
+    <td width="50%" class="f2"><?php echo lang('GLOBAL_DESCRIPTION') ?></a></td>
     <td width="50%" class="f2"><textarea cols="40" rows="10" name="desc"><?php echo $desc ?></textarea></td>
   </tr>
   <tr>
-    <td width="50%" class="f1"><?php echo lang('template') ?></a></td>
+    <td width="50%" class="f1"><?php echo lang('GLOBAL_TEMPLATE') ?></a></td>
     <?php if (isset($template_url))
           { ?>
     <td width="50%" class="f1"><a href="<?php echo $template_url ?>" target="cms_main"><img src="<?php echo $image_dir ?>icon_tpl.png" border="0" align="left"><?php echo $template_name ?></a></td>
@@ -44,23 +44,23 @@
     <?php } ?>
   </tr>
   <tr>
-    <td width="50%" class="f1"><?php echo lang('full_filename') ?></a></td>
+    <td width="50%" class="f1"><?php echo lang('GLOBAL_FULL_FILENAME') ?></a></td>
     <td width="50%" class="f1"><tt><?php echo $full_filename ?></tt></td>
   </tr>
 
   <?php if ($delete)
         { ?>
   <tr>
-    <td class="f1" rowspan="2"><?php echo lang('DELETE') ?></a></td>
+    <td class="f1" rowspan="2"><?php echo lang('GLOBAL_DELETE') ?></a></td>
     <td class="f1"><input type="checkbox" name="delete" value="1"></td>
   </tr>
   <tr>
-    <td class="help"><?php echo lang('HELP_PAGE_DELETE') ?></td>
+    <td class="help"><?php echo lang('PAGE_DELETE_DESC') ?></td>
   </tr>
   <?php } ?>
 
   <tr>
-    <td class="act" colspan="2"><input type="submit" class="submit" value="<?php echo lang('SAVE') ?>"></td>
+    <td class="act" colspan="2"><input type="submit" class="submit" value="<?php echo lang('GLOBAL_SAVE') ?>"></td>
   </tr>
 
 </table>
@@ -97,42 +97,7 @@
 <?php } ?>
 
 
-<!-- Verschieben -->
-<form action="<?php echo $self ?>" method="post" target="_self">
-
-<input type="hidden" name="action"    value="page" />
-
-<table class="main" width="90%" cellspacing="0" cellpadding="4">
-
-  <tr>
-    <th colspan="2"><?php echo lang('GLOBAL_MOVE') ?> / <?php echo lang('GLOBAL_COPY') ?></th>
-  </tr>
-
-  <tr>
-    <td width="50%" class="f1"><?php echo lang('GLOBAL_ACTION') ?></a></td>
-    <td width="50%" class="f1"><input type="radio" name="subaction" value="move" checked="checked" /> <?php echo lang('GLOBAL_MOVE') ?><br />
-    <input type="radio" name="subaction" value="copy" /> <?php echo lang('GLOBAL_COPY') ?><br />
-    <input type="radio" name="subaction" value="link" /> <?php echo lang('GLOBAL_LINK') ?><br />
-    </td>
-  </tr>
-
-  <tr>
-    <td width="50%" class="f1"><?php echo lang('GLOBAL_FOLDER') ?></a></td>
-    <td width="50%" class="f1"><?php echo Html::selectBox('targetobjectid',$folder,$act_folderobjectid) ?></td>
-  </tr>
-
-  <tr>
-    <td class="act" colspan="2"><input type="submit"  class="submit" value="<?php echo lang('GLOBAL_SAVE') ?>"></td>
-  </tr>
-
-</table>
-
-</form>
-
-
-<script name="JavaScript" type="text/javascript"><!--
-document.forms[0].name.focus();
-//--></script>
+<?php Html::focusField('name') ?>
 
 </center>
 
