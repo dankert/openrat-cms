016ebea78f198adacc1c5354c552d8add5095d5e
dankert <devnull@localhost>
2004-11-28 18:35:55 +0100

    Bei 1 Projekt Deaktivieren des Eingabefeldes

--- themes/default/pages/html/tree/menu.tpl.php
+++ themes/default/pages/html/tree/menu.tpl.php
@@ -6,43 +6,30 @@
   <tr>
     <td class="menu">
 
-    <?php if (isset($act_projectid))
-	      { ?>
-      <?php if (count($projects)>1)
-            { ?>
       <?php echo Html::form( array('action'=>'index','subaction'=>'show','target'=>'_parent','method'=>'get','name'=>'') ) ?>
       <table>
       <tr>
       <td>
+        <?php
+          if	( count($projects) < 2 )
+          	$attrDisabled = array('disabled'=>'disabled');
+          else
+          	$attrDisabled = array();
+        ?>
         <input type="hidden" name="action"    value="index">
         <input type="hidden" name="subaction" value="show">
-        <?php echo Html::selectBox( 'projectid',$projects,$act_projectid,array('onchange'=>'submit();') ) ?></td><td><noscript>&nbsp;<input type="submit" class="submit" value="&raquo;"></noscript>
+        <?php echo Html::selectBox( 'projectid',$projects,$act_projectid,array('onchange'=>'submit();','style'=>'margin:0px;padding:0px;')+$attrDisabled ) ?></td><td><noscript>&nbsp;<input type="submit" class="submit" value="&raquo;"></noscript>
       </td>
       </tr>
       </table>
       </form>
-      <?php } else
-            { ?>
-      <table cellpadding="0" cellspacing="0" width="100%">
-        <tr>
-          <td><span class="mainmenu_headline"><?php echo lang('GLOBAL_PROJECT') ?></span></td>
-        </tr>
-        <tr>
-          <td class="menu" colspan="3"><span class="mainmenu_name"><?php echo $projects[$act_projectid] ?></span></td>          
-        </tr>
-      </table>
-	<?php } ?>
-	<?php } else
-	      { ?>
-	      &nbsp;
-	<?php } ?>
     </td>
   </tr>
   <tr>
     <td class="submenu" height="20">
       <?php if (isset($language_name)) { ?>
-      <?php echo lang('LANGUAGE') ?>:&nbsp;<a href="<?php echo $language_url     ?>" target="cms_main"><?php echo $language_name     ?></a> | 
-      <?php echo lang('MODEL')    ?>:&nbsp;<a href="<?php echo $projectmodel_url ?>" target="cms_main"><?php echo $projectmodel_name ?></a>
+      <?php echo lang('GLOBAL_LANGUAGE') ?>:&nbsp;<a href="<?php echo $language_url     ?>" target="cms_main"><?php echo $language_name     ?></a> | 
+      <?php echo lang('GLOBAL_MODEL')    ?>:&nbsp;<a href="<?php echo $projectmodel_url ?>" target="cms_main"><?php echo $projectmodel_name ?></a>
   	  <?php } else  { ?>&nbsp;
 	  <?php } ?>
     </td>
