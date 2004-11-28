86f12a41cc60598a5a1dfbfa2d77ef50a8cacd2a
dankert <devnull@localhost>
2004-11-28 18:51:23 +0100

    Anpassen an neue Sprachdatei-Konventionen

--- themes/default/pages/html/title.tpl.php
+++ themes/default/pages/html/title.tpl.php
@@ -4,8 +4,8 @@
 
 <table cellpadding="5" cellspacing="0" width="100%">
   <tr>
-    <td class="title"><span title="<?php echo lang('LOGINAS') ?>: <?php echo $username ?>"><?php echo $userfullname ?></span> <strong>@</strong> <span title="<?php echo lang('DATABASE') ?>: <?php echo $dbid ?>"><?php echo $dbname ?></span></td>
-    <td class="title" style="text-align:right;"><a href="<?php echo $profile_url ?>" title="<?php echo lang('HELP_PROFILE'); ?>" target="cms_main_main"><?php echo lang('YOURPROFILE'); ?></a>&nbsp;|&nbsp;<a href="<?php echo $logout_url ?>" title="<?php echo lang('HELP_LOGOUT'); ?>" target="_top"><?php echo lang('LOGOUT'); ?></a></td>
+    <td class="title"><span title="<?php echo lang('USER_LOGINAS') ?>: <?php echo $username ?>"><?php echo $userfullname ?></span> <strong>@</strong> <span title="<?php echo lang('GLOBAL_DATABASE') ?>: <?php echo $dbid ?>"><?php echo $dbname ?></span></td>
+    <td class="title" style="text-align:right;"><a href="<?php echo $profile_url ?>" title="<?php echo lang('USER_PROFILE_DESC'); ?>" target="cms_main_main"><?php echo lang('USER_YOURPROFILE'); ?></a>&nbsp;|&nbsp;<a href="<?php echo $logout_url ?>" title="<?php echo lang('USER_LOGOUT_DESC'); ?>" target="_top"><?php echo lang('USER_LOGOUT'); ?></a></td>
   </tr>
 </table>
 
