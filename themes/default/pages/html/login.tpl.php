66557759208008a454fbd7dd95bc4a2ab7cd63ef
dankert <devnull@localhost>
2004-11-28 18:49:00 +0100

    Anpassen an neue Sprachdatei-Konventionen

--- themes/default/pages/html/login.tpl.php
+++ themes/default/pages/html/login.tpl.php
@@ -57,7 +57,7 @@
 <!-- The GPL licence requires this text, so NEVER remove nor change it. -->
 
 <br><br>
-<p class="copyright"><a href="http://www.gnu.org/copyleft/gpl.html" target="_top"><?php echo lang('GLOBAL_LOGIN_GPL') ?></a></p>
+<p class="copyright"><a href="http://www.gnu.org/copyleft/gpl.html" target="_top"><?php echo lang('GLOBAL_GPL') ?></a></p>
 
 </center>
 
