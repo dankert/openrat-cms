63e2d79e1867776e96e2bde7cdbc5ed3525ebe54
dankert <devnull@localhost>
2004-11-29 20:26:28 +0100

    Anpassen an neue Sprachdatei-Konventionen

--- themes/default/pages/html/link/prop.tpl.php
+++ themes/default/pages/html/link/prop.tpl.php
@@ -11,45 +11,38 @@
 <table class="main" width="90%" cellspacing="0" cellpadding="4">
 
 <tr>
-  <th colspan="2"><?php echo lang('PROP') ?></th>
+  <th colspan="2"><?php echo lang('GLOBAL_PROP') ?></th>
 </tr>
 
   <tr>
-    <td width="50%" class="f1" rowspan="2"><?php echo lang('name') ?></a></td>
+    <td width="50%" class="f1" rowspan="2"><?php echo lang('GLOBAL_name') ?></a></td>
     <td width="50%" class="f1"><input type="text" class="name" name="name" size="50" value="<?php echo $name ?>"></td>
   </tr>
   <tr>
-    <td width="50%" class="help"><?php echo lang('HELP_NAME') ?></td>
+    <td width="50%" class="help"><?php echo lang('GLOBAL_NAME_DESC') ?></td>
   </tr>
   <tr>
-    <td width="50%" class="f2"><?php echo lang('description') ?></a></td>
+    <td width="50%" class="f2"><?php echo lang('GLOBAL_description') ?></a></td>
     <td width="50%" class="f2"><textarea class="desc" cols="40" rows="10" name="desc"><?php echo $desc ?></textarea></td>
   </tr>
   <tr>
-    <td class="f1"><input type="radio" name="type" value="link"<?php if ($type=='link') echo ' checked="checked"'; ?>><?php echo lang('target') ?></a></td>
+    <td class="f1"><input type="radio" name="type" value="link"<?php if ($type=='link') echo ' checked="checked"'; ?>><?php echo lang('LINK_TARGET') ?></a></td>
     <td class="f1"><?php echo Html::selectBox('linkobjectid',$objects,$act_linkobjectid) ?></td>
   </tr>
   <tr>
-    <td class="f1"><input type="radio" name="type" value="url"<?php if ($type=='url') echo ' checked="checked"'; ?>><?php echo lang('url') ?></a></td>
+    <td class="f1"><input type="radio" name="type" value="url"<?php if ($type=='url') echo ' checked="checked"'; ?>><?php echo lang('LINK_URL') ?></a></td>
     <td class="f1"><input type="text" name="url" size="50" maxlength="255" value="<?php echo $url; ?>"></td>
   </tr>
   <tr>
-    <td width="50%" class="f2"><?php echo lang('created') ?></a></td>
+    <td width="50%" class="f2"><?php echo lang('GLOBAL_created') ?></a></td>
     <td width="50%" class="f2"><?php echo date(lang('DATE_FORMAT'),$create_date) ?>, <?php if (isset($create_user['url'])) echo'<a href="'.$create_user['url'].'" target="cms_main">' ?><?php echo $create_user['name'] ?><?php if (isset($create__user['url'])) echo'</a>' ?></td>
   </tr>
   <tr>
-    <td width="50%" class="f2"><?php echo lang('lastchange') ?></a></td>
+    <td width="50%" class="f2"><?php echo lang('GLOBAL_lastchange') ?></a></td>
     <td width="50%" class="f2"><?php echo date(lang('DATE_FORMAT'),$lastchange_date) ?>, <?php if (isset($lastchange_user['url'])) echo'<a href="'.$lastchange_user['url'].'" target="cms_main">' ?><?php echo $lastchange_user['name'] ?><?php if (isset($lastchange_user['url'])) echo'</a>' ?></td>
   </tr>
   <tr>
-    <td class="f1" rowspan="2"><?php echo lang('DELETE') ?></a></td>
-    <td class="f1"><input type="checkbox" name="delete" value="1"></td>
-  </tr>
-  <tr>
-    <td class="help"><?php echo lang('LINK_DELETE_DESC') ?></td>
-  </tr>
-  <tr>
-    <td class="act" colspan="2"><input type="submit" class="submit" value="<?php echo lang('SAVE') ?>"></td>
+    <td class="act" colspan="2"><input type="submit" class="submit" value="<?php echo lang('GLOBAL_SAVE') ?>"></td>
   </tr>
 
 </table>
