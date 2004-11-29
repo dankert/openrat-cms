ca9ead42476c4500656100d2571ae341259957ba
dankert <devnull@localhost>
2004-11-29 22:10:54 +0100

    Anpassen an neue Sprachdatei-Konventionen

--- themes/default/pages/html/folder/pub.tpl.php
+++ themes/default/pages/html/folder/pub.tpl.php
@@ -5,24 +5,28 @@
 <form action="<?php echo $self ?>" method="post" target="_self">
 
 <input type="hidden" name="<?php echo session_name() ?>" value="<?php echo session_id() ?>">
-<input type="hidden" name="subaction" value="pub">
-<input type="hidden" name="go" value="1">
+<input type="hidden" name="subaction" value="pub2">
 
 <table class="main" width="90%" cellspacing="0" cellpadding="4">
 
 <tr>
-  <th colspan="2"><?php echo lang('PUBLISH') ?></th>
+  <th colspan="2"><?php echo lang('GLOBAL_PUBLISH') ?></th>
 </tr>
 
 <tr>
-<td width="50%" class="f1"><input type="checkbox" name="pages" value="1" checked="checked"><?php echo lang('pages') ?><br>
-                           <input type="checkbox" name="files" value="1" checked="checked"><?php echo lang('files') ?></td>
-<td width="50%" class="f1"><input type="checkbox" name="subdirs" value="1"><?php echo lang('PUBLISH_WITH_SUBDIRS') ?></td>
+<td width="50%" class="f1"><input type="checkbox" name="pages" value="1" checked="checked"><?php echo lang('GLOBAL_pages') ?><br>
+                           <input type="checkbox" name="files" value="1" checked="checked"><?php echo lang('GLOBAL_files') ?></td>
+<td width="50%" class="f1"><input type="checkbox" name="subdirs" value="1"><?php echo lang('GLOBAL_PUBLISH_WITH_SUBDIRS') ?></td>
 </tr>
 
 <tr>
-<td class="act" colspan="2"><input type="submit"  class="submit" value="<?php echo lang('PUBLISH') ?>"></td>
+  <td class="help" colspan="2"><?php echo lang('GLOBAL_MUCH_TIME') ?></td>
 </tr>
+
+<tr>
+  <td class="act" colspan="2"><input type="submit"  class="submit" value="<?php echo lang('GLOBAL_PUBLISH') ?>"></td>
+</tr>
+
 
 </table>
 
