a7d4defdd6549c300a63fa14ff14ed2792e925f6
dankert <devnull@localhost>
2004-12-14 20:44:30 +0100

    Konstante statt ".png"

--- themes/default/pages/html/project/list.tpl.php
+++ themes/default/pages/html/project/list.tpl.php
@@ -18,7 +18,7 @@
       foreach( $el as $id=>$e )
       { ?>
 <tr>
-<td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $e['url'] ?>" target="cms_main"><img src="<?php echo $image_dir ?>icon_project.png" border="0" align="left"><?php echo $e['name'] ?></a></td>
+<td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $e['url'] ?>" target="cms_main"><img src="<?php echo $image_dir ?>icon_project<?php echo IMG_EXT ?>" border="0" align="left"><?php echo $e['name'] ?></a></td>
 </tr>
 <?php } ?>
 
--- themes/default/pages/html/project/select.tpl.php
+++ themes/default/pages/html/project/select.tpl.php
@@ -13,7 +13,7 @@
       foreach( $el as $id=>$e )
       { ?>
 <tr>
-<td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $e['url'] ?>" target="_top"><img src="<?php echo $image_dir ?>icon_project.png" border="0" align="left"><?php echo $e['name'] ?></a></td>
+<td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $e['url'] ?>" target="_top"><img src="<?php echo $image_dir ?>icon_project<?php echo IMG_EXT ?>" border="0" align="left"><?php echo $e['name'] ?></a></td>
 </tr>
 <?php } ?>
 
