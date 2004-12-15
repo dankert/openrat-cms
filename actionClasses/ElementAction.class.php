7e2dd3d3e9477db74bf8d38823cd70c8877a90f1
dankert <devnull@localhost>
2004-12-16 00:22:37 +0100

    *** empty log message ***

--- actionClasses/ElementAction.class.php
+++ actionClasses/ElementAction.class.php
@@ -20,7 +20,10 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ---------------------------------------------------------------------------
 // $Log$
-// Revision 1.5  2004-10-06 09:54:43  dankert
+// Revision 1.6  2004-12-15 23:22:37  dankert
+// *** empty log message ***
+//
+// Revision 1.5  2004/10/06 09:54:43  dankert
 // Neuer Elementtyp: dynamic
 //
 // Revision 1.4  2004/07/07 20:43:57  dankert
@@ -46,7 +49,7 @@
 class ElementAction extends Action
 {
 	var $element;
-
+	var $defaultSubAction = 'edit';
 
 	/**
 	 * Konstruktor
