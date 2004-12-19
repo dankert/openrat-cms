a79d8e820f8c657cbda67341792020317806deba
dankert <devnull@localhost>
2004-12-19 16:17:31 +0100

    URLs angepasst

--- actionClasses/TreemenuAction.class.php
+++ actionClasses/TreemenuAction.class.php
@@ -1,7 +1,6 @@
 <?php
-// ---------------------------------------------------------------------------
 // $Id$
-// ---------------------------------------------------------------------------
+
 // OpenRat Content Management System
 // Copyright (C) 2002 Jan Dankert, jandankert@jandankert.de
 //
@@ -18,30 +17,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-// ---------------------------------------------------------------------------
-// $Log$
-// Revision 1.7  2004-11-28 18:23:55  dankert
-// Anpassen an neue Sprachdatei-Konventionen
-//
-// Revision 1.6  2004/11/27 13:09:17  dankert
-// Ermitteln und Anzeige von Sprache,Modell
-//
-// Revision 1.5  2004/11/10 22:41:36  dankert
-// Neue Ermittlung der Projekte, Entfernen des Punktes "Bitte auswaehlen"
-//
-// Revision 1.4  2004/10/13 22:13:24  dankert
-// Auswerten der Berechtigungen
-//
-// Revision 1.3  2004/05/02 14:49:37  dankert
-// Einf?gen package-name (@package)
-//
-// Revision 1.2  2004/04/25 17:35:42  dankert
-// openall_url setzen
-//
-// Revision 1.1  2004/04/24 15:14:52  dankert
-// Initiale Version
-//
-// ---------------------------------------------------------------------------
 
 /**
  * Action-Klasse zur Darstellung des Projekt-Auswahlmenues
@@ -75,12 +50,12 @@ class TreemenuAction extends Action
 		// Ermitteln Sprache
 		$language = Session::getProjectLanguage();
 		$this->setTemplateVar('language_name',$language->name);
-		$this->setTemplateVar('language_url' ,Html::url( array('action'=>'main','callAction'=>'language','callSubaction'=>'listing') ));
+		$this->setTemplateVar('language_url' ,Html::url( 'main','language' ));
 
 		// Ermitteln Projektmodell
 		$model = Session::getProjectModel();
 		$this->setTemplateVar('projectmodel_name',$model->name);
-		$this->setTemplateVar('projectmodel_url' ,Html::url( array('action'=>'main','callAction'=>'model','callSubaction'=>'listing')));
+		$this->setTemplateVar('projectmodel_url' ,Html::url( 'main','model' ));
 
 
 		// Ausgabe des Templates
