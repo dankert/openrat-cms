81b8b8653f695ed4479c6a2a23aa20b615215ad6
dankert <devnull@localhost>
2004-12-19 16:25:43 +0100

    Korrektur form()

--- serviceClasses/Html.class.php
+++ serviceClasses/Html.class.php
@@ -9,6 +9,14 @@
  */
 class Html
 {
+	/**
+	 * Erzeugt eine HTML-Dropdown-Box
+	 *
+	 * @param Name des Feldes
+	 * @param Inhalte als assoziatives Array
+	 * @param Vorbelegter Inhalt
+	 * @param Weitere Parameter
+	 */
 	function selectBox( $name,$values,$default='',$params=Array() )
 	{
 		$src = '<select size="1" name="'.$name.'"';
@@ -29,6 +37,15 @@ class Html
 	}
 
 
+
+	/**
+	 * Erzeugt eine HTML-Checkbox zum Ankreuzen
+	 *
+	 * @param Name des Feldes
+	 * @param boolean, ob Feld angekreuzt ist (default=nein)
+	 * @param boolean, ob Feld offen ist      (default=ja)
+	 * @param Weitere Parameter
+	 */
 	function checkBox( $name,$value=false,$writable=true,$params=Array() )
 	{
 		$src = '<input type="checkbox" name="'.$name.'"';
@@ -48,6 +65,14 @@ class Html
 	}
 
 
+	/**
+	 * Erzeugt eine relative Url innerhalb von Openrat
+	 *
+	 * @param Aktion, die aufgerufen werden soll
+	 * @param Unteraktion, die innerhalb der Aktion aufgerufen werden soll
+	 * @param Id fuer diesen Aufruf
+	 * @param Weitere beliebige Parameter
+	 */
 	function url( $action,$subaction='',$id='-',$params=array() )
 	{
 		global $conf;
@@ -76,13 +101,17 @@ class Html
 			$params['old']='true';
 		}
 
+		// Session-Id ergaenzen
+		if	( $conf['interface']['url_sessionid'] )
+			$params[ session_name() ] = session_id();
+
 		$fake_urls = $conf['interface']['nice_urls'];
 		
 		if	( isset($params['objectid']) && !isset($params['id']) )
 			$params['id'] = $params['objectid']; 
 
 		if	( $fake_urls )
-		{		
+		{
 			if	( $id != '' )
 				$id = '.'.$id;
 		}
@@ -114,29 +143,51 @@ class Html
 	}
 
 
-	function form( $params )
+
+	/**
+	  * Erstellt den Beginn eines HTML-Formulares
+	  *
+	  * @param Parameter
+	  */
+	function form( $action,$subaction='',$id='-',$params=array())
 	{
+		global $conf;
 		extract( $params );
 
-		if	( !isset($target) )  $target = '_self';
-		if	( !isset($method) )  $method = 'post';
-		if	( !isset($name  ) )  $name   = '';
+		if	( !isset($target  ) )  $target  = '_self';
+		if	( !isset($method  ) )  $method  = 'post';
+		if	( !isset($name    ) )  $name    = '';
+		if	( !isset($enctype ) )  $enctype = '';
 
 		unset( $params['name'     ]);
 		unset( $params['method'   ]);
 		unset( $params['target'   ]);
-		$action = Html::url($params);
+		unset( $params['enctype'  ]);
 
-		if	( !isset($params['subaction'])) $params['subaction'] = '';
-		if	( !isset($params['id'       ])) $params['id'       ] = '';
+		$url = Html::url( $action,$subaction,$id );
 
-		$text = '<form name="'.$name.'" target="'.$target.'" action="'.$action.'" method="'.$method.'" />'."\n";
-		$text.= '<input type="hidden" name="'.session_name().'" value="'.session_id().'" />'."\n";
+		$text = '<form name="'.$name.'" target="'.$target.'" action="'.$url.'" method="'.$method.'" enctype="'.$enctype.'" />'."\n";
+
+		$text.= '<input type="hidden" name="'.REQ_PARAM_ACTION.'" value="'.$action.'" />'."\n";
+		$text.= '<input type="hidden" name="'.REQ_PARAM_SUBACTION.'" value="'.$subaction.'" />'."\n";
+		$text.= '<input type="hidden" name="'.REQ_PARAM_ID.'" value="'.$id.'" />'."\n";
+
+		if	( $conf['interface']['url_sessionid'] )
+			$text.= '<input type="hidden" name="'.session_name().'" value="'.session_id().'" />'."\n";
+
+		foreach( $params as $paramName=>$paramValue )
+			$text.= '<input type="hidden" name="'.$paramName.'" value="'.$paramValue.'" />'."\n";
 
 		return $text;
 	}
 
 
+
+	/**
+	 * Setzt den Cursor beim Laden der Seite in ein definiertes Formularfeld
+	 *
+	 * @param Name des Feldes
+	 */ 
 	function focusField( $name )
 	{
 		echo '<script name="JavaScript" type="text/javascript"><!--'."\n";
