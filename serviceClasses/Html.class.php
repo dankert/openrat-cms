ad4be28244fb3bbb069ed1ae4916a380057020fd
dankert <devnull@localhost>
2004-12-20 23:03:57 +0100

    neue Methode printUser()

--- serviceClasses/Html.class.php
+++ serviceClasses/Html.class.php
@@ -197,5 +197,19 @@ class Html
 		echo 'document.forms[0].'.$name.'.focus();'."\n";
 		echo '//--></script>'."\n";
 	}
+	
+	
+	function printUser( $user )
+	{
+		if	( empty($user->name) )
+			$user->name = lang('GLOBAL_UNKNOWN');
+		if	( empty($user->fullname) )
+			$user->fullname = lang('GLOBAL_NO_DESCRIPTION_AVAILABLE');
+
+		if	( !empty($user->mail) )
+			echo '<a href="mailto:'.$user->mail.'" title="'.$user->fullname.'">'.$user->name.'</a>';
+		else
+			echo '<span title="'.$user->fullname.'"'.$user->name.'</span>';
+	}
 }
 ?>
