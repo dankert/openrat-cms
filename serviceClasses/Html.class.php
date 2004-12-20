047adfb9277e72818f19fc6c20bbfa26ba35c443
dankert <devnull@localhost>
2004-12-20 23:41:54 +0100

    Korrektur in printuser()

--- serviceClasses/Html.class.php
+++ serviceClasses/Html.class.php
@@ -209,7 +209,7 @@ class Html
 		if	( !empty($user->mail) )
 			echo '<a href="mailto:'.$user->mail.'" title="'.$user->fullname.'">'.$user->name.'</a>';
 		else
-			echo '<span title="'.$user->fullname.'"'.$user->name.'</span>';
+			echo '<span title="'.$user->fullname.'">'.$user->name.'</span>';
 	}
 }
 ?>
