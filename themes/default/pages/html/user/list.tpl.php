e0d4dd5e9ca28c68d2ee95fab81fda5a0dce4ea2
dankert <devnull@localhost>
2004-12-21 00:35:35 +0100

    Anzeige der Namen

--- themes/default/pages/html/user/list.tpl.php
+++ themes/default/pages/html/user/list.tpl.php
@@ -6,22 +6,24 @@
 
 <?php echo Html::form('user','add') ?>
 
-<table class="main" width="50%" cellspacing="0" cellpadding="4">
+<table class="main" width="80%" cellspacing="0" cellpadding="4">
 
 <tr>
-  <th><?php echo lang('GLOBAL_USERS') ?></th>
+  <th colspan="2"><?php echo lang('GLOBAL_USERS') ?></th>
 </tr>
 
 <?php $f1=true;
-      foreach( $el as $id=>$e )
-      { ?>
+      foreach( $el as $u )
+      {
+      	extract($u) ?>
 <tr>
-<td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $e['url'] ?>" target="cms_main"><img src="<?php echo $image_dir ?>icon_user<?php echo IMG_EXT ?>" border="0" align="left"><?php echo $e['name'] ?></a></td>
+  <td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><a href="<?php echo $url ?>" target="cms_main" title="<?php echo $desc ?>"><img src="<?php echo $image_dir ?>icon_user<?php echo IMG_EXT ?>" border="0" align="left"><?php echo $name ?></a></td>
+  <td class="<?php if($f1==true) {echo'f1';          } else{echo'f2';         }?>"><?php echo $fullname ?> <?php if($isAdmin) echo '('.lang('USER_ADMIN').')' ?></td>
 </tr>
 <?php } ?>
 
 <tr>
-<td class="act"><input type="text" name="name" value="">&nbsp;<input type="submit" class="submit" value="<?php echo lang('GLOBAL_ADD') ?>"></td>
+<td class="act" colspan="2"><input type="text" name="name" value="">&nbsp;<input type="submit" class="submit" value="<?php echo lang('GLOBAL_ADD') ?>"></td>
 </tr>
 
 </table>
