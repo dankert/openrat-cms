f7dc80901df71b6f2cd2fc4fcb7d5f7016115262
dankert <devnull@localhost>
2004-12-25 23:48:45 +0100

    Hoehe Menue auf 69 erhoeht

--- themes/default/pages/html/frameset.tpl.php
+++ themes/default/pages/html/frameset.tpl.php
@@ -9,7 +9,7 @@
 <frameset rows="20,*" border="0" frameborder="5" framespacing="0" bordercolor="#000000">
      <frame src="<?php echo $frame_src_title ?>" name="cms_title" marginheight="0" marginwidth="0" scrolling="no">
      <frameset cols="<?php echo $tree_width ?>,*" border="0" frameborder="0" framespacing="0" bordercolor="grey">
-          <frameset rows="65,*"  border="0" frameborder="0" framespacing="0" bordercolor="#000000">
+          <frameset rows="69,*"  border="0" frameborder="0" framespacing="0" bordercolor="#000000">
                <frame src="<?php echo $frame_src_treemenu ?>" name="cms_treemenu" marginheight="0" marginwidth="0" scrolling="no">
 
                <frame src="<?php echo $frame_src_tree ?>" name="cms_tree" marginheight="0" marginwidth="0" scrolling="auto">
