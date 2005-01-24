50f1f1b4ea30183f0a03af922c1dae677268c04b
dankert <devnull@localhost>
2005-01-24 22:35:42 +0100

    Abfrage auf "nologin" und "nopublish"

--- themes/default/pages/html/login.tpl.php
+++ themes/default/pages/html/login.tpl.php
@@ -25,9 +25,19 @@
 </tr>
 <?php } ?>
 
-<?php if ( $readonly ) { ?>
+<?php if ( $nologin ) { ?>
+<tr>
+  <td colspan="2" class="help"><?php echo lang('LOGIN_NOLOGIN_DESC') ?></td>
+</tr>
+<?php }
+      elseif ( $readonly ) { ?>
 <tr>
   <td colspan="2" class="help"><?php echo lang('GLOBAL_READONLY_DESC') ?></td>
+</tr>
+<?php }
+      elseif ( $nopublish ) { ?>
+<tr>
+  <td colspan="2" class="help"><?php echo lang('GLOBAL_NOPUBLISH_DESC') ?></td>
 </tr>
 <?php } ?>
 
@@ -38,12 +48,14 @@
 </tr>
 <?php } ?>
 
+<?php if ( !$nologin ) { ?>
+
 <tr>
   <td class="f1" width="50%"><?php echo lang('USER_USERNAME') ?></td>
-  <td class="f1" width="50%"><input name="login_name"     type="text"     value="" width="25"></td>
+  <td class="f1" width="50%"><input name="login_name"     type="text"     value="" width="25" /></td>
 <tr>
   <td class="f2" width="50%"><?php echo lang('USER_PASSWORD') ?></td>
-  <td class="f2" width="50%"><input name="login_password" type="password" value="" width="25"></td>
+  <td class="f2" width="50%"><input name="login_password" type="password" value="" width="25" /></td>
 <tr>
 
 <?php if (count($dbids)>1)
@@ -55,15 +67,16 @@
 <?php } ?>
 
 <tr>
-<td class="act" colspan="2">
+  <td class="act" colspan="2">
 <?php if (count($dbids)==1)
       { ?>
-<input type="hidden" name="dbid" value="<?php echo key($dbids) ?>">
+  <input type="hidden" name="dbid" value="<?php echo key($dbids) ?>" />
+<?php } ?>
+  <input type="submit" class="submit" value="<?php echo lang('GLOBAL_LOGIN') ?>" />
+  </td>
+</tr>
 <?php } ?>
 
-<input type="submit" class="submit" value="<?php echo lang('GLOBAL_LOGIN') ?>">
-</td>
-</tr>
 </table>
 
 </form>
