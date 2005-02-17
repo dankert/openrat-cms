57ff722b2fe657081b2046a9ff98b1c6f67e282e
dankert <devnull@localhost>
2005-02-17 20:19:28 +0100

    Titel anzeigen

--- themes/default/pages/html/title.tpl.php
+++ themes/default/pages/html/title.tpl.php
@@ -4,8 +4,9 @@
 
 <table cellpadding="5" cellspacing="0" width="100%">
   <tr>
-    <td class="title"><span title="<?php echo lang('USER_LOGINAS') ?>: <?php echo $username ?>"><?php echo $userfullname ?></span> <strong>@</strong> <span title="<?php echo lang('GLOBAL_DATABASE') ?>: <?php echo $dbid ?>"><?php echo $dbname ?></span></td>
-    <td class="title" style="text-align:right;"><a href="<?php echo $profile_url ?>" title="<?php echo lang('USER_PROFILE_DESC'); ?>" target="cms_main_main"><?php echo lang('USER_YOURPROFILE'); ?></a>&nbsp;|&nbsp;<a href="<?php echo $logout_url ?>" title="<?php echo lang('USER_LOGOUT_DESC'); ?>" target="_top"><?php echo lang('USER_LOGOUT'); ?></a></td>
+    <td class="title" width="40%"><span title="<?php echo lang('USER_LOGINAS') ?>: <?php echo $username ?>"><?php echo $userfullname ?></span> (<span title="<?php echo lang('GLOBAL_DATABASE') ?>: <?php echo $dbid ?>"><?php echo $dbname ?></span>)&nbsp;|&nbsp;<a href="<?php echo $showtree_url ?>" target="_parent"><?php echo $showtree_text ?></a></td>
+    <td class="title" width="20%" style="text-align:center;"><?php echo $cms_title ?></td>
+    <td class="title" width="40%" style="text-align:right;"><a href="<?php echo $profile_url ?>" title="<?php echo lang('USER_PROFILE_DESC'); ?>" target="cms_main_main"><?php echo lang('USER_YOURPROFILE'); ?></a>&nbsp;|&nbsp;<a href="<?php echo $logout_url ?>" title="<?php echo lang('USER_LOGOUT_DESC'); ?>" target="_top"><?php echo lang('USER_LOGOUT'); ?></a></td>
   </tr>
 </table>
 
