619daeb861df7dedabc81ddf2fa2b2f8ee3a95b3
dankert <devnull@localhost>
2005-02-17 22:22:22 +0100

    Weitere Funktionen f?r HTML und BB-Code

--- serviceClasses/Text.class.php
+++ serviceClasses/Text.class.php
@@ -20,7 +20,10 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // ---------------------------------------------------------------------------
 // $Log$
-// Revision 1.2  2004-05-02 15:04:16  dankert
+// Revision 1.3  2005-02-17 21:22:22  dankert
+// Weitere Funktionen f?r HTML und BB-Code
+//
+// Revision 1.2  2004/05/02 15:04:16  dankert
 // Einfügen package-name (@package)
 //
 // Revision 1.1  2004/04/24 17:03:28  dankert
@@ -37,17 +40,88 @@
  */
 class Text
 {
-	// Einen Text auf eine maximale Laenge beschränken.
-	//
-	// Falls Text zu lang ist wird mit '...' abgekürzt
-	//
+	/**
+	 * Alias fuer Methode maxLength()
+	 *
+	 * @deprecated use maxlength() !
+	 */
 	function maxLaenge( $laenge,$text )
 	{
-		if   ( strlen($text) > $laenge )
-			$text = substr($text,0,$laenge).'...';
+		return Text::maxLength($text,$laenge);
+	}
+
+
+	/**
+	 * Einen Text auf eine bestimmte Laenge begrenzen.
+	 *
+	 * Ist der Text zu lang, so wird abgeschnitten und
+	 * eine Zeichenkette angehaengt.
+	 *
+	 * @param String Text, der zu begrenzen ist
+	 * @param Integer maximale Laenge des Textes (optional)
+	 * @param Text, der an gekuerzten Text angehangen wird (optional)
+	 */
+	function maxLength( $text,$laenge=20,$append='...' )
+	{
+		if	( strlen($text) > $laenge )
+			$text = substr($text,0,$laenge).$append;
 
 		return $text;
 	}
+
+
+	/**
+	 * Umwandeln von BB-Code in Wiki-Textauszeichnungen
+	 *
+	 * @param text zu bearbeitender Text
+	 *
+	 * @return String Ausgabe
+	 */
+	function bbCode2Wiki( $inhalt )
+	{
+		$inhalt = eregi_replace('\[b\]([^\[]*)\[\/b\]'       , '*\\1*' ,$inhalt);
+		$inhalt = eregi_replace('\[i\]([^\[]*)\[\/i\]'       , '_\\1_' ,$inhalt);
+		$inhalt = eregi_replace('\[code\]([^\[]*)\[\/code\]' , '=\\1=' ,$inhalt);
+
+		$inhalt = eregi_replace('\[url\]([^\[]*)[\/url\]'          ,'"\\1"->"\\1"' ,$inhalt);
+		$inhalt = eregi_replace('\[url=([^\[]*)\]([^\[]*)\[\/url\]','"\\2"->"\\1"' ,$inhalt);
+
+		return $inhalt;
+	}
+
+
+	/**
+	 * Umwandeln von einfachen HTML-Befehlen in Wiki-Textauszeichnungen
+	 *
+	 * @param text zu bearbeitender Text
+	 *
+	 * @return String Ausgabe
+	 */
+	function Html2Wiki( $inhalt )
+	{
+		$inhalt = eregi_replace('<b(.*)>(.*)</b>','*\\2*' ,$inhalt);
+		$inhalt = eregi_replace('<i(.*)>(.*)</i>','_\\2_' ,$inhalt);
+		$inhalt = eregi_replace('<a(.*)href="(.*)">(.*)</a>','"\\3"->"\\2"' ,$inhalt);
+
+		return $inhalt;
+	}
+
+
+	/**
+	 * HTML-Entitaeten fuer HTML-Tags verwenden
+	 *
+	 * @param String Text, in dem HTML-Tags umgewandelt werden sollen
+	 * @return String Ausgabe
+	 */
+	function encodeHtml( $inhalt )
+	{
+		$inhalt = str_replace('&','&amp;',$inhalt);
+		$inhalt = str_replace('<','&lt;' ,$inhalt);
+		$inhalt = str_replace('>','&gt;' ,$inhalt);
+
+		return $inhalt;
+	}
+
 }
 
  
