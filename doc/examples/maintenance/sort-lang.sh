dc788cd7f7806d4f10bdd79a0761fa8754050716
dankert <devnull@localhost>
2005-11-02 22:13:40 +0100

    Nur auf stdout schreiben.

--- doc/examples/maintenance/sort-lang.sh
+++ doc/examples/maintenance/sort-lang.sh
@@ -6,19 +6,17 @@ if [ ! -f $1 ]; then
 	echo "usage: $0 <filename>"
 	echo ""
 	echo "This script is sorting a Openrat language file in alphabetic order."
-	echo "Just give it the filename of the language file."
+	echo "Just give it the filename of the language file and it puts the sorted file on stdout."
 	echo ""
 	exit
 fi
 
-if [ ! -w $1 ]; then
-	echo "$1 is not writable"
+if [ ! -r $1 ]; then
+	echo "$1 is not readable"
 	exit
 fi
 
-cp $1 $1.orig
-
-cat $1.orig|grep "^;" > $1
-cat $1.orig|grep "^;" -v|sort >> $1
+cat $1|grep "^;"
+cat $1|grep "^;" -v|sort
 
 
