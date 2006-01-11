bb43dd1696011d7817bd83f432ed47f9f9df22b8
dankert <devnull@localhost>
2006-01-11 23:36:40 +0100

    Neue Datei zum Einbinden aller Dateien im Ordner

---
+++ objectClasses/include.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once( OR_OBJECTCLASSES_DIR."Value.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Acl.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Template.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Object.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Folder.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Link.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."File.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."User.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Group.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Project.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Page.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Language.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Model.class.".PHP_EXT );
+require_once( OR_OBJECTCLASSES_DIR."Element.class.".PHP_EXT );
+
+?>
---
+++ serviceClasses/include.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once( OR_SERVICECLASSES_DIR."GlobalFunctions.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Http.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Html.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."HtmlView.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Upload.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Ftp.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Text.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Publish.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Api.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."TreeElement.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."AbstractTree.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."AdministrationTree.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."ProjectTree.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Preferences.class.".PHP_EXT );
+
+require_once( OR_SERVICECLASSES_DIR."Dynamic.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Code.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Transformer.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Line.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."TemplateEngine.class.".PHP_EXT );
+
+//require_once( OR_SERVICECLASSES_DIR."Session.class.".PHP_EXT );
+//require_once( OR_SERVICECLASSES_DIR."Sql.class.".PHP_EXT );
+
+?>
