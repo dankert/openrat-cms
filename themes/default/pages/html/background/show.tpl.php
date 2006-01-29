569aa9eb835123453d46f9fc20102197bfd6978a
dankert <devnull@localhost>
2006-01-29 19:06:10 +0100

    Seiten neu aus Vorlagen (../templates/*) generiert.

---
+++ themes/default/pages/html/background/show.tpl.php
@@ -0,0 +1,29 @@
+<?php /* source: ./themes/default/include/html/page.inc.php - compile time: Sun, 29 Jan 2006 16:10:25 +0100 */ ?><?php $attr = array('class'=>'') ?><?php $attr_class='' ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<!-- $Id$ -->
+<head>
+  <title><?php echo $cms_title ?></title>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+  <meta name="MSSmartTagsPreventParsing" content="true" />
+  <meta name="robots" content="noindex,nofollow" />
+  <link rel="stylesheet" type="text/css" href="./themes/default/css/default.css" />
+<?php if($stylesheet!='default') { ?>
+  <link rel="stylesheet" type="text/css" href="<?php echo $stylesheet ?>" />
+<?php } ?>
+</head>
+
+<body<?php echo !empty($$attr_class)?' class="'.$$attr_class.'"':' class="'.$attr_class.'"' ?>>
+
+
+<?php unset($attr) ?><?php unset($attr_class) ?><?php /* source: ./themes/default/include/html/page-end.inc.php - compile time: Sun, 29 Jan 2006 16:10:25 +0100 */ ?><?php $attr = array() ?>
+<!-- $Id$ -->
+
+<?php if ($showDuration && false) { ?>
+<br/>
+<small>&nbsp;
+<?php $dur = time()-START_TIME;
+      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small>
+<?php } ?>
+
+</body>
+</html><?php unset($attr) ?>
