86f41f56c5bd34057b7f9f984d306aaec98e087a
dankert <devnull@localhost>
2006-01-29 18:28:52 +0100

    Feld auch selektieren

--- themes/default/include/html/focus.inc.php
+++ themes/default/include/html/focus.inc.php
@@ -1,3 +1,4 @@
 <script name="JavaScript" type="text/javascript"><!--
-document.forms[0].<?php echo $$attr_field ?>.focus();
+document.forms[0].<?php echo $attr_field ?>.focus();
+document.forms[0].<?php echo $attr_field ?>.select();
 //--></script>
