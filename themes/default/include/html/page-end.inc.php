021c3858456742ddfb7c56a10e2ee2977f16a707
dankert <devnull@localhost>
2006-01-29 18:30:59 +0100

    Anzeige Laufzeit von "page-end" nach "window-end" verschoben

--- themes/default/include/html/page-end.inc.php
+++ themes/default/include/html/page-end.inc.php
@@ -5,7 +5,7 @@
 <br/>
 <small>&nbsp;
 <?php $dur = time()-START_TIME;
-      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small>
+//      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small>
 <?php } ?>
 
 </body>
--- themes/default/include/html/window-end.inc.php
+++ themes/default/include/html/window-end.inc.php
@@ -4,3 +4,11 @@
 </table>
 
 </center>
+
+<?php if ($showDuration)
+      { ?>
+<br/>
+<small>&nbsp;
+<?php $dur = time()-START_TIME;
+      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small>
+<?php } ?>
