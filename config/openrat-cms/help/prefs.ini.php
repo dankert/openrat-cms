b69937b41832718ec02103eae19af368eeeff918
dankert <devnull@localhost>
2006-02-05 12:30:41 +0100

    Einstellungen f?r Online-Hilfe

---
+++ config/openrat-cms/help/prefs.ini.php
@@ -0,0 +1,11 @@
+; <?php exit('direct access denied') ?>
+
+; Enable online help
+enabled=true
+
+; URL to the documents 
+url=doc/online/html/
+; url=http://help.openrat.de/
+
+; file extension of the help documents
+suffix=.html
