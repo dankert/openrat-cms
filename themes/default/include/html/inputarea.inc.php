9763ad41b2c3a071eab6423da9b85f07c6b59492
dankert <devnull@localhost>
2006-07-19 22:25:33 +0200

    Attribut "class" erg?nzt.

--- themes/default/include/html/inputarea.inc.php
+++ themes/default/include/html/inputarea.inc.php
@@ -1,1 +1,1 @@
-<textarea name="<?php echo $attr_name ?>" rows="<?php echo $attr_rows ?>" cols="<?php echo $attr_cols ?>"><?php echo htmlentities(isset($$attr_name)?$$attr_name:$attr_default) ?></textarea>
+<textarea class="<?php echo $attr_class ?>" name="<?php echo $attr_name ?>" rows="<?php echo $attr_rows ?>" cols="<?php echo $attr_cols ?>"><?php echo htmlentities(isset($$attr_name)?$$attr_name:$attr_default) ?></textarea>
