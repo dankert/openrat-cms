8cd41e6393885e6a81dc9b901e5ca98401969fd8
dankert <devnull@localhost>
2006-07-27 22:36:02 +0200

    Alternativ-Frameset als Kommentar.

--- themes/default/templates/index/show.tpl.src.php
+++ themes/default/templates/index/show.tpl.src.php
@@ -23,3 +23,17 @@ frameset-page
 			frame file:frame_src_background
 		frame file:frame_src_background
 				
+//	frameset rows:20,5,*,5
+//		frame file:frame_src_title name:cms_title
+//		frame file:frame_src_background
+//
+//		frameset columns:5,25%,5,*,5
+//			frame file:frame_src_background
+//			frameset rows:54,*
+//				frame file:frame_src_tree_title name:cms_treemenu
+//				frame file:frame_src_tree name:cms_tree scrolling:auto
+//			frame file:frame_src_background
+//			frame file:frame_src_main name:cms_main
+//			frame file:frame_src_background
+//		frame file:frame_src_background
+				
