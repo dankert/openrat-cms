f239e07a33a7e6087a852f44298a06009961cbb2
dankert <devnull@localhost>
2006-08-08 22:09:53 +0200

    Masken f?r Im- und Export

---
+++ themes/default/templates/pageelement/exportlongtext.tpl.src.php
@@ -0,0 +1,12 @@
+page
+	form
+		window name:export columnclasses:a
+			row
+				cell 
+					selectbox list:types name:type
+			
+			row
+				cell 
+					button type:ok
+
+	focus field:text
---
+++ themes/default/templates/pageelement/importlongtext.tpl.src.php
@@ -0,0 +1,16 @@
+page
+	form
+		window name:import columnclasses:a
+			row
+				cell 
+					selectbox list:types name:type
+			
+			row
+				cell 
+					upload name:file
+			
+			row
+				cell 
+					button type:ok
+
+	focus field:text
