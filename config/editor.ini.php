070f5a349dd4b41261cc29222f595492a4de6ee4
dankert <devnull@localhost>
2006-11-26 23:01:41 +0100

    Datei "text-markup.ini.php" umbenannt in "editor.ini.php".

--- config/editor.ini.php
+++ config/editor.ini.php
@@ -1,5 +1,7 @@
 ; <?php exit('direct access denied') ?>
 
+[text-markup]
+
 strong-begin = *
 strong-end   = *
 
@@ -27,11 +29,36 @@ remove-end   = --
 definition-sep = ":"
 headline       = "+"
 
+headline_level1_underline = "="
+headline_level2_underline = "-"
+headline_level3_underline = "."
+
 list-unnumbered = "-"
 list-numbered   = "#"
 
-linkto="->"
-table-cell-sep="|"
+table-of-content= "##TOC##"
+linkto          = "->"
+table-cell-sep  = "|"
 
 style-begin = "'"
 style-end   = "'"
+
+quote            = >
+quote-line-begin = >
+quote-line-end   = >
+
+
+[html]
+
+tag_teletype=tt
+tag_emphatic=em
+tag_strong_open="strong
+tag_speech_="cite"
+speech_open="&bdquo;"
+speech_close="&rdquo;"
+
+
+[wiki]
+
+convert_html=true
+convert_bbcode=true
