36acaa3b9c9765359d3e703efc31a9e0c6046dda
dankert <devnull@localhost>
2006-12-09 17:53:03 +0100

    Neue Einstellung, u.a. "html/rendermode"

--- config/editor.ini.php
+++ config/editor.ini.php
@@ -1,48 +1,74 @@
 ; <?php exit('direct access denied') ?>
 
+; Settings for Text Formatting Rules
 [text-markup]
 
+; Strong/important text (mostly "bold")
 strong-begin = *
 strong-end   = *
 
+; Emphatic text (mostly "italic")
 emphatic-begin = _
 emphatic-end   = _
 
+; Image
 image-begin = "{"
 image-end   = "}"
 
-speech-begin = "\""
-speech-end   = "\""
+; Speech
+speech-begin = QUOTE
+speech-end   = QUOTE
 
+; text with same width
 code-begin = "="
 code-end   = "="
 
+; pre-formatted Text
 pre-begin = "="
 pre-end   = "="
 
+; Inserted Text
 insert-begin = ++
 insert-end   = ++
 
+; Removed text
 remove-begin = --
 remove-end   = --
 
-definition-sep = ":"
+; Separator for a definition item
+definition-sep = "::"
+
+; Indenting headline
 headline       = "+"
 
+; Underlining of headline level 1
 headline_level1_underline = "="
+
+; Underlining of headline level 2
 headline_level2_underline = "-"
+
+; Underlining of headline level 3
 headline_level3_underline = "."
 
+; Unnumbered Listentry
 list-unnumbered = "-"
+
+; Numbered Listentry
 list-numbered   = "#"
 
+; Table of content
 table-of-content= "##TOC##"
+
+; Link to
 linkto          = "->"
+
+; Table cell separator
 table-cell-sep  = "|"
 
 style-begin = "'"
 style-end   = "'"
 
+; Quote Text
 quote            = >
 quote-line-begin = >
 quote-line-end   = >
@@ -50,13 +76,29 @@ quote-line-end   = >
 
 [html]
 
-tag_teletype=tt
-tag_emphatic=em
-tag_strong_open="strong
-tag_speech_="cite"
-speech_open="&bdquo;"
-speech_close="&rdquo;"
+; Which HTML-Tag to use for cites
+tag_strong_open="strong""
 
+; Which HTML-Tag to use for emphatic text
+tag_emphatic=em
+
+; Which HTML-Tag to use for teletyped text
+tag_teletype=tt
+
+; Which HTML-Tag to use for cites
+tag_speech_="cite"
+
+; OpenRat tries to use a good speech tag. You may override this. 
+override_speech=false
+override_speech_open=&laquo;
+override_speech_close=&raquo;
+
+; HTML-Rendermode
+; explains how to handle emtpy elements.
+; 'xml'  => <br />, <image src="..." />
+; 'sgml' => <br>, <image src="...">
+rendermode=sgml
+;rendermode=xml
 
 [wiki]
 
