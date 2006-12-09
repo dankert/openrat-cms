f3327e29f98bf05e5091efb3628499acafb55a12
dankert <devnull@localhost>
2006-12-09 02:25:11 +0100

    Entfernen nicht notwendiger Codeteile.

--- serviceClasses/Preferences.class.php
+++ serviceClasses/Preferences.class.php
@@ -9,49 +9,10 @@
  */
 class Preferences
 {
-	/*
-
-	function loadOLDxxxxxxxxxxxxxxx( $dir='' )
-	{
-		$values = array();
-		
-		if	( empty($dir) )
-			$dir = OR_PREFERENCES_DIR;
-			
-		if (is_dir($dir))
-		{
-			if	( $dh = opendir($dir) )
-			{
-				while( ($file = readdir($dh)) !== false )
-				{
-					$file = basename($file);
-//					if	( substr($file,0,1) != '.' && $file != 'CVS' && is_dir($dir.$file) && is_file($dir.$file.'/prefs.ini.php') )
-					if	( substr($file,0,1) != '.' && is_file($dir.$file.'/prefs.ini.php') )
-					{
-						$values[$file] = $this->load($dir.$file.'/');
-					}
-		        }
-		        closedir($dh);
-		    }
-
-		    if	( !is_file($dir.'prefs.ini.php') )
-				die( 'file not found: '.$dir.'prefs.ini.php');
-
-		    $values = $values + parse_ini_file( $dir.'prefs.ini.php' );
-			ksort($values);
-			
-			return $values;
-		}
-		else die('not a folder: '.$dir);
-	}
-	
-	*/
-	
-	
 	
 	function load( $dir='' )
 	{
-//		echo "x:$dir<br>";
+		
 		$values = array();
 		
 		// Bei erstem (nicht-rekursiven) Aufruf der Methoden das Konfigurationsverzeichnis voreinstellen 
@@ -67,35 +28,19 @@ class Preferences
 			{
 				$filename = $dir.$verzEintrag;
 				
-//				echo "....-$filename-....<br>";
-//				if	( is_dir($filename) && substr($verzEintrag,0,1)!='.' && substr($verzEintrag,0,3)!='CVS' )
-//				{
-////					echo " ist ein DIR<br>";
-//					if	( !isset($values[$verzEintrag]) )
-//						$values[$verzEintrag] = array();
-//					$values[$verzEintrag] += $this->load($filename.'/');
-//				}
-//else
 				if	( is_file($filename) && eregi('\.(ini.*|conf)$',$verzEintrag) )
 				{
-//					echo " ist ein FILE<br>";
 					$nameBestandteile = explode('.',$verzEintrag);
-//					if	( !isset($values[$nameBestandteile[0]]) )
-//						$values[$nameBestandteile[0]] = array();
 					
 				    $values[$nameBestandteile[0]] = parse_ini_file( $filename,true );
 				}
-//				else
-//					echo " ist GARNIX<br>";
 	        }
 	        closedir($dh);
 	    }
 
 		ksort($values);
 
-//echo"<pre>";
-//print_r($values);		
-//echo"</pre>";
+//		Html::debug($values,"Konfiguration");
 		return $values;
 	}
 }
