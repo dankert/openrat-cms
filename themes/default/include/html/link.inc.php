68e12634049c585083f3988a59dfaad097d7b239
dankert <devnull@localhost>
2007-01-20 16:15:36 +0100

    URLs ausgeben.

--- themes/default/include/html/link.inc.php
+++ themes/default/include/html/link.inc.php
@@ -1,5 +1,6 @@
 <?php
-	if	(!empty($attr_action))
-	{
+	if(!empty($attr_url))
+		$tmp_url = $attr_url;
+	else
 		$tmp_url = Html::url($attr_action,$attr_subaction,!empty($$attr_id)?$$attr_id:$this->getRequestId(),array(!empty($var1)?$var1:'asdf'=>!empty($value1)?$$value1:''));
-?><a href="<?php echo $tmp_url ?>" class="<?php echo $attr_class ?>" target="<?php echo $attr_target ?>" title="<?php echo hasLang($attr_title)?lang($attr_title):lang($$attr_title) ?>"><?php } ?>
+?><a href="<?php echo $tmp_url ?>" class="<?php echo $attr_class ?>" target="<?php echo $attr_target ?>" title="<?php echo $attr_title ?>">
