f1d784972e27c2be6bbfd47f1defebe3a71142e2
dankert <devnull@localhost>
2007-01-20 16:14:13 +0100

    Neue Attribute f?r div. Elemente.

--- themes/default/include/elements.ini.php
+++ themes/default/include/elements.ini.php
@@ -3,37 +3,38 @@
 ; default-value could be nothing (blank), a string or "*" for required attributes
 
 
-button   = type:*
+button   = type:submit,class:ok,value:ok,text:button_ok
 cell     = width,style,class,colspan
 char     = type:*
 checkbox = default:false,readonly:false,name:*
 date     = date
 dummy    =
 focus    = field:*
-form     = action,subaction,id,name,target:_self,method:post,enctype:application/x-www-form-urlencoded
+form     = action,subaction,id,name:,target:_self,method:post,enctype:application/x-www-form-urlencoded
 frame    = file,name,scrolling
 frameset = rows,columns
 frameset-page=
 hidden   = name:*,default
+else     =
 if       = var,value,invert,empty,present,contains,true,false
 image    = config,file,url,icon,align:left,type,elementtype
-input    = class,default,type:text,index,name:*,prefix,value,size:40,maxlength:256,onchange
+input    = class:,default:,type:text,index,name:*,prefix,value,size:40,maxlength:256,onchange:
 inputarea= name,rows:10,cols:40,value,index,onchange,prefix,class,default
 insert   = file:*
-link     = title,config,target,var,url,class,action,subaction,id,var1,value1
+link     = title:,config,target:_self,var,url,class:,action,subaction,id,var1,value1
 list     = list:*,extract:false,key:list_key,value:list_value
 logo     = name:*
 newline  =
 page     = class:main
-password = name:*,default,class,size:40,maxlength:256
+password = name:*,default:,class:,size:40,maxlength:256
 radio    = readonly,name:*,value,default,prefix,suffix
 raw      =
 row      = class
-selectbox=list:*,name:*,default,onchange,title,class
-listbox  =list:*,name:*,default,onchange,title,class
+selectbox=list:*,name:*,default,onchange:,title,class:
+listbox  =list:*,name:*,default,onchange:,title,class:
 set      = var:*,value:*
 table    = class,width:100%,space:0px,padding:0px,widths,rowclasses:oddCOMMAeven,columnclasses
-text     = title,class,var,text,textvar,raw,maxlength
+text     = title,class:text,var,text,key,textvar,raw,maxlength,value
 upload   = name:*,class:upload
 user     = user
-window   = title,name,icon,widths,width:93%,rowclasses:oddCOMMAeven,columnclasses
+window   = title,name,icon,widths,width:93%,rowclasses:oddCOMMAeven,columnclasses:1COMMA2COMMA3
