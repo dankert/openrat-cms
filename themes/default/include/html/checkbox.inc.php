f9d0b3ab93606b7d0c64b42073ce806bb7413cde
dankert <devnull@localhost>
2007-01-20 17:54:08 +0100

    Vereinfachung.

--- themes/default/include/html/checkbox.inc.php
+++ themes/default/include/html/checkbox.inc.php
@@ -1,10 +1,9 @@
 <?php
-	$attr_name    = !empty($$attr_name)?$$attr_name:$attr_name;
-	$attr_readonly = ( $attr_readonly == 'true' );
-	$attr_default  = ( $attr_default  == 'true' );
+	$attr_default  = ( $attr_default  == true );
 	
 	if	( isset($$attr_name) )
-		$checked = isset($$$attr_name)&& $$$attr_name==true;
+		$checked = $$attr_name == true;
+//		$checked = isset($$$attr_name)&& $$$attr_name==true;
 	else
-		$checked = $attr_default;
-?><input type="checkbox" name="<?php echo $attr_name  ?>" <?php if ($attr_readonly) echo ' disabled="disabled"' ?> value="1" <?php if( $checked ) echo 'checked="checked"' ?> />
+		$checked = $attr_default == true;
+?><input type="checkbox" name="<?php echo $attr_name  ?>" <?php if ($attr_readonly) echo ' disabled="disabled"' ?> value="1" <?php if( $checked ) echo 'checked="checked"' ?> /><?php unset($attr_name); unset($attr_readonly); unset($attr_default); ?>
