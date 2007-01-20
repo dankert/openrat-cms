e502d348ae66ff12fa0760aa3095029647e14dac
dankert <devnull@localhost>
2007-01-20 18:02:40 +0100

    Unverschl?sseltes Kennwort f?r Adminuser.

--- doc/database/sql/postgresql/create.sql
+++ doc/database/sql/postgresql/create.sql
@@ -307,4 +307,4 @@ CREATE INDEX idx_acl_05 ON or_acl (is_transmit);
 
 INSERT INTO or_user
    (id,name,password,ldap_dn,fullname,tel,mail,descr,style,is_admin)
-   VALUES( 1,'admin','21232f297a57a5a743894a0e4a801fc3','','Administrator','','','The Admin User','default',1 )
+   VALUES( 1,'admin','admin','','Administrator','','','The Admin User','default',1 )
