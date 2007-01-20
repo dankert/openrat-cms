7d638c217d314e47c4cab953df0b838ffdaab603
dankert <devnull@localhost>
2007-01-20 01:10:56 +0100

    Sektion entfernt.

--- config/mail.ini.php
+++ config/mail.ini.php
@@ -1,7 +1,7 @@
 ; <?php exit('direct access denied') ?>
 
-[mail]
-
 enabled=true                        ; Enable sending E-Mails
-from="OpenRat <user@example.com>"   ; Sender Adress
-signature="http://www.openrat.de"   ; Signature (blank if unused)
+;from="OpenRat <user@example.com>"  ; Your Sender Adress
+signature="http://www.openrat.de"   ; Signature, blank if unused, use ';' for line-breaks
+;cc=                                ; Copy recipient
+;bcc=                               ; Blind-copy recipient
