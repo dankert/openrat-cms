c0893313a21aff5290941849f998725549f07c55
dankert <devnull@localhost>
2007-01-20 18:36:26 +0100

    Verbesserung Entfernen der Variablen.

--- themes/default/include/html/if-end.inc.php
+++ themes/default/include/html/if-end.inc.php
@@ -1,13 +1,4 @@
 <?php
 	}
 	
-	unset($attr_true);
-	unset($attr_false);
-	unset($attr_notempty);
-	unset($attr_empty);
-	unset($attr_contains);
-	unset($attr_present);
-	unset($attr_invert);
-	unset($attr_value);
-	unset($attr_var);
 ?>
--- themes/default/include/html/if.inc.php
+++ themes/default/include/html/if.inc.php
@@ -71,6 +71,17 @@
 	if  ( !empty($attr_not) )
 		$exec = !$exec;
 
+	unset($attr_true);
+	unset($attr_false);
+	unset($attr_notempty);
+	unset($attr_empty);
+	unset($attr_contains);
+	unset($attr_present);
+	unset($attr_invert);
+	unset($attr_not);
+	unset($attr_value);
+	unset($attr_var);
+
 	if	( $exec )
 	{
 ?>
