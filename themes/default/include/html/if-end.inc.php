c3f1a9b0f6000fe3a925b34d8756cd3c1c86cc55
dankert <devnull@localhost>
2007-01-20 16:16:42 +0100

    Entfernen aller Variablen aus dem aktuellen G?ltigkeitsbereich, da das sonst, wenn die IF-Bedingung "false" ergibt, nicht passiert.

--- themes/default/include/html/if-end.inc.php
+++ themes/default/include/html/if-end.inc.php
@@ -1,3 +1,13 @@
 <?php
 	}
+	
+	unset($attr_true);
+	unset($attr_false);
+	unset($attr_notempty);
+	unset($attr_empty);
+	unset($attr_contains);
+	unset($attr_present);
+	unset($attr_invert);
+	unset($attr_value);
+	unset($attr_var);
 ?>
