11e89076cb702763c8e41570f3161de82bc58743
dankert <devnull@localhost>
2007-01-20 16:13:35 +0100

    Umstellung von Attributen auf neues Value-Binding

--- themes/default/templates/index/showlogin.tpl.src.php
+++ themes/default/templates/index/showlogin.tpl.src.php
@@ -1,54 +1,68 @@
 page
 	form action:index subaction:login target:_top
 		window title:GLOBAL_LOGIN name:login width:400 icon:user
+	
+			if present:config:login/logo/file
+				if false:property:mustChangePassword
+					row
+						cell colspan:2
+							if present:config:login/logo/url
+								link url:config:login/logo/url target:_top
+									image url:config:login/logo/file
+							if empty:config:login/logo/url
+								image url:config:login/logo/file
 
-			if present:logo
-				row
-					cell colspan:2
-						if present:logo_url
-							link config:login/logo_url target:_top
-								image config:login/logo
-						if empty:logo_url
-							image url:logo
-
-			if present:motd
+			if present:config:login/motd
 				row
 					cell class:motd colspan:2
-						text var:motd
+						text raw:config:login/motd
 
-			if true:nologin
+			if true:config:login/nologin
 				row
 					cell class:help colspan:2 
-						text text:LOGIN_NOLOGIN_DESC
+						text key:LOGIN_NOLOGIN_DESC
 
-			if true:readonly
+			if true:config:security/readonly
 				row
 					cell class:help colspan:2
-						text text:GLOBAL_READONLY_DESC
+						text key:GLOBAL_READONLY_DESC
 
-			if true:nopublish
+			if true:config:security/nopublish
 				row
 					cell class:help colspan:2
-						text text:GLOBAL_NOPUBLISH_DESC
+						text key:GLOBAL_NOPUBLISH_DESC
 
-			if false:nologin
+			if false:config:login/nologin
 				row
 					cell class:logo colspan:2
 						logo name:login
 				row
 					cell class:fx width:50%
-						text text:USER_USERNAME
+						text key:USER_USERNAME
 					cell class:fx width:50%
 						input type:text name:login_name value: size:25
 				row
 					cell class:fx width:50%
-						text text:USER_PASSWORD
+						text key:USER_PASSWORD
 					cell class:fx width:50%
 						password name:login_password default: size:25
 	
+				if true:property:mustChangePassword
+					row
+						cell class:fx width:50%
+							text key:USER_NEW_PASSWORD
+						cell class:fx width:50%
+							password name:password1 default: size:25
+
+					row
+						cell class:fx width:50%
+							text key:USER_NEW_PASSWORD_REPEAT
+						cell class:fx width:50%
+							password name:password2 default: size:25
+	
 				row
 					cell class:fx width:50%
-						text text:GLOBAL_DATABASE
+						text key:GLOBAL_DATABASE
 					cell class:fx width:50%
 						selectbox name:dbid list:dbids default:actdbid
 						hidden name:screenwidth default:9999
@@ -60,7 +74,7 @@ RAW
 <script name="javascript1.2" type="text/javascript">
 <!--
 document.forms[0].screenwidth.value=window.innerWidth;
-//-->
+//	-->
 </script>
 END
 
@@ -68,7 +82,7 @@ END
 
 	newline
 	newline
-	link url:http://www.gnu.org/copyleft/gpl.html target:_top
-		text text:GLOBAL_GPL
+	link url:config:login/gpl/url target:_top
+		text value:message:GLOBAL_GPL
 
 	focus field:login_name
