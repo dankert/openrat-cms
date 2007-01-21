f6b555ce5c5a5e2032158f1ef2e3a075f81b2461
dankert <devnull@localhost>
2007-01-21 23:24:09 +0100

    Valuebinding im Template ("var:")

--- themes/default/templates/index/show.tpl.src.php
+++ themes/default/templates/index/show.tpl.src.php
@@ -1,41 +1,41 @@
 frameset-page
 
 	frameset rows:23,3,*,3,5
-		frame file:frame_src_title name:cms_title
-		frame file:frame_src_border
-		#frame file:frame_src_background
+		frame file:var:frame_src_title name:cms_title
+		frame file:var:frame_src_border
+		#frame file:var:frame_src_background
 
 		frameset columns:25%,*
-			#frame file:frame_src_background
-			#frame file:frame_src_border
+			#frame file:var:frame_src_background
+			#frame file:var:frame_src_border
 			frameset rows:54,*
-				#frame file:frame_src_border
-				frame file:frame_src_tree_title name:cms_treemenu
-				frame file:frame_src_tree name:cms_tree scrolling:auto
-				//frame file:frame_src_clipboard name:cms_clipboard
-				#frame file:frame_src_border
-				//frame file:frame_src_tree_menu name:cms_treemenu
-				//frame file:frame_src_border
-			#frame file:frame_src_border
-			#frame file:frame_src_background
-			#frame file:frame_src_border
-			frame file:frame_src_main name:cms_main
-			#frame file:frame_src_border
-			#frame file:frame_src_background
-		frame file:frame_src_border
-		frame file:frame_src_background
+				#frame file:var:frame_src_border
+				frame file:var:frame_src_tree_title name:cms_treemenu
+				frame file:var:frame_src_tree name:cms_tree scrolling:auto
+				//frame file:var:frame_src_clipboard name:cms_clipboard
+				#frame file:var:frame_src_border
+				//frame file:var:frame_src_tree_menu name:cms_treemenu
+				//frame file:var:frame_src_border
+			#frame file:var:frame_src_border
+			#frame file:var:frame_src_background
+			#frame file:var:frame_src_border
+			frame file:var:frame_src_main name:cms_main
+			#frame file:var:frame_src_border
+			#frame file:var:frame_src_background
+		frame file:var:frame_src_border
+		frame file:var:frame_src_background
 				
 //	frameset rows:20,5,*,5
-//		frame file:frame_src_title name:cms_title
-//		frame file:frame_src_background
+//		frame file:var:frame_src_title name:cms_title
+//		frame file:var:frame_src_background
 //
 //		frameset columns:5,25%,5,*,5
-//			frame file:frame_src_background
+//			frame file:var:frame_src_background
 //			frameset rows:54,*
-//				frame file:frame_src_tree_title name:cms_treemenu
-//				frame file:frame_src_tree name:cms_tree scrolling:auto
-//			frame file:frame_src_background
-//			frame file:frame_src_main name:cms_main
-//			frame file:frame_src_background
-//		frame file:frame_src_background
+//				frame file:var:frame_src_tree_title name:cms_treemenu
+//				frame file:var:frame_src_tree name:cms_tree scrolling:auto
+//			frame file:var:frame_src_background
+//			frame file:var:frame_src_main name:cms_main
+//			frame file:var:frame_src_background
+//		frame file:var:frame_src_background
 				
--- themes/default/templates/main/show.tpl.src.php
+++ themes/default/templates/main/show.tpl.src.php
@@ -2,6 +2,6 @@ frameset-page
 #	frameset rows:3,54,*,3
 	frameset rows:54,*
 #		frame file:frame_src_border
-		frame file:frame_src_main_menu name:cms_main_menu
-		frame file:frame_src_main_main name:cms_main_main scrolling:auto
+		frame file:var:frame_src_main_menu name:cms_main_menu
+		frame file:var:frame_src_main_main name:cms_main_main scrolling:auto
 #		frame file:frame_src_border
