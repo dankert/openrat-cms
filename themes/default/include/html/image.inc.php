536c3279e677328d00c4cb842280aec6da2d5470
dankert <devnull@localhost>
2007-01-21 23:21:22 +0100

    Ausbau von "$$", da dies ?ber das neue Valuebinding im Template geschieht.

--- themes/default/include/html/image.inc.php
+++ themes/default/include/html/image.inc.php
@@ -1,20 +1,11 @@
 <?php
 if (isset($attr_elementtype)) {
-?><img src="<?php echo $image_dir.'icon_el_'.$$attr_elementtype.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php
-} elseif(!empty($attr_config)) {
-		global $conf;
-		$c = $conf;
-		$path = explode('/',$attr_config);
-		foreach($path as $part)
-			$c = $c[$part]; 
-		$tmp_url = $c;
-?><img src="<?php echo $tmp_url ?>" border="0" align="<?php echo $attr_align ?>"><?php
-	}   
-elseif (isset($attr_type)) {
-?><img src="<?php echo $image_dir.'icon_'.$$attr_type.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php
+?><img src="<?php echo $image_dir.'icon_el_'.$attr_elementtype.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php
+} elseif (isset($attr_type)) {
+?><img src="<?php echo $image_dir.'icon_'.$attr_type.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php
 } elseif (isset($attr_icon)) {
 ?><img src="<?php echo $image_dir.'icon_'.$attr_icon.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php
 } elseif (isset($attr_url)) {
 ?><img src="<?php echo $attr_url ?>" border="0" align="<?php echo $attr_align ?>"><?php
 } elseif (isset($attr_file)) {
-?><img src="<?php echo $image_dir.$$attr_file.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php } ?>
+?><img src="<?php echo $image_dir.$attr_file.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php } ?>
