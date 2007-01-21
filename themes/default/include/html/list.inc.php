f46d04ac2b17c9817de850cdf1a533dba4bc6e43
dankert <devnull@localhost>
2007-01-21 23:21:57 +0100

    Korrektur Attribut "extract".

--- themes/default/include/html/list.inc.php
+++ themes/default/include/html/list.inc.php
@@ -1,7 +1,7 @@
 <?php
 	$list_tmp_key   = $attr_key;
 	$list_tmp_value = $attr_value;
-	$list_extract   = ($attr_extract=='true');
+	$list_extract   = ($attr_extract==true);
 
 	
 	foreach( $$attr_list as $$list_tmp_key => $$list_tmp_value )
