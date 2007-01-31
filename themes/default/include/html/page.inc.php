ef8b94f0fcfc9f0dccf12ca81596d80c6c503220
dankert <devnull@localhost>
2007-01-31 23:22:50 +0100

    Character-Set in HTTP- und HTML-Header ausgeben.

--- themes/default/include/html/page.inc.php
+++ themes/default/include/html/page.inc.php
@@ -1,9 +1,10 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<?php header('Content-Type: text/html; charset='.lang('CHARSET'))
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <!-- $Id$ -->
 <head>
   <title><?php echo $attr_title ?></title>
-  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+  <meta http-equiv="content-type" content="text/html; charset=<?php echo lang('CHARSET') ?>" />
   <meta name="MSSmartTagsPreventParsing" content="true" />
   <meta name="robots" content="noindex,nofollow" />
   <link rel="stylesheet" type="text/css" href="./themes/default/css/default.css" />
