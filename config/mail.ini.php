4f7b2e4bb318afb4dbfb51646a666787e1e1cc5b
dankert <devnull@localhost>
2007-02-17 11:45:10 +0100

    Zeichensatz erg?nzt.

--- config/mail.ini.php
+++ config/mail.ini.php
@@ -1,7 +1,26 @@
 ; <?php exit('direct access denied') ?>
 
-enabled=true                        ; Enable sending E-Mails
-;from="OpenRat <user@example.com>"  ; Your Sender Adress
-signature="http://www.openrat.de"   ; Signature, blank if unused, use ';' for line-breaks
-;cc=                                ; Copy recipient
-;bcc=                               ; Blind-copy recipient
+; E-Mail Settings
+
+; Does your server send e-mails?
+; 'true' or 'false'
+enabled=true
+
+; The "from"-Adress. Creates a "From: "-Header.
+; This is not neccecary. Hint: Most MTAs require a valid email adress.
+;from="OpenRat <user@example.com>"
+
+; This signature is appended at the end of a mail. Use ';' for line-breaks.
+; A useful information is maybe the URL of your OpenRat installation.
+signature="http://www.openrat.de"
+
+; Copy Recipient
+;cc=
+
+; Blind Copy recipient
+;bcc=
+
+; Priority of the mail (creates an "X-Priority"-Header)
+; 1=Highest, 2=High, 3=Normal, 4=Low, 5=Lowest
+; Hint: Most MUAs ignore this header.
+priority=3
