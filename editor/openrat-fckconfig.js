aad41f1eeadccce140a9caf90a8d3106be9df38a
dankert <devnull@localhost>
2007-03-01 01:01:50 +0100

    Eigener Editor f?r HTML-Textbereiche.

---
+++ editor/openrat-fckconfig.js
@@ -0,0 +1,24 @@
+
+FCKConfig.ToolbarSets["Default"] = [
+	['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
+	'/',
+	['Bold','Italic','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['Image','Flash','Table','Rule','SpecialChar','PageBreak'],
+	'/',
+	['Style','FontFormat','FontName','FontSize'],
+	['TextColor','BGColor'],
+	['FitWindow']
+] ;
+
+FCKConfig.LinkBrowser = false;
+FCKConfig.LinkUpload = false ;
+FCKConfig.ImageBrowser = false ;
+FCKConfig.ImageUpload = false ;
+FCKConfig.FlashBrowser = false ;
+FCKConfig.FlashUpload = false ;
