83601c79618e2cbf60fe19619a57d2e9a30d515c
dankert <devnull@localhost>
2007-03-05 23:39:54 +0100

    Neues Attribut "fileext"

--- themes/default/include/html/image.inc.php
+++ themes/default/include/html/image.inc.php
@@ -7,5 +7,7 @@ if (isset($attr_elementtype)) {
 ?><img src="<?php echo $image_dir.'icon_'.$attr_icon.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php
 } elseif (isset($attr_url)) {
 ?><img src="<?php echo $attr_url ?>" border="0" align="<?php echo $attr_align ?>"><?php
+} elseif (isset($attr_fileext)) {
+?><img src="<?php echo $image_dir.$attr_fileext ?>" border="0" align="<?php echo $attr_align ?>"><?php
 } elseif (isset($attr_file)) {
 ?><img src="<?php echo $image_dir.$attr_file.IMG_ICON_EXT ?>" border="0" align="<?php echo $attr_align ?>"><?php } ?>
