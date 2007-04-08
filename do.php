9bfc296a75c643d219027aa8ad2048a136da1f90
dankert <devnull@localhost>
2007-04-08 17:30:41 +0200

    Neue Version 0.7-beta1 :)

--- do.php
+++ do.php
@@ -25,7 +25,7 @@ define('IMG_ICON_EXT'    ,'.png'   );
 define('MAX_FOLDER_DEPTH',5        );
 define('OR_CONFIG_DIR'   ,'config' );
 
-define('OR_VERSION'      ,'0.6-beta1'  );
+define('OR_VERSION'      ,'0.7-beta1'  );
 define('OR_TITLE'        ,'OpenRat CMS');
 
 define('OR_TYPE_PAGE'  ,'page'  );
