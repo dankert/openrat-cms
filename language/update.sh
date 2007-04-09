ce1d83565e9b0378eb0c77521584c0e598afaead
dankert <devnull@localhost>
2007-04-09 12:54:37 +0200

    Shellskript zum Aktualisieren der Sprachdateien.

---
+++ language/update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+#
+pgmdir=../doc/examples/maintenance
+
+echo "Step 1: Backup"
+for i in de en es fr it ru; do
+	echo "Language: $i"
+	cp -v $i.ini.php $i.ini.php.orig
+done
+
+echo "Step 2: Ordering"
+for i in de en es fr it ru; do
+	echo "Language: $i"
+	$pgmdir/sort-lang.sh $i.ini.php.orig > $i.ini.php
+done
+
+echo "Step 3: Copying missing keys"
+echo "de > en"
+$pgmdir/compare-lang.sh de.ini.php en.ini.php > en.ini.php.missing
+
+for i in es fr it ru; do
+	echo "en > $i"
+	$pgmdir/compare-lang.sh en.ini.php $i.ini.php > $i.ini.php.missing
+done
