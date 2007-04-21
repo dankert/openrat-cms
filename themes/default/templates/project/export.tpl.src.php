4dc3290fa7bd1f8e840146f39626a94204e57df6
dankert <devnull@localhost>
2007-04-21 13:38:51 +0200

    Umbenennung von Im- in Export.

---
+++ themes/default/templates/project/export.tpl.src.php
@@ -0,0 +1,12 @@
+page
+	form
+		window icon:project name:GLOBAL_PROJECT
+			row
+				cell class:fx width:50%
+					text key:GLOBAL_DATABASE
+				cell class:fx width:50%
+					selectbox name:dbid list:dbids default:actdbid
+
+			row
+				cell colspan:2 class:act
+					button
--- themes/default/templates/project/import.tpl.src.php
+++
@@ -1,9 +0,0 @@
-page
-	form
-		window icon:project name:GLOBAL_PROJECT
-			row
-				cell
-					newline
-					newline
-					newline
-					button
