796e448360156f677eba393735ecfd852b87ec63
dankert <devnull@localhost>
2007-04-21 13:39:05 +0200

    *** empty log message ***

--- themes/default/templates/project/maintenance.tpl.src.php
+++ themes/default/templates/project/maintenance.tpl.src.php
@@ -2,9 +2,6 @@ page
 	form
 		window icon:project name:GLOBAL_PROJECT
 			row
-				cell
+				cell colspan:2 class:act
 					if not:true present:done
-						newline
-						newline
-						newline
 						button
