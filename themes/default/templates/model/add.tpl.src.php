b3eb7b2295fe884d2e5af03657cec6ae58ea32bc
dankert <devnull@localhost>
2007-05-08 22:20:41 +0200

    Spalte "Name" erg?nzt.

--- themes/default/templates/model/add.tpl.src.php
+++ themes/default/templates/model/add.tpl.src.php
@@ -4,7 +4,9 @@ page
 		window icon:project name:GLOBAL_PROJECTS
 			row
 				cell class:fx
+					text key:global_name
+				cell class:fx
 					input name:name
 			row
-				cell class:act
+				cell colspan:2 class:act
 					button type:ok
