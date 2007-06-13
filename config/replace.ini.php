8c7ef254824071a82abc44d2a7d8961ac46fb136
dankert <devnull@localhost>
2007-06-13 22:46:13 +0200

    Deutlichen Hinweis auf Veraltung der Datei erg?nzt.

--- config/replace.ini.php
+++ config/replace.ini.php
@@ -3,10 +3,9 @@
 
 ; *** This part is deprecated and will be moved/removed in one of the next versions.
 
-; HTML-Markup of the EUR-sign
+; THIS FILE IS OUTDATED AND NOT USED ANY MORE!!!
+;
+; See file editor.ini.php section "html", setting "replace".
+;
 euro   = "EUR,&euro;"
-
-; HTML-Markup of copyright-char 
 copy   = "(c),&copy;"
-
-; ... add useful replacements here
