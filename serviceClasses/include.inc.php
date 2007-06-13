f7e60541af6112e8029a11565ecd80a607a32583
dankert <devnull@localhost>
2007-06-14 00:05:00 +0200

    Nur Einbinden von notwendigen Dateien, um den PHP-Parser zu entlasten und zu beschleunigen.

--- serviceClasses/include.inc.php
+++ serviceClasses/include.inc.php
@@ -3,27 +3,40 @@
 require_once( OR_SERVICECLASSES_DIR."GlobalFunctions.class.".PHP_EXT );
 require_once( OR_SERVICECLASSES_DIR."Http.class.".PHP_EXT );
 require_once( OR_SERVICECLASSES_DIR."Html.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."Upload.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."Ftp.class.".PHP_EXT );
 require_once( OR_SERVICECLASSES_DIR."Text.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."Publish.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."Api.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."TreeElement.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."AbstractTree.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."AdministrationTree.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."ProjectTree.class.".PHP_EXT );
-require_once( OR_SERVICECLASSES_DIR."Preferences.class.".PHP_EXT );
 require_once( OR_SERVICECLASSES_DIR."Mail.class.".PHP_EXT );
 require_once( OR_SERVICECLASSES_DIR."Ldap.class.".PHP_EXT );
-
-require_once( OR_SERVICECLASSES_DIR."Dynamic.class.".PHP_EXT        );
-require_once( OR_SERVICECLASSES_DIR."Code.class.".PHP_EXT           );
-require_once( OR_SERVICECLASSES_DIR."Transformer.class.".PHP_EXT    );
-require_once( OR_SERVICECLASSES_DIR."Line.class.".PHP_EXT           );
 require_once( OR_SERVICECLASSES_DIR."TemplateEngine.class.".PHP_EXT );
+require_once( OR_SERVICECLASSES_DIR."Preferences.class.".PHP_EXT );
 
-require_once( OR_SERVICECLASSES_DIR."ArchiveTar.class.".PHP_EXT     );
-require_once( OR_SERVICECLASSES_DIR."ArchiveUnzip.class.".PHP_EXT   );
-require_once( OR_SERVICECLASSES_DIR."ArchiveZip.class.".PHP_EXT     );
+if	( !empty($REQ[REQ_PARAM_ACTION]) && in_array($REQ[REQ_PARAM_ACTION],array('tree')) )
+{
+	require_once( OR_SERVICECLASSES_DIR."TreeElement.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."AbstractTree.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."AdministrationTree.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."ProjectTree.class.".PHP_EXT );
+}
+
+
+// Nur bei der Erzeugung von Seiten notwendig.
+if	( !empty($REQ[REQ_PARAM_ACTION]) && in_array($REQ[REQ_PARAM_ACTION],array('pageelement','page','folder')) )
+{
+	require_once( OR_SERVICECLASSES_DIR."Ftp.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."Publish.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."Api.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."Dynamic.class.".PHP_EXT        );
+	require_once( OR_SERVICECLASSES_DIR."Code.class.".PHP_EXT           );
+	require_once( OR_SERVICECLASSES_DIR."Transformer.class.".PHP_EXT    );
+	require_once( OR_SERVICECLASSES_DIR."Line.class.".PHP_EXT           );
+}
+
+
+if	( !empty($REQ[REQ_PARAM_ACTION]) && in_array($REQ[REQ_PARAM_ACTION],array('file','folder')) )
+{
+	require_once( OR_SERVICECLASSES_DIR."Upload.class.".PHP_EXT );
+	require_once( OR_SERVICECLASSES_DIR."ArchiveTar.class.".PHP_EXT     );
+	require_once( OR_SERVICECLASSES_DIR."ArchiveUnzip.class.".PHP_EXT   );
+	require_once( OR_SERVICECLASSES_DIR."ArchiveZip.class.".PHP_EXT     );
+}
 
 ?>
--- textClasses/include.inc.php
+++ textClasses/include.inc.php
@@ -1,29 +1,32 @@
 <?php
 
-require_once( OR_TEXTCLASSES_DIR."AbstractElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."QuoteElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."CodeElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."DocumentElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."EmphaticElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."HeadlineElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."LineBreakElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."LinkElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."ImageElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."TeletypeElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."SpeechElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."ListElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."ListEntryElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."NumberedListElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."ParagraphElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."StrongElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."TableElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."TableLineElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."TableCellElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."TableOfContentElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."TextElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."DefinitionListElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."DefinitionItemElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."InsertedElement.class.".PHP_EXT );
-require_once( OR_TEXTCLASSES_DIR."RemovedElement.class.".PHP_EXT );
-
+// Die DOM-Klassen sind nur bei der Erzeugung von Seiten notwendig.
+if	( !empty($REQ[REQ_PARAM_ACTION]) && in_array($REQ[REQ_PARAM_ACTION],array('pageelement','page','folder')) )
+{
+	require_once( OR_TEXTCLASSES_DIR."AbstractElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."QuoteElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."CodeElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."DocumentElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."EmphaticElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."HeadlineElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."LineBreakElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."LinkElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."ImageElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."TeletypeElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."SpeechElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."ListElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."ListEntryElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."NumberedListElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."ParagraphElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."StrongElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."TableElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."TableLineElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."TableCellElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."TableOfContentElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."TextElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."DefinitionListElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."DefinitionItemElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."InsertedElement.class.".PHP_EXT );
+	require_once( OR_TEXTCLASSES_DIR."RemovedElement.class.".PHP_EXT );
+}
 ?>
