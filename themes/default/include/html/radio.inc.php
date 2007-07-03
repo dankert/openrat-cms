23252e21e2a76cab2f7231f36398bf7f5709c3dd
dankert <devnull@localhost>
2007-07-03 23:08:44 +0200

    Bessere Ermittlung des Default-Wertes f?r Radio-Knopf.

--- themes/default/include/html/radio.inc.php
+++ themes/default/include/html/radio.inc.php
@@ -1,1 +1,9 @@
-<input type="radio" id="id_<?php echo $attr_name.'_'.$attr_value ?>"  name="<?php echo $attr_prefix.$attr_name ?>"<?php if ( $attr_readonly ) echo ' disabled="disabled"' ?> value="<?php echo $attr_value ?>" <?php if($attr_value==$attr_default) echo 'checked="checked"' ?> />
+<?php
+		if	( isset($$attr_name)  )
+			$attr_tmp_default = $$attr_name;
+		elseif ( isset($attr_default) )
+			$attr_tmp_default = $attr_default;
+		else
+			$attr_tmp_default = '';
+
+ ?><input type="radio" id="id_<?php echo $attr_name.'_'.$attr_value ?>"  name="<?php echo $attr_prefix.$attr_name ?>"<?php if ( $attr_readonly ) echo ' disabled="disabled"' ?> value="<?php echo $attr_value ?>" <?php if($attr_value==$attr_tmp_default) echo 'checked="checked"' ?> />
