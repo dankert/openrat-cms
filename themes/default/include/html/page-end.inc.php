488d3d514203a2bfa481fda4b04db2deb53dc200
dankert <devnull@localhost>
2007-10-10 22:17:29 +0200

    Vollst?ndiger Ausbau von der Dauerz?hlung.

--- themes/default/include/html/page-end.inc.php
+++ themes/default/include/html/page-end.inc.php
@@ -1,12 +1,6 @@
 
 <!-- $Id$ -->
 
-<?php if ($showDuration) { ?>
-<br/>
-<small>&nbsp;
-<?php $dur = time()-START_TIME;
-//      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small>
-<?php } ?>
 
 </body>
 </html>
