bc4cd02bb798cac4c1642e050b5685cacda4e3be
dankert <devnull@localhost>
2007-10-22 19:59:56 +0200

    Ausgabe der Laufzeit zentrieren.

--- themes/default/include/html/window-end.inc.php
+++ themes/default/include/html/window-end.inc.php
@@ -8,7 +8,7 @@
 <?php if ($showDuration)
       { ?>
 <br/>
-<small>&nbsp;
+<center><small>&nbsp;
 <?php $dur = time()-START_TIME;
-      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small>
+      echo floor($dur/60).':'.str_pad($dur%60,2,'0',STR_PAD_LEFT); ?></small></center>
 <?php } ?>
