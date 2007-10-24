df43d8fe83a65ff169e729c3db55c418a4ed597e
dankert <devnull@localhost>
2007-10-24 23:07:25 +0200

    Globale Funktionen.

---
+++ functions/common.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * Fügt einen Slash ("/") an das Ende an, sofern nicht bereits vorhanden.
+ *
+ * @param String $pfad
+ * @return Pfad mit angehängtem Slash.
+ */
+function slashify($pfad)
+{
+	if	( substr($pfad,-1,1) == '/')
+		return $pfad;
+	else
+		return $pfad.'/';
+}
+
+?>
