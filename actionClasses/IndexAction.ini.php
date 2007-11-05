2ea834e67f7e45ae052d142304da420b7988c10a
dankert <devnull@localhost>
2007-11-05 21:53:22 +0100

    F?r Aufruf einzelner Funktionen muss man nicht angemeldet sein.

--- actionClasses/IndexAction.ini.php
+++ actionClasses/IndexAction.ini.php
@@ -1,12 +1,23 @@
 
+admin=false
+
 [default]
-goto=showlogin
+goto=show
 
 [logout]
 goto=showlogin
 
 [object]
 goto=show
+guest=true
+
+[language]
+goto=show
+guest=true
+
+[model]
+goto=show
+guest=true
 
 [applications]
 menu=menu
@@ -19,37 +30,46 @@ goto=show
 [register]
 menu=login
 target=registercode
+guest=true
 
 [registercode]
 menu=login
 target=registercommit
+guest=true
 
 [registercommit]
 menu=login
 goto=showlogin
+guest=true
 
 [password]
 menu=login
 target=passwordcode
+guest=true
 
 [passwordcode]
 menu=login
 target=passwordcommit
+guest=true
 
 [passwordcommit]
 menu=login
 goto=showlogin
+guest=true
 
 [showlogin]
 menu=login
+guest=true
 
 [administration]
 goto=show
 
 [login]
 goto=show
+guest=true
 
 [openid]
+guest=true
 goto=show
 
 [projectmenu]
@@ -64,6 +84,7 @@ menu=menu
 goto=projectmenu
 
 [show]
+guest=true
 
 [menu]
 login=showlogin,register,password
