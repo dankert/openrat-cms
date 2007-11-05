da33ab956ff155475ca9e317ea803a237590d52c
dankert <devnull@localhost>
2007-11-05 22:01:33 +0100

    Komplette Erneuerung der Anzeige von Rechten f?r diesen Benutzer.

--- themes/default/templates/user/rights.tpl.src.php
+++ themes/default/templates/user/rights.tpl.src.php
@@ -1,60 +1,52 @@
-RAW
-<?php include( $tpl_dir.'header.tpl.php') ?>
+page
 
+	window title:ACL name:x
 
-<center>
-
-<?php foreach($projects as $projectid=>$projectname) { ?>
-
-<?php windowOpen( lang('GLOBAL_PROJECT').': '.$projectname,15) ?>
-
-  <tr>
-    <td class="help"><?php echo lang('GLOBAL_FILENAME')     ?></td>
-    <td class="help"><?php echo lang('GLOBAL_LANGUAGE')     ?></td>
-    <?php foreach( $show as $t ) { ?>
-    <td class="help"><span title="<?php echo lang('ACL_'.strtoupper($t)) ?>"><strong><?php echo lang('ACL_'.strtoupper($t).'_ABBREV') ?></strong></span></td>
-    <?php } ?>
-  </tr>
-
-
-
-
-
-<?php $fx = '';
-      if (isset($rights[$projectid]) && is_array($rights[$projectid]))
-      {
-        foreach( $rights[$projectid] as $oid=>$z )
-        { $fx = fx($fx); ?>
-  <tr>
-<?php $objectUrl = Html::url( 'index','object',$oid ) ?>
-    <td class="<?php echo $fx ?>"><a href="<?php echo $objectUrl ?>" target="_top"><img src="<?php echo $image_dir.'icon_'.$objects[$oid]['type'].IMG_EXT ?>" border="0" align="left"><?php echo $objects[$oid]['filename'] ?></a></td>
-    <td class="<?php echo $fx ?>"><?php echo $z['languagename'] ?></td>
-
-<?php foreach( $show as $t )
-      { ?>
-    <td class="<?php echo $fx ?>"><?php echo Html::checkBox('',$z[$t],false,array('title'=>lang('ACL_'.strtoupper($t))) ) ?></td>
-<?php } ?>
-
-
-  </tr>
-<?php }
-      $fx = fx($fx); ?>
-<?php }
-      else
-      { $fx = fx($fx); ?>
-  <tr>
-    <td colspan="14" class="<?php echo $fx ?>"><?php echo lang('GLOBAL_NOT_FOUND') ?></td>
-  </tr>
-<?php } ?>
-
-
-<?php windowClose() ?>
-
-<?php } ?>
-
-
-
-
-</center>
-<?php include( $tpl_dir.'footer.tpl.php') ?>
-END
+		list list:projects extract:true
+			row
+				cell colspan:14
+					fieldset title:var:projectname
+		
+			if empty:acls
+				row
+					cell class:fx
+						text text:GLOBAL_NOT_FOUND
+			if not:true empty:acls
+				row
+					cell class:help
+						text text:GLOBAL_USER
+					cell class:help
+						text text:GLOBAL_NAME
+					cell class:help
+						text text:GLOBAL_LANGUAGE
+						
+					list list:show value:t
+						cell class:help
+							text key:var:t prefix:acl_ suffix:_abbrev
+	
+			list list:rights key:aclid value:acl extract:true
+				row
+					cell
+						if present:username
+							image type:user
+							text text:var:username maxlength:20
+						if present:groupname
+							image type:group
+							text text:var:groupname maxlength:20
+						if not:true present:username
+							if not:true present:groupname
+								image type:group
+								text key:global_all
+						set var:username
+						set var:groupname
+					cell
+						image type:var:objecttype
+						link action:index subaction:object id:var:objectid target:_top
+							text var:objectname maxlength:20
+					cell
+						text text:var:languagename maxlength:20
+	
+					list list:show
+						cell
+							set var:var:list_value value:var:bits key:var:list_value
+							checkbox name:var:list_value readonly:true
