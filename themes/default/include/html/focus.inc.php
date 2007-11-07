e395ec2e9a33b4cc7447cd5b7b536935c7e11c44
dankert <devnull@localhost>
2007-11-07 21:53:25 +0100

    Falls Fehler, dann Fokus auf (erstes) fehlerhaftes Feld setzen.

--- themes/default/include/html/focus.inc.php
+++ themes/default/include/html/focus.inc.php
@@ -1,7 +1,8 @@
-
-<script name="JavaScript" type="text/javascript">
-<!--
+<?php
+// Wenn Eingabefehler vorhanden, dann den Fokus auf das Fehlerfeld setzen.
+if (isset($errors[0])) $attr_field = $errors[0];
+?><script name="JavaScript" type="text/javascript"><!--
 document.forms[0].<?php echo $attr_field ?>.focus();
 document.forms[0].<?php echo $attr_field ?>.select();
-// -->
+    // -->
 </script>
