01f6e6f4b1cef7b77e1c34080377acd5fdefbf17
dankert <devnull@localhost>
2007-11-09 21:39:39 +0100

    Bei Fehler die Selectbox rot umranden.

--- themes/default/include/html/selectbox.inc.php
+++ themes/default/include/html/selectbox.inc.php
@@ -3,6 +3,7 @@ if ($attr_addempty) $$attr_list = array(''=>lang('LIST_ENTRY_EMPTY'))+$$attr_lis
 ?><select id="id_<?php echo $attr_name ?>"  name="<?php echo $attr_name; if ($attr_multiple) echo '[]'; ?>" onchange="<?php echo $attr_onchange ?>" title="<?php echo $attr_title ?>" class="<?php echo $attr_class ?>"<?php
 if (count($$attr_list)<=1) echo ' disabled="disabled"';
 if	($attr_multiple) echo ' multiple="multiple"';
+if (in_array($attr_name,$errors)) echo ' style="background-color:red; border:2px dashed red;"';
 echo ' size="'.intval($attr_size).'"';
 ?>><?php
 		$attr_tmp_list = $$attr_list;
