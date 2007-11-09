4395f14e70a15b3008e74bce872feb99dd68a18d
dankert <devnull@localhost>
2007-11-09 21:40:42 +0100

    Sch?neres Layout, Typ nicht mehr vorausw?hlen.

--- themes/default/templates/object/aclform.tpl.src.php
+++ themes/default/templates/object/aclform.tpl.src.php
@@ -9,40 +9,40 @@ page
 					fieldset title:message:users
 					
 			row
+				cell class:fx width:10px
+					radio name:type value:all
 				cell class:fx
 					label for:type value:all
 						text text:GLOBAL_ALL
 				cell class:fx
-					radio name:type value:all default:true
-				cell class:fx
 					text raw:_
 			row
+				cell class:fx width:10px
+					radio name:type value:user
 				cell class:fx
 					label for:type value:user
 						text text:GLOBAL_USER
 				cell class:fx
-					radio name:type value:user
-				cell class:fx
-					selectbox name:userid list:users
+					selectbox name:userid list:users addempty:true
 			if present:groups
 				row
+					cell class:fx width:10px
+						radio name:type value:group
 					cell class:fx
 						label for:type value:group
 							text text:GLOBAL_GROUP
 					cell class:fx
-						radio name:type value:group
-					cell class:fx
-						selectbox name:groupid list:groups
+						selectbox name:groupid list:groups addempty:true
 
 			row
 				cell colspan:3
 					fieldset title:message:language
 			row
+				cell width:10px
+					text raw:_
 				cell class:fx
 					label for:languageid
 						text text:GLOBAL_LANGUAGE
-				cell class:fx
-					text raw:_
 				cell class:fx
 					selectbox name:languageid list:languages
 
@@ -52,19 +52,20 @@ page
 	
 			list list:show value:t key:k
 				row
-					cell
-						label for:var:t
-							text key:var:t prefix:acl_
-					cell width:20px
-						label for:var:t
-							text key:var:t prefix:acl_ suffix:_abbrev
-					cell
+//					cell
+//					cell width:20px
+//						label for:var:t
+//							text key:var:t prefix:acl_ suffix:_abbrev
+					cell width:10px
 						if value:var:t equals:read
 							set var:var:t value:true
 							checkbox name:var:t readonly:true
 						else
 							set var:var:t value:false
 							checkbox name:var:t readonly:false 
+					cell colspan:2
+						label for:var:t
+							text key:var:t prefix:acl_
 			row
 				cell colspan:3 class:act
 					button type:ok
