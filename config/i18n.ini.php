775aced98898a8d4f1dd80d68f4e8969bc5179e7
dankert <devnull@localhost>
2007-11-10 00:35:19 +0100

    Verf?gbare Sprachen eingetragen.

--- config/i18n.ini.php
+++ config/i18n.ini.php
@@ -4,9 +4,12 @@
 ; This is a useful setting. The Browser says, which language will be taken.
 use_http=true
 
+
 ; Default language
 default=de
 
-; Uncomplete words are taken from following language
-; *** maybe outdated ***
-complete_from=en
+
+; Available Languages.
+; A comma seperated list with language codes.
+available=de,en,es,fr,it,ru
+
