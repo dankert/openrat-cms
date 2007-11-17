b560ca65a463f030da117a7f4a3b80956ba64052
dankert <devnull@localhost>
2007-11-17 18:10:33 +0100

    Online-Hilfe per default aktivieren, da mittlerweile vollst?ndig.

--- config/help.ini.php
+++ config/help.ini.php
@@ -1,16 +1,14 @@
 ; <?php exit('direct access denied') ?>
 
-; *** available in a future version of OpenRat. ***
-
 ; Enable online help
-enabled=false
+enabled=true
 
 ; URL to the documents 
-url=doc/online/html/
-; url=http://help.openrat.de/
+;url=doc/online/html/
+url=http://help.openrat.de/
 
 ; file extension of the help documents
 suffix=.html
 
 ; show only a question mark instead of "help"
-only_question_mark=true
+only_question_mark=false
