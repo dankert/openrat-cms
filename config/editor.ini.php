2eb1ce83148d6e65fc46e162e550ebae860021c3
dankert <devnull@localhost>
2007-11-24 15:25:04 +0100

    Einstellungen f?r Fu?noten.

--- config/editor.ini.php
+++ config/editor.ini.php
@@ -23,6 +23,10 @@ speech-end   = QUOTE
 code-begin = "="
 code-end   = "="
 
+; footnotes
+footnote-begin="["
+footnote-end="]"
+
 ; pre-formatted Text
 pre-begin = "="
 pre-end   = "="
@@ -102,10 +106,21 @@ rendermode=sgml
 
 replace = "EUR:&euro; Ä:&Auml; ä:&auml; Ö:&Ouml; ö:&ouml; Ü:&Uuml; ü:&uuml; ß:&szlig; (c):&copy; (r):&reg; ^1:&sup1; ^2:&sup2; ^3:&sup3; 1/4:&frac14; 1/2:&frac12; 3/4:&frac34;"
 
+
+
 [wiki]
 
 convert_html=true
 convert_bbcode=true
+
+
+
+; Settings for text/plain-documents
+[text]
+
+; maximum line length
+linelength=70
+
 
 
 ; Calendar settings
@@ -115,3 +130,7 @@ convert_bbcode=true
 ; 0 = Week begins with Sunday (America, Australia, Islam)
 ; 1 = Week begins with Monday (ISO-8601, Europe)
 weekday_offset=1
+
+
+[text]
+linelength=70
