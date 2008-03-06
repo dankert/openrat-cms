7b84d9cff3ed449770d8f48cc129d320e5088506
dankert <devnull@localhost>
2008-03-06 21:42:58 +0100

    Untere Statusleiste erstmal deaktiviert.

--- themes/default/templates/index/show.tpl.src.php
+++ themes/default/templates/index/show.tpl.src.php
@@ -16,7 +16,7 @@ frameset-page
 				frame file:var:frame_src_main name:cms_main
 
 	else					
-		frameset rows:23,3,*,3,20
+		frameset rows:23,3,*
 			frame file:var:frame_src_title name:cms_title
 			frame file:var:frame_src_border
 			#frame file:var:frame_src_background
@@ -31,6 +31,6 @@ frameset-page
 					frame file:var:frame_src_tree_title name:cms_treemenu
 					frame file:var:frame_src_tree name:cms_tree scrolling:auto
 				frame file:var:frame_src_main name:cms_main
-			frame file:var:frame_src_border
-			frame file:var:frame_src_status
+			#frame file:var:frame_src_border
+			#frame file:var:frame_src_status
 					
