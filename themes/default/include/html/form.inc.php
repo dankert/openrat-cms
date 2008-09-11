bc058d0ceec4057ee9d5ddbd49b7b402a80d9121
dankert <devnull@localhost>
2008-09-11 20:59:22 +0200

    Falls Edit-Mode vorhanden, aber nicht aktiv, dann Formular-Ziel auf Bearbeiten-Modus setzen.

--- themes/default/include/html/form.inc.php
+++ themes/default/include/html/form.inc.php
@@ -5,12 +5,20 @@
 		$attr_subaction = $targetSubActionName;
 	if	(empty($attr_id))
 		$attr_id = $this->getRequestId();
+
+	if ($this->isEditable() && $this->getRequestVar('mode')!='edit')
+		$attr_subaction = $subActionName;
 		
 ?><form name="<?php echo $attr_name ?>"
       target="<?php echo $attr_target ?>"
       action="<?php echo Html::url( $attr_action,$attr_subaction,$attr_id ) ?>"
       method="<?php echo $attr_method ?>"
       enctype="<?php echo $attr_enctype ?>" style="margin:0px;padding:0px;">
+      
+<?php if ($this->isEditable() && $this->getRequestVar('mode')!='edit') { ?>
+<input type="hidden" name="mode" value="edit" />
+<?php } ?>
+	
 <input type="hidden" name="<?php echo REQ_PARAM_ACTION ?>" value="<?php echo $attr_action ?>" />
 <input type="hidden" name="<?php echo REQ_PARAM_SUBACTION ?>" value="<?php echo $attr_subaction ?>" />
 <input type="hidden" name="<?php echo REQ_PARAM_ID ?>" value="<?php echo $attr_id ?>" /><?php
