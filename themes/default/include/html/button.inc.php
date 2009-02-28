419828c59a5ebd4513a6a89250c4c9d5d2a9524e
dankert <devnull@localhost>
2009-03-01 00:29:19 +0100

    Korrektur Ausgabe Textvariablen wenn Edit-Mode.

--- themes/default/include/html/button.inc.php
+++ themes/default/include/html/button.inc.php
@@ -1,8 +1,8 @@
 <?php
 	if ($attr_type=='ok')
 	{
-		if ($this->isEditable() && $this->getRequestVar('mode')!='edit')
-		$attr_text = lang('MODE_EDIT');
+		if ($this->isEditable() && !$this->isEditMode())
+		$attr_text = 'MODE_EDIT';
 	}
 
 	if ($attr_type=='ok')
@@ -11,4 +11,5 @@
 		$attr_type  = 'image';
 	else
 		$attr_src  = '';
-?><input type="<?php echo $attr_type ?>"<?php if(isset($attr_src)) { ?> src="<?php echo $image_dir.'icon_'.$attr_src.IMG_ICON_EXT ?>"<?php } ?> name="<?php echo $attr_value ?>" class="<?php echo $attr_class ?>" title="<?php echo lang($attr_text.'_DESC') ?>" value="&nbsp;&nbsp;&nbsp;&nbsp;<?php echo lang($attr_text) ?>&nbsp;&nbsp;&nbsp;&nbsp;" /><?php unset($attr_src) ?>
+?><input type="<?php echo $attr_type ?>"<?php if(isset($attr_src)) { ?> src="<?php echo $image_dir.'icon_'.$attr_src.IMG_ICON_EXT ?>"<?php } ?> name="<?php echo $attr_value ?>" class="<?php echo $attr_class ?>" title="<?php echo lang($attr_text.'_DESC') ?>" value="&nbsp;&nbsp;&nbsp;&nbsp;<?php echo langHtml($attr_text) ?>&nbsp;&nbsp;&nbsp;&nbsp;" /><?php unset($attr_src) ?><?php
+?>
