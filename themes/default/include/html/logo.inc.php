64b2dd9f24f2835929639f381fa821bc0259ca42
dankert <devnull@localhost>
2009-03-01 13:38:33 +0100

    Textausgabe HTML-Kodieren.

--- themes/default/include/html/logo.inc.php
+++ themes/default/include/html/logo.inc.php
@@ -1,1 +1,1 @@
-<img src="<?php echo $image_dir.'logo_'.$attr_name.IMG_ICON_EXT ?>" border="0" align="left"><h2 class="logo"><?php echo lang('logo_'.$attr_name) ?></h2><p class="logo"><?php echo lang('logo_'.$attr_name.'_text') ?></p>
+<img src="<?php echo $image_dir.'logo_'.$attr_name.IMG_ICON_EXT ?>" border="0" align="left"><h2 class="logo"><?php echo langHtml('logo_'.$attr_name) ?></h2><p class="logo"><?php echo langHtml('logo_'.$attr_name.'_text') ?></p>
