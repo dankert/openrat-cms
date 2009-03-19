4d5e64756dd98f0297b1fe652902da411f41ab77
dankert <devnull@localhost>
2009-03-19 05:33:44 +0100

    Eingabemaske zum Starten des Vererbungsvorganges von Berechtigungen.

--- themes/default/templates/object/rights.tpl.src.php
+++ themes/default/templates/object/rights.tpl.src.php
@@ -1,47 +1,59 @@
 page
 
-	window title:ACL name:x
-
-		if empty:acls
-			row
-				cell class:fx
-					text text:GLOBAL_NOT_FOUND
-		if not:true empty:acls
-			row
-				cell class:help
-					text text:GLOBAL_NAME
-				cell class:help
-					text text:GLOBAL_LANGUAGE
-					
-				list list:show value:t
+	form
+		window title:ACL name:x
+	
+			if empty:acls
+				row
+					cell class:fx
+						text text:GLOBAL_NOT_FOUND
+			if not:true empty:acls
+				row
 					cell class:help
-						text key:var:t prefix:acl_ suffix:_abbrev
-
-				cell class:help
-					text text:global_delete
-
-		list list:acls key:aclid value:acl extract:true
-			row
-				cell
-					if present:username
-						image type:user
-						text var:username
-					if present:groupname
-						image type:group
-						text var:groupname
-					if not:true present:username
-						if not:true present:groupname
-							image type:group
-							text key:global_all
-				cell
-					text var:languagename
-
-				list list:show value:t
+						text text:GLOBAL_NAME
+					cell class:help
+						text text:GLOBAL_LANGUAGE
+						
+					list list:show value:t
+						cell class:help
+							text key:var:t prefix:acl_ suffix:_abbrev
+	
+					cell class:help
+						if true:mode:edit
+							text text:global_delete
+	
+			list list:acls key:aclid value:acl extract:true
+				row
 					cell
-						checkbox name:var:t default:false readonly:true
-				cell
-					if present:delete_url
-							link url:var:delete_url
-								text key:GLOBAL_DELETE
-					else
-						text key:ACL_INHERITED
+						if present:username
+							image type:user
+							text var:username
+						if present:groupname
+							image type:group
+							text var:groupname
+						if not:true present:username
+							if not:true present:groupname
+								image type:group
+								text key:global_all
+					cell
+						text var:languagename
+	
+					list list:show value:t
+						cell
+							checkbox name:var:t default:false readonly:true
+					cell
+						if true:mode:edit
+							if present:delete_url
+									link url:var:delete_url
+										text key:GLOBAL_DELETE
+	
+			if true:mode:edit
+				row
+					cell colspan:15
+						set var:inherit value:1
+						input type:checkbox name:inherit
+						label for:inherit
+							text key:inherit_rights
+				row
+					cell class:act colspan:15
+						button type:ok
