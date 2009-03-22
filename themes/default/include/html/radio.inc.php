9db97280edda88b86bb9ddb07ff3661fd9bf38dd
dankert <devnull@localhost>
2009-03-22 20:51:47 +0100

    Radio-Boxen sollen CSS-Klasse "radio" haben.

--- themes/default/include/html/radio.inc.php
+++ themes/default/include/html/radio.inc.php
@@ -8,4 +8,4 @@
 		else
 			$attr_tmp_default = '';
 
- ?><input type="radio" id="id_<?php echo $attr_name.'_'.$attr_value ?>"  name="<?php echo $attr_prefix.$attr_name ?>"<?php if ( $attr_readonly ) echo ' disabled="disabled"' ?> value="<?php echo $attr_value ?>" <?php if($attr_value==$attr_tmp_default) echo 'checked="checked"' ?><?php if (in_array($attr_name,$errors)) echo ' style="borderx:2px dashed red; background-color:red;"' ?> />
+ ?><input class="radio" type="radio" id="id_<?php echo $attr_name.'_'.$attr_value ?>"  name="<?php echo $attr_prefix.$attr_name ?>"<?php if ( $attr_readonly ) echo ' disabled="disabled"' ?> value="<?php echo $attr_value ?>" <?php if($attr_value==$attr_tmp_default) echo 'checked="checked"' ?><?php if (in_array($attr_name,$errors)) echo ' style="borderx:2px dashed red; background-color:red;"' ?> />
