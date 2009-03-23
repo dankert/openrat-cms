884b6de074b7e889501d2acbe1b8f09cca90dcaf
dankert <devnull@localhost>
2009-03-23 22:47:21 +0100

    Neue Version: 0.9-2

--- do.php
+++ do.php
@@ -34,7 +34,7 @@ define('IMG_EXT'         ,'.gif'   );
 define('IMG_ICON_EXT'    ,'.png'   );
 define('MAX_FOLDER_DEPTH',5        );
 
-define('OR_VERSION'      ,'0.9-1'  );
+define('OR_VERSION'      ,'0.9-2'  );
 define('OR_TITLE'        ,'OpenRat CMS');
 
 define('OR_TYPE_PAGE'  ,'page'  );
