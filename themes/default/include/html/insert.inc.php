456e6f847637030d6f673a192228f9990ea72ed5
dankert <devnull@localhost>
2009-03-31 23:01:47 +0200

    Attribut "inline" für Element "insert" ergänzt.

--- themes/default/include/html/insert.inc.php
+++ themes/default/include/html/insert.inc.php
@@ -4,5 +4,18 @@
 /* #END-IF# */
 
 /* #IF-ATTR script# */
-<script src="<?php echo OR_THEMES_DIR.$conf['interface']['theme'].'/js/'.basename($attr_script).'.js' ?>" type="text/javascript"></script>
+<?php
+$attr_tmp_file = OR_THEMES_DIR.$conf['interface']['theme'].'/js/'.basename($attr_script).'.js';
+if	(!$attr_inline)
+{
+	?><script src="<?php echo $attr_tmp_file ?>" type="text/javascript"></script><?php 
+}
+else
+{
+	echo '<script type="text/javascript">';
+	// Sehr einfaches Minifizieren des Java-Skriptes.
+	echo str_replace('  ',' ',str_replace('~','',strtr(implode('',file($attr_tmp_file)),"\t\n\b",'~~~')));
+	echo '</script>';
+}
+?>
 /* #END-IF# */
