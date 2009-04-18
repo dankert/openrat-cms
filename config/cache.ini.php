1efd786fdadbf88ddafc78b606658e1ee5e9d721
dankert <devnull@localhost>
2009-04-18 02:59:30 +0200

    Cond-GETs auf FALSE (default) setzen.

--- config/cache.ini.php
+++ config/cache.ini.php
@@ -2,8 +2,8 @@
 
 ; Conditional-GET enables the "304 not modified" HTTP-Header
 ; This is much faster, but sometimes caching is unwanted
-; if you have caching problems, set this to 'false'. Default: 'true'
-conditional_get=true
+; if you have caching problems, set this to 'false'. Default: 'false'
+conditional_get=false
 
 
 
