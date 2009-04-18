85511687233162c6b1d13c7f7fa8960d5e02a105
dankert <devnull@localhost>
2009-04-18 02:53:02 +0200

    HTML-Editor-Renderer entfernt.

--- textClasses/include.inc.php
+++ textClasses/include.inc.php
@@ -5,7 +5,6 @@ if	( !empty($REQ[REQ_PARAM_ACTION]) && in_array($REQ[REQ_PARAM_ACTION],array('pa
 {
 	require_once( OR_TEXTCLASSES_DIR."parser/WikiParser.class.".PHP_EXT );
 	require_once( OR_TEXTCLASSES_DIR."renderer/HtmlRenderer.class.".PHP_EXT );
-	require_once( OR_TEXTCLASSES_DIR."renderer/HtmlEditorRenderer.class.".PHP_EXT );
 	require_once( OR_TEXTCLASSES_DIR."renderer/HtmlDomRenderer.class.".PHP_EXT );
 	require_once( OR_TEXTCLASSES_DIR."renderer/PdfRenderer.class.".PHP_EXT );
 	require_once( OR_TEXTCLASSES_DIR."renderer/TextRenderer.class.".PHP_EXT );
