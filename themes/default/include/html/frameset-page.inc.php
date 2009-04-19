a2525c9f0b39326e7b1ee821176456e0dacf7ab9
dankert <devnull@localhost>
2009-04-20 00:38:11 +0200

    Charset in den HTTP-Header schreiben.

--- themes/default/include/html/frameset-page.inc.php
+++ themes/default/include/html/frameset-page.inc.php
@@ -1,21 +1,23 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Frameset//EN">
+<?php
+ if (!headers_sent()) header('Content-Type: text/html; charset='.$charset)
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Frameset//EN">
 <html>
 <!-- $Id$ -->
   <head>
     <title><?php echo @$title ?> - <?php echo $cms_title ?></title>
-    <link rel="shortcut icon" href="<?php echo $image_dir.'favicon.ico' ?>" />
+    <link rel="shortcut icon" href="<?php echo $image_dir.'favicon.ico' ?>" >
 
     <?php if (isset($windowMenu) && is_array($windowMenu)) foreach( $windowMenu as $menu )
       {
        	?>
-  <link rel="section" href="<?php echo Html::url($actionName,$menu['subaction'],$this->getRequestId() ) ?>" title="<?php echo lang($menu['text']) ?>" /><?php
+  <link rel="section" href="<?php echo Html::url($actionName,$menu['subaction'],$this->getRequestId() ) ?>" title="<?php echo lang($menu['text']) ?>" ><?php
       }
 ?>
     <?php if (isset($metaList) && is_array($metaList)) foreach( $metaList as $meta )
       {
        	?>
-  <link rel="<?php echo $meta['name'] ?>" href="<?php echo $meta['url'] ?>" title="<?php echo lang($meta['title']) ?>" /><?php
+  <link rel="<?php echo $meta['name'] ?>" href="<?php echo $meta['url'] ?>" title="<?php echo lang($meta['title']) ?>" ><?php
       }
 ?>
-    <meta name="robots" content="noindex,nofollow" />
+    <meta name="robots" content="noindex,nofollow" >
   </head>
