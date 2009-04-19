78bae71257db0847332777b20302d0831ecea6d2
dankert <devnull@localhost>
2009-04-20 00:37:30 +0200

    Korrektur der Präprozessor-Anweisungen.

--- themes/default/include/html/frameset.inc.php
+++ themes/default/include/html/frameset.inc.php
@@ -1,8 +1,8 @@
 <frameset
 <!-- #IF-ATTR rows -->
-<?php echo 'rows="'.$attr_rows.'"' ?>
+<?php echo ' rows="'.$attr_rows.'"' ?>
 <!-- #END-IF # -->
-<!-- #IF-ATTR-columns# -->
+<!-- #IF-ATTR columns# -->
 <?php echo ' cols="'.$attr_columns.'"' ?>
 <!-- #END-IF # -->
  border="0" frameborder="5" framespacing="0" bordercolor="#000000">
