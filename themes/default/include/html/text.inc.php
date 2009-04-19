e71b36141eeb122ab587a4f6dd6cfc9694701a1b
dankert <devnull@localhost>
2009-04-19 22:05:49 +0200

    Verwenden von Präprozessor-Anweisungen.

--- themes/default/include/html/text.inc.php
+++ themes/default/include/html/text.inc.php
@@ -1,50 +1,56 @@
 <?php
-	if	( isset($attr_prefix)&& isset($attr_key))
+
+	#IF-ATTR prefix#
 		$attr_key = $attr_prefix.$attr_key;
-	if	( isset($attr_suffix)&& isset($attr_key))
+	#END-IF-ATTR#
+
+	#IF-ATTR suffix#
 		$attr_key = $attr_key.$attr_suffix;
-		
-	if(empty($attr_title))
-//		if (!empty($attr_key))
-//			$attr_title = lang($attr_key.'_HELP');
-//		else
-			$attr_title = '';
-	if	(empty($attr_type))
+	#END-IF-ATTR#
+
+	#IF-ATTR title#
+	#ELSE#
+		$attr_title = '';
+	#END-IF#
+
+	#IF-ATTR type#
+	#ELSE#
+		// Attribut "type" nicht vorhanden, also Default-Tag "<span>..." verwenden.
 		$tmp_tag = 'span';
-	else
-		switch( $attr_type )
-		{
-			case 'emphatic':
-			case 'italic':
-				$tmp_tag = 'em';
-				break;
-			case 'strong':
-			case 'bold':
-				$tmp_tag = 'strong';
-				break;
-			case 'tt':
-			case 'teletype':
-				$tmp_tag = 'tt';
-				break;
-			case 'preformatted';
-				$tmp_tag = 'pre';
-				break;
-			case 'code';
-				$tmp_tag = 'code';
-				break;
-			default:
-				$tmp_tag = 'span';
-		}
+	#END-IF#
+
+	#IF-ATTR-VALUE type:emphatic#
+		$tmp_tag = 'em';
+	#END-IF#
+	#IF-ATTR-VALUE type:italic#
+		$tmp_tag = 'em';
+	#END-IF#
+	#IF-ATTR-VALUE type:strong#
+		$tmp_tag = 'strong';
+	#END-IF#
+	#IF-ATTR-VALUE type:bold#
+		$tmp_tag = 'strong';
+	#IF-ATTR-VALUE type:tt
+		$tmp_tag = 'tt';
+	#END-IF#
+	#IF-ATTR-VALUE type:teletype#
+		$tmp_tag = 'tt';
+	#END-IF#
+	#IF-ATTR-VALUE type:preformatted#
+		$tmp_tag = 'pre';
+	#END-IF#
+	#IF-ATTR-VALUE type:code#
+		$tmp_tag = 'code';
+	#END-IF#
 
 ?><<?php echo $tmp_tag ?> class="<?php echo $attr_class ?>" title="<?php echo $attr_title ?>"><?php
-	$attr_title = '';
-	if	( $attr_escape )
+	#IF-ATTR escape#
+		$langF = $attr_escape?'langHtml':'lang';
+	#ELSE
 		$langF = 'langHtml';
-	else
-		$langF = 'lang';
-	
-	if (!empty($attr_array))
-	{
+	#END-IF
+
+	#IF-ATTR array#
 		//geht nicht:
 		//echo $$attr_array[$attr_var].'%';
 		$tmpArray = $$attr_array;
@@ -52,37 +58,42 @@
 			$tmp_text = $tmpArray[$attr_var];
 		else
 			$tmp_text = $langF($tmpArray[$attr_text]);
-	}
-	elseif (!empty($attr_text))
-		$tmp_text = $langF($attr_text);
-	elseif (!empty($attr_textvar))
-		$tmp_text = $langF($$attr_textvar);
-	elseif (!empty($attr_key))
-		$tmp_text = $langF($attr_key);
-	elseif (!empty($attr_var))
-		$tmp_text = isset($$attr_var)?$$attr_var:'?unset:'.$attr_var.'?';	
-	elseif (!empty($attr_raw))
-		$tmp_text = str_replace('_','&nbsp;',$attr_raw);
-	elseif (!empty($attr_value))
-		$tmp_text = $attr_value;
-	else
-	  $tmp_text = '&nbsp;';
+	#END-IF
 	
-//	if	( $attr_escape && empty($attr_raw) && $tmp_text!='&nbsp;' )
-//	{
-//		$tmp_text = htmlentities($tmp_text,ENT_QUOTES,lang('CHARSET') );
-//	}
+	#IF-ATTR text#
+		$tmp_text = $langF($attr_text);
+	#END-IF
 		
-	if	( !empty($attr_maxlength) && intval($attr_maxlength)!=0  )
-		$tmp_text = Text::maxLength( $tmp_text,intval($attr_maxlength) );
+	#IF-ATTR textvar#
+		$tmp_text = $langF($$attr_textvar);
+	#END-IF
 
-	if	(isset($attr_accesskey))
-	{
+	#IF-ATTR key#
+		$tmp_text = $langF($attr_key);
+	#END-IF
+		
+	#IF-ATTR var#
+		$tmp_text = isset($$attr_var)?$$attr_var:'?unset:'.$attr_var.'?';
+	#END-IF
+		
+	#IF-ATTR raw#
+		$tmp_text = str_replace('_','&nbsp;',$attr_raw);
+	#END-IF
+		
+	#IF-ATTR value#
+		$tmp_text = $attr_value;
+	#END-IF
+		
+	#IF-ATTR maxlength#
+		$tmp_text = Text::maxLength( $tmp_text,intval($attr_maxlength) );
+	#END-IF
+		
+	#IF-ATTR accesskey#
 		$pos = strpos(strtolower($tmp_text),strtolower($attr_accesskey));
 		if	( $pos !== false )
 			$tmp_text = substr($tmp_text,0,max($pos,0)).'<span class="accesskey">'.substr($tmp_text,$pos,1).'</span>'.substr($tmp_text,$pos+1);
-	}
-			
+	#END-IF
+	
 	echo $tmp_text;
 	
 	unset($tmp_text);
