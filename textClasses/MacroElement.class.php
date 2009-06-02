01130bc0aed0d0756eedee1f5341d6b39f0ecade
dankert <devnull@localhost>
2009-06-02 22:59:26 +0200

    Makro-DOM-Element

---
+++ textClasses/MacroElement.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Darstellung eines Makros.
+ * 
+ * Ein Makro hat einen Namen sowie n Attribute.
+ * 
+ * @author Jan Dankert
+ * @package openrat.text
+ */
+class MacroElement extends AbstractElement
+{
+	var $name;
+	var $attributes = array();
+}
+
+?>
