6117066c4dff537274081d4d94ceea13f10f07f7
dankert <devnull@localhost>
2009-10-18 23:36:30 +0200

    Neue Methode \"now()\", die die Systemzeit abhÃ¤ngig von der Konfiguration ermittelt.

--- functions/common.inc.php
+++ functions/common.inc.php
@@ -20,4 +20,22 @@ function convertToXmlAttribute( $value )
 	return utf8_encode( htmlspecialchars( $value ) ) ;
 }
 
+
+/**
+ * Ermittelt die aktuelle Systemzeit als Unix-Timestamp.<br>
+ * Es wird konfigurationsgesteuert entweder die lokale Zeit oder
+ * die GMT-Zeit (UTC) zurückgegeben.<br>
+ * Konfigurationsschalter ist 'date/database/utc'.
+ * - 
+ * @return Unix-Timestamp der aktuellen Zeit
+ */
+function now()
+{
+	global $conf;
+	
+	if	( @$conf['date']['database']['utc'] )
+		return gmdate('U');
+	else
+		return time();
+}
 ?>
