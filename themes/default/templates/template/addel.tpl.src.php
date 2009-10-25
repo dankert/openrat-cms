c88f16de7703f5de28d24a774ae11b43c346ed1b
dankert <devnull@localhost>
2009-10-25 11:22:24 +0100

    Beim Anlegen von Elementen das Element sofort zum Quellcode hinzufügen.

--- themes/default/templates/template/addel.tpl.src.php
+++ themes/default/templates/template/addel.tpl.src.php
@@ -13,7 +13,17 @@ page
 					text text:element_type
 				cell
 					set var:text value:text
-					selectbox list:types default:text name:type
+					selectbox list:types default:text name:type lang:true
+					
+			row
+				cell colspan:2
+					fieldset title:message:options
+			row
+				cell
+				cell
+					label for:addtotemplate
+						checkbox name:addtotemplate default:true
+						text key:menu_template_srcelement
 	
 			row
 				cell class:act colspan:2
