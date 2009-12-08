17dcb62ac0b2665efd1bf7fc44dba40609379a29
dankert <devnull@localhost>
2009-12-08 09:49:01 +0100

    Namen von Objekten in der LÃ¤nge begrenzen.

--- themes/default/templates/project/listing.tpl.src.php
+++ themes/default/templates/project/listing.tpl.src.php
@@ -5,7 +5,7 @@ page
 				cell
 					link url:var:url target:cms_main
 						image file:icon_project
-						text value:var:name
+						text value:var:name maxlength:30
 				cell
 					link url:var:use_url target:config:interface/frames/top
 						text key:GLOBAL_SELECT
