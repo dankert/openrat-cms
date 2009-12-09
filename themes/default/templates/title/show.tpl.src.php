83ef43a788f8ac99277a1a0e735d45f83707ff91
dankert <devnull@localhost>
2009-12-09 09:25:20 +0100

    Namen von Objekten in der LÃ¤nge begrenzen.

--- themes/default/templates/title/show.tpl.src.php
+++ themes/default/templates/title/show.tpl.src.php
@@ -5,7 +5,7 @@ page class:title
 			// Datenbank anzeigen
 			cell class:title width:30%
 				image icon:database align:left
-				text title:message:database var:dbname
+				text title:message:database var:dbname maxlength:25
 				text raw:_-_
 				text var:cms_title
 					
@@ -13,12 +13,12 @@ page class:title
 			// Titel anzeigen				
 			cell class:title width:40% style::text-align:center;
 				if present:projectname
-					text title:message:project var:projectname
+					text title:message:project var:projectname maxlength:20
 				if present:modelname
 					text raw:_(
-					text title:message:model var:modelname
+					text title:message:model var:modelname maxlength:20
 					text raw:,
-					text title:message:language var:languagename
+					text title:message:language var:languagename maxlength:20
 					text raw:)
 
 			// Benutzer-Funktionen
@@ -26,7 +26,7 @@ page class:title
 
 				link title:message:USER_PROFILE_DESC url:var:profile_url target:cms_main_main
 					image icon:user align:right
-					text var:userfullname
+					text var:userfullname maxlength:20
 					
 			cell class:title width:10% style::text-align:right;
 
