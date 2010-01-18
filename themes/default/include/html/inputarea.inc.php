13d146a6676d6331bd55d682972efb6bf513e243
dankert <devnull@localhost>
2010-01-18 22:31:15 +0100

    Attribut 'readonly' gibt es nicht.

--- themes/default/include/html/inputarea.inc.php
+++ themes/default/include/html/inputarea.inc.php
@@ -1,5 +1,4 @@
-<?php if ($this->isEditable() && !$this->isEditMode()) $attr_readonly=true;
-      if ( !$attr_readonly) {
-?><textarea <?php if ($attr_readonly) echo ' disabled="true"' ?> class="<?php echo $attr_class ?>" name="<?php echo $attr_name ?>" rows="<?php echo $attr_rows ?>" cols="<?php echo $attr_cols ?>"><?php echo htmlentities(isset($$attr_name)?$$attr_name:$attr_default) ?></textarea><?php
+<?php if ($this->isEditMode()) {
+?><textarea class="<?php echo $attr_class ?>" name="<?php echo $attr_name ?>" rows="<?php echo $attr_rows ?>" cols="<?php echo $attr_cols ?>"><?php echo htmlentities(isset($$attr_name)?$$attr_name:$attr_default) ?></textarea><?php
  } else {
 ?><span class="<?php echo $attr_class ?>"><?php echo isset($$attr_name)?$$attr_name:$attr_default ?></span><?php } ?>
