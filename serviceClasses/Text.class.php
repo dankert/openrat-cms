31a81bf70ffa842fcf6bb08b8912c3bf8797cdb8
dankert <devnull@localhost>
2010-03-27 00:46:38 +0100

    In encodeHtml auch Anführungszeichen ('\"') maskieren.

--- serviceClasses/Text.class.php
+++ serviceClasses/Text.class.php
@@ -142,6 +142,7 @@ class Text
 	function encodeHtml( $inhalt )
 	{
 		$inhalt = str_replace('&','&amp;',$inhalt);
+		$inhalt = str_replace('"','&quot;',$inhalt);
 		$inhalt = str_replace('<','&lt;' ,$inhalt);
 		$inhalt = str_replace('>','&gt;' ,$inhalt);
 
