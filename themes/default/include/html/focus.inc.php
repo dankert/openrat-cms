8d37735147b8c51cc77ecd09a97bceb36e02b22a
dankert <devnull@localhost>
2011-02-15 23:30:43 +0100

    Fix: Javascript-Fehler vermeiden.

--- themes/default/include/html/focus.inc.php
+++ themes/default/include/html/focus.inc.php
@@ -3,6 +3,4 @@
 if (isset($errors[0])) $attr_field = $errors[0];
 ?><script name="JavaScript" type="text/javascript"><!--
 document.forms[0].<?php echo $attr_field ?>.focus();
-document.forms[0].<?php echo $attr_field ?>.select();
-    // -->
-</script>
+document.forms[0].<?php echo $attr_field ?>.select(); // --></script>
