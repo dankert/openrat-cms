73df3380ebf25e45cc2d52e584c7bca7e10e93f5
dankert <devnull@localhost>
2011-02-15 21:47:41 +0100

    Javascript-Konstanten ergänzt, Ausgabe konstant in UTF-8.

--- themes/default/pages/view.php
+++ themes/default/pages/view.php
@@ -1,11 +1,11 @@
 <?php
  if (!defined('OR_VERSION')) die('Forbidden');
- if (!headers_sent()) header('Content-Type: text/html; charset='.$charset)
+ if (!headers_sent()) header('Content-Type: text/html; charset=UTF-8')
 ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
   <title><?php echo OR_TITLE.' '.OR_VERSION ?></title>
-  <meta http-equiv="content-type" content="text/html; charset=<?php echo $charset ?>" >
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
 <?php if ( isset($refresh_url) ) { ?>
   <meta http-equiv="refresh" content="<?php echo isset($refresh_timeout)?$refresh_timeout:0 ?>; URL=<?php echo $refresh_url; if (ini_get('session.use_trans_sid')) echo '&'.session_name().'='.session_id(); ?>">
 <?php } ?>
@@ -25,7 +25,10 @@
   <link rel="stylesheet" type="text/css" href="<?php echo OR_THEMES_EXT_DIR ?>default/css/layout.css" >
   <link rel="stylesheet" type="text/css" href="<?php echo OR_THEMES_EXT_DIR ?>default/css/user/default.css" >
   <script src="/~dankert/cms-test/cms09/themes/default/js/jquery-1.5.min.js"></script>
+  <!-- 
   <script src="/~dankert/cms-test/cms09/themes/default/js/jquery-ui/js/jquery-ui-1.8.9.custom.min.js"></script>
+  <script src="/~dankert/cms-test/cms09/themes/default/js/xxxxxxxxxxxjquery-plugin-orSearchBox.js"></script>
+   -->
   <link rel="stylesheet" type="text/css" href="<?php echo OR_THEMES_EXT_DIR ?>default/js/jquery-ui/css/pepper-grinder/jquery-ui-1.8.9.custom.css" >
   <script src="/~dankert/cms-test/cms09/themes/default/js/openrat.js"></script>
 </head>
@@ -49,7 +52,29 @@ $ping_timeout = @$viewCache['header']['ping_timeout'];
   </script>
 <?php } ?>
 
+
 <body>
+
+<script type="text/javascript">
+<!--
+// Konstanten
+var OR_THEMES_EXT_DIR = '<?php echo OR_THEMES_EXT_DIR ?>';
+var OR_CONTROLLER_FILE  = '<?php echo  OR_CONTROLLER_FILE ?>';
+var REQ_PARAM_TOKEN  = '<?php echo  REQ_PARAM_TOKEN ?>';
+var REQ_PARAM_ACTION  = '<?php echo  REQ_PARAM_ACTION ?>';
+var REQ_PARAM_SUBACTION  = '<?php echo  REQ_PARAM_SUBACTION ?>';
+var REQ_PARAM_TARGETSUBACTION  = '<?php echo  REQ_PARAM_TARGETSUBACTION ?>';
+var REQ_PARAM_ID  = '<?php echo  REQ_PARAM_ID ?>';
+var REQ_PARAM_OBJECT_ID  = '<?php echo  REQ_PARAM_OBJECT_ID ?>';
+var REQ_PARAM_LANGUAGE_ID  = '<?php echo  REQ_PARAM_LANGUAGE_ID ?>';
+var REQ_PARAM_MODEL_ID  = '<?php echo  REQ_PARAM_MODEL_ID ?>';
+var REQ_PARAM_PROJECT_ID  = '<?php echo  REQ_PARAM_PROJECT_ID ?>';
+var REQ_PARAM_ELEMENT_ID  = '<?php echo  REQ_PARAM_ELEMENT_ID ?>';
+var REQ_PARAM_TEMPLATE_ID  = '<?php echo  REQ_PARAM_TEMPLATE_ID ?>';
+var REQ_PARAM_DATABASE_ID  = '<?php echo  REQ_PARAM_DATABASE_ID ?>';
+var REQ_PARAM_TARGET  = '<?php echo  REQ_PARAM_TARGET ?>';
+// -->
+</script>
 
 <?php global $viewCache; /* Debug-Information */ if (@$showDuration||true) { echo "<!-- Output Variables are:\n";echo str_replace('-->','-- >',print_r($viewCache,true));echo "\n-->";} ?>
 
