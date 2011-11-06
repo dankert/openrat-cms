6e6edfc07d75f4f9fe67347eddee757812dd3aa4
Jan Dankert <devnull@localhost>
2011-11-06 22:28:13 +0100

    Perspektive \"Administration\": Views korrigiert und Actions angepasst.

--- themes/default/templates/group/memberships.tpl.src.php
+++ themes/default/templates/group/memberships.tpl.src.php
@@ -1,6 +1,10 @@
-dummy
-	form
-		window icon:user name:GROUP_MEMBERSHIPS
+form
+	window icon:user name:GROUP_MEMBERSHIPS
+		table
+			row class:headline
+				cell width:10%
+				cell
+					text key:name
 			list list:memberships extract:true
 				row class:data
 					cell
@@ -9,6 +13,6 @@ dummy
 						label for:var:var
 							image file:icon_user
 							text var:name
-			row
-				cell colspan:2 class:act
-					button type:ok 
+		row
+			cell colspan:2 class:act
+				button type:ok 
