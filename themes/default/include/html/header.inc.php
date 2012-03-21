087c866c66bae61e051233756c59d6fb25629926
Jan Dankert <devnull@localhost>
2012-03-21 01:07:09 +0100

    Header-Menü mit neuen CSS-Klassen.

--- themes/default/include/html/header.inc.php
+++ themes/default/include/html/header.inc.php
@@ -1,14 +1,16 @@
 <div class="header">
   <?php if ($attr_back) { ?>
-  <a href="javascript:void(0);" onclick="javascript:refreshActualView(this);">
+  <a href="javascript:void(0);" onclick="javascript:refreshActualView(this);" class="back button">
     <img src="<?php  echo $image_dir ?>icon/window/back.gif" />
     <?php echo lang('BACK') ?>
   </a>
   <?php } ?><?php if(!empty($attr_views)) { ?>
   <img src="<?php  echo $image_dir ?>icon/window/down.gif" />
-  <div class="dropdown">
+  <div class="headermenu">
     <?php foreach( explode(',',$attr_views) as $attr_tmp_view ) { ?>
-	<a class="entry" href="javascript:void(0);" onclick="javascript:startView(this,'<?php echo $attr_tmp_view ?>');"><?php echo lang('MENU_'.$attr_tmp_view) ?></a>
+	<a class="entry" href="javascript:void(0);" onclick="javascript:startView(this,'<?php echo $attr_tmp_view ?>');">
+	  <img src="<?php  echo $image_dir ?>icon/<?php echo $attr_tmp_view ?>.png" /><?php echo lang('MENU_'.$attr_tmp_view) ?>
+	</a>
     <?php } ?>
   </div>
 <?php } ?>
