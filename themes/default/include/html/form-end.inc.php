c9fda9413037fa332555b3b46ba5cb7d7a1432e8
Jan Dankert <devnull@localhost>
2012-07-21 01:37:54 +0200

    Am Formularende automatisch Submit-Buttons erzeugen.

--- themes/default/include/html/form-end.inc.php
+++ themes/default/include/html/form-end.inc.php
@@ -1,2 +1,9 @@
-</form>
 
+<div class="bottom">
+	<div class="command">
+		<input type="button" class="submit" value="<?php echo lang('BUTTON_OK') ?>" onclick="$(this).closest('div.window').find('form').submit();" />
+		<input type="button" class="submit" value="<?php echo lang('CANCEL') ?>" onclick="$('ul#history li.active').click();" />
+	</div>
+</div>
+
+</form>
