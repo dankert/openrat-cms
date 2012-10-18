0f0a1023fe980143faaa52d9e7b411a7313374f4
Jan Dankert <devnull@localhost>
2012-10-19 00:13:54 +0200

    Das menschenlesbare Datumformat ("vor xxx Tagen") immer mit anzeigen.

--- themes/default/include/html/date.inc.php
+++ themes/default/include/html/date.inc.php
@@ -2,17 +2,17 @@
     global $conf;
 	$time = $attr_date;
 
-	// Benutzereinstellung 'Zeitzonen-Offset' auswerten.
-	if	( isset($_COOKIE['or_timezone_offset']) )
-	{
-		$time -= (int)date('Z');
-		$time += ((int)$_COOKIE['or_timezone_offset']*60);
-	}
-	
 	if	( $time==0)
 		echo lang('GLOBAL_UNKNOWN');
-	elseif ( !$conf['interface']['human_date_format'] )
+	else
 	{
+		// Benutzereinstellung 'Zeitzonen-Offset' auswerten.
+		if	( isset($_COOKIE['or_timezone_offset']) )
+		{
+			$time -= (int)date('Z');
+			$time += ((int)$_COOKIE['or_timezone_offset']*60);
+		}
+	
 		echo '<span title="';
 		$dl = date(lang('DATE_FORMAT_LONG'),$time);
 		$dl = str_replace('{weekday}',lang('DATE_WEEKDAY'.strval(date('w',$time))),$dl);
@@ -20,56 +20,54 @@
 //		$dl = str_replace(' ','&nbsp;',$dl);
 		echo $dl;
 		unset($dl);
-		echo '">';
-		echo date(lang('DATE_FORMAT'),$time);
-		echo '</span>';
-	}
-	else
-	{
-	
+		
+		
 		$sekunden = time()-$time;
 		$minuten = intval($sekunden/60);
 		$stunden = intval($minuten /60);
 		$tage    = intval($stunden /24);
 		$monate  = intval($tage    /30);
 		$jahre   = intval($monate  /12);
-
-		echo '<span title="'.date(lang('DATE_FORMAT'),$time).'"">';
-
-		if	( $time==0)
-			echo lang('GLOBAL_UNKNOWN');
-		elseif ( !$conf['interface']['human_date_format'] )
-			echo date(lang('DATE_FORMAT'),$time);
-		elseif	( $sekunden == 1 )
-			echo $sekunden.' '.lang('GLOBAL_SECOND');
+		
+		echo ' (';
+		
+		
+		if	( $sekunden == 1 )
+		echo $sekunden.' '.lang('GLOBAL_SECOND');
 		elseif	( $sekunden < 60 )
-			echo $sekunden.' '.lang('GLOBAL_SECONDS');
-	
+		echo $sekunden.' '.lang('GLOBAL_SECONDS');
+		
 		elseif	( $minuten == 1 )
-			echo $minuten.' '.lang('GLOBAL_MINUTE');
+		echo $minuten.' '.lang('GLOBAL_MINUTE');
 		elseif	( $minuten < 60 )
-			echo $minuten.' '.lang('GLOBAL_MINUTES');
-	
+		echo $minuten.' '.lang('GLOBAL_MINUTES');
+		
 		elseif	( $stunden == 1 )
-			echo $stunden.' '.lang('GLOBAL_HOUR');
+		echo $stunden.' '.lang('GLOBAL_HOUR');
 		elseif	( $stunden < 60 )
-			echo $stunden.' '.lang('GLOBAL_HOURS');
-	
+		echo $stunden.' '.lang('GLOBAL_HOURS');
+		
 		elseif	( $tage == 1 )
-			echo $tage.' '.lang('GLOBAL_DAY');
+		echo $tage.' '.lang('GLOBAL_DAY');
 		elseif	( $tage < 60 )
-			echo $tage.' '.lang('GLOBAL_DAYS');
-	
+		echo $tage.' '.lang('GLOBAL_DAYS');
+		
 		elseif	( $monate == 1 )
-			echo $monate.' '.lang('GLOBAL_MONTH');
+		echo $monate.' '.lang('GLOBAL_MONTH');
 		elseif	( $monate < 12 )
-			echo $monate.' '.lang('GLOBAL_MONTHS');
-	
+		echo $monate.' '.lang('GLOBAL_MONTHS');
+		
 		elseif	( $jahre == 1 )
-			echo $jahre.' '.lang('GLOBAL_YEAR');
+		echo $jahre.' '.lang('GLOBAL_YEAR');
 		else
 			echo $jahre.' '.lang('GLOBAL_YEARS');
 			
+		echo ')';
+						
+		
+		echo '">';
+		echo date(lang('DATE_FORMAT'),$time);
 		echo '</span>';
 	}
+
 ?>
