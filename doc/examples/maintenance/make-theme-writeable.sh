c440fbffdb53ee654dcdf5286f7dfdf9c6ed0bab
Jan Dankert <devnull@localhost>
2012-10-24 21:27:58 +0200

    Alte Dateien entfernt.

--- doc/examples/maintenance/make-theme-writeable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -d themes/default/pages ]; then
-  echo "not an openrat directory. please execute this script in openrat program directory"
-  exit 4
-fi
-find themes/default/pages |xargs chmod -v a+w
