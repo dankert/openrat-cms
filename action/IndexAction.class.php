504214116d9e07cf31b15206008e3d67f61e464a
Jan Dankert <devnull@localhost>
2012-10-24 19:43:35 +0200

    Auch die index.php durch den Dispatcher und eine IndexAction schicken, da hier (später) die Perspektive initialisiert wird.

---
+++ action/IndexAction.class.php
@@ -0,0 +1,40 @@
+<?php
+// OpenRat Content Management System
+// Copyright (C) 2002-2010 Jan Dankert
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+/**
+ * Action-Klasse fuer die Anzeige der Hauptseite.
+ * 
+ * @author Jan Dankert
+ * @package openrat.actions
+ */
+class IndexAction extends Action
+{
+	/**
+	 * Konstruktor
+	 */
+	function IndexAction()
+	{
+	}
+
+
+	public function showView()
+	{
+		require('themes/default/layout/index.php');
+		exit;
+	}
+}
+?>
--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php require('init.php'); require('themes/default/layout/index.php'); ?>
+<?php 
+
+// Welcome to OpenRat content management system.
+
+$_GET['action']    = 'index';
+$_GET['subaction'] = 'show';
+
+require('dispatcher.php'); 
+
+?>
