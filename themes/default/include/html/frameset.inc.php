8ef06f6bada75b8fecb9d38f039264bc8ca844d3
Jan Dankert <devnull@localhost>
2017-01-03 22:16:36 +0100

    Cleanup: Unnötige Dateien entfernt.

--- themes/default/include/html/frameset.inc.php
+++
@@ -1,8 +0,0 @@
-<frameset
-<!-- #IF-ATTR rows# -->
-<?php echo ' rows="'.$attr_rows.'"' ?>
-<!-- #END-IF# -->
-<!-- #IF-ATTR columns# -->
-<?php echo ' cols="'.$attr_columns.'"' ?>
-<!-- #END-IF# -->
- border="0" frameborder="5" framespacing="0" bordercolor="#000000">
