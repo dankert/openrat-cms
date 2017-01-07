0a56bd3d64ddc1be1edc85c0780836a3b9317523
Jan Dankert <devnull@localhost>
2017-01-07 01:46:57 +0100

    Include auf entfernte Dateien entfernen.

--- db/include.inc.php
+++ db/include.inc.php
@@ -3,14 +3,6 @@
 require_once( OR_DBCLASSES_DIR."Database.class.php" );
 require_once( OR_DBCLASSES_DIR."Statement.class.php" );
 require_once( OR_DBCLASSES_DIR."Sql.class.php" );
-require_once( OR_DBCLASSES_DIR."driver/postgresql.class.php" );
-require_once( OR_DBCLASSES_DIR."driver/mysql.class.php" );
-if (version_compare(PHP_VERSION, '5.0.0', '>'))
-	require_once( OR_DBCLASSES_DIR."driver/mysqli.class.php" );
-if (version_compare(PHP_VERSION, '5.0.0', '>'))
-	require_once( OR_DBCLASSES_DIR."driver/sqlite.class.php" );
-if (version_compare(PHP_VERSION, '5.3.0', '>'))
-	require_once( OR_DBCLASSES_DIR."driver/sqlite3.class.php" );
 if (version_compare(PHP_VERSION, '5.1.0', '>'))
 	require_once( OR_DBCLASSES_DIR."driver/pdo.class.php" );
 
