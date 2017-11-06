fd77d1dac2c870b71ca8331ae397fd1ef0cfabc8
Jan Dankert <devnull@localhost>
2017-11-06 22:33:47 +0100

    Aufgeräumt...

--- themes/default/include/html/header.inc.php
+++ themes/default/include/html/header.inc.php
@@ -3,7 +3,7 @@
     <?php foreach( explode(',',$attr_views) as $attr_tmp_view ) { ?>
   	<div class="toolbar-icon clickable">
     <a href="javascript:void(0);" data-type="dialog" data-name="<?php echo lang('MENU_'.$attr_tmp_view) ?>" data-method="<?php echo $attr_tmp_view ?>">
-		  <img src="<?php  echo $image_dir ?>icon/<?php echo $attr_tmp_view ?>.png" title="<?php echo lang('MENU_'.$attr_tmp_view) ?>" />
+		  <img src="<?php  echo $image_dir ?>icon/<?php echo $attr_tmp_view ?>.png" title="<?php echo lang('MENU_'.$attr_tmp_view.'_DESC') ?>" /> <?php echo lang('MENU_'.$attr_tmp_view) ?>
 		</a>
   </div>
 		<?php } ?>
