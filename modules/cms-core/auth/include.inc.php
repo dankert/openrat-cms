c8253a86ad4c308d4bb57f8d1086ee53fccfcd07
Jan Dankert <devnull@localhost>
2017-12-19 22:14:47 +0100

    Beim Einbinden von Dateien relativen Pfad verwenden.

--- modules/cms-core/auth/include.inc.php
+++ modules/cms-core/auth/include.inc.php
@@ -1,15 +1,15 @@
 <?php
-require_once( OR_AUTHCLASSES_DIR."Auth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."CookieAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."DatabaseAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."GuestAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."HttpAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."IdentAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."InternalAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."LdapAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."LdapUserDNAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."OpenIdAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."RememberAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."SingleSignonAuth.class.".PHP_EXT );
-require_once( OR_AUTHCLASSES_DIR."SSLAuth.class.".PHP_EXT );
+require_once( __DIR__."/Auth.class.".PHP_EXT );
+require_once( __DIR__."/CookieAuth.class.".PHP_EXT );
+require_once( __DIR__."/DatabaseAuth.class.".PHP_EXT );
+require_once( __DIR__."/GuestAuth.class.".PHP_EXT );
+require_once( __DIR__."/HttpAuth.class.".PHP_EXT );
+require_once( __DIR__."/IdentAuth.class.".PHP_EXT );
+require_once( __DIR__."/InternalAuth.class.".PHP_EXT );
+require_once( __DIR__."/LdapAuth.class.".PHP_EXT );
+require_once( __DIR__."/LdapUserDNAuth.class.".PHP_EXT );
+require_once( __DIR__."/OpenIdAuth.class.".PHP_EXT );
+require_once( __DIR__."/RememberAuth.class.".PHP_EXT );
+require_once( __DIR__."/SingleSignonAuth.class.".PHP_EXT );
+require_once( __DIR__."/SSLAuth.class.".PHP_EXT );
 ?>
