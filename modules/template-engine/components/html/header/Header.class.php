19ce73e5cc9e9bafc21c0636577668d3ceb841be
Jan Dankert <devnull@localhost>
2017-12-19 22:16:20 +0100

    Die Headerausgaben mit Links auf Actions benötigen wir doch noch...

--- modules/template-engine/components/html/header/Header.class.php
+++ modules/template-engine/components/html/header/Header.class.php
@@ -13,7 +13,7 @@ class HeaderComponent extends Component
 
             foreach (explode(',', $this->views) as $view) {
                 echo '<div class="toolbar-icon clickable">';
-                echo '<a href="javascript:void(0);" data-type="dialog" data-name="<?php echo lang(\'MENU_' . strtoupper($view ). '\') ?>" data-method="' . $view . '">';
+                echo '<a href="javascript:void(0);" title="<?php echo lang(\'MENU_' . strtoupper($view ). '\') ?>" data-type="dialog" data-name="<?php echo lang(\'MENU_' . strtoupper($view ). '\') ?>" data-method="' . $view . '">';
                 echo '<img src="./themes/default/images/icon/action/' . $view . '.svg" title="<?php echo lang(\'MENU_' . $view . '_DESC\') ?>" /><?php echo lang(\'MENU_' . $view . '\') ?>';
                 echo '</a>';
                 echo '</div>';
