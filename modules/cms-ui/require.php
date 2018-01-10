b73888da4a0dd8930afeccb1a5ced5a10801770f
Jan Dankert <devnull@localhost>
2018-01-10 23:37:06 +0100

    Die Actionklassen, die das UI mit definieren, gehören nicht in den Core, sondern in das UI-Modul.

--- modules/cms-ui/require.php
+++ modules/cms-ui/require.php
@@ -4,3 +4,7 @@ require_once(__DIR__ . "/../cms-core/require.php");
 require_once(__DIR__ . "/../template-engine/require.php");
 
 require_once(__DIR__ . "/UI.class.php");
+
+require_once(__DIR__ . "/action/IndexAction.class.php");
+require_once(__DIR__ . "/action/TitleAction.class.php");
+require_once(__DIR__ . "/action/WorkbenchAction.class.php");
