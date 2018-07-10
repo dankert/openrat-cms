48b44c4c4631bc6112a6c963c09e946a555a63f7
Jan Dankert <devnull@localhost>
2018-07-10 23:02:01 +0200

    Die .clickable-Elemente sollen nur 1x als Link registriert werden.

--- modules/template-engine/components/html/tree/tree.js
+++ modules/template-engine/components/html/tree/tree.js
@@ -2,7 +2,5 @@
 $(document).on('orViewLoaded',function(event, data) {
 
     // Clickable.
-    $(event.target).find('div.entry.clickable').click(function () {
-        openNewAction($(this).data('name'), $(this).data('action'), $(this).data('id'), 0);
-    })
+    // done by link.js
 });
