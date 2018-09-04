0ad70799a8f9791c22242de1beecb651ecbd5408
Jan Dankert <devnull@localhost>
2018-09-04 23:11:09 +0200

    Fix der URL zum Aufruf der Suche.

--- modules/cms-ui/themes/default/script/plugin/jquery-plugin-orSearch.js
+++ modules/cms-ui/themes/default/script/plugin/jquery-plugin-orSearch.js
@@ -15,14 +15,14 @@ jQuery.fn.orSearch = function( options )
 		if	( val.length > 3 )
 		{
 			$(settings.dropdown).html('');
-			$.ajax( { 'type':'GET',url:'./?action=search&subaction=quicksearch&output=json&search='+val, data:null, success:function(data, textStatus, jqXHR)
+			$.ajax( { 'type':'GET',url:'./api/?action=search&subaction=quicksearch&output=json&search='+val, data:null, success:function(data, textStatus, jqXHR)
 				{
 					for( id in data.output.result )
 					{
 						var result = data.output.result[id];
 						
 						// Suchergebnis-Zeile in das Ergebnis schreiben.
-						$(settings.dropdown).append('<div class="entry clickable" title="'+result.desc+'"><a href="javascript:void(0);" data-type="open" data-name="'+result.name+'" data-action="'+result.type+'" data-id="'+id+'"><img src="'+OR_THEMES_EXT_DIR+'default/images/icon_'+result.type+'.png" />'+result.name+'</a></div>');
+						$(settings.dropdown).append('<div class="entry clickable" title="'+result.desc+'"><a href="javascript:void(0);" data-type="open" data-name="'+result.name+'" data-action="'+result.type+'" data-id="'+result.id+'"><img src="'+OR_THEMES_EXT_DIR+'default/images/icon_'+result.type+'.png" />'+result.name+'</a></div>');
 					}
 					$(settings.dropdown).orLinkify();
 				} } );
