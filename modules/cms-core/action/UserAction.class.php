68fef14878dbd03c06c0bd15cbb72751d8a445ce
Jan Dankert <devnull@localhost>
2018-09-18 23:00:14 +0200

    Einträge aus dem Benutzerverlauf sind nun anklickbar.

--- modules/cms-core/action/UserAction.class.php
+++ modules/cms-core/action/UserAction.class.php
@@ -498,8 +498,18 @@ class UserAction extends Action
 	 */
 	public function historyView()
 	{
-		$result = $this->user->getLastChanges();
-		$this->setTemplateVar('timeline', $result);
+        $lastChanges = $this->user->getLastChanges();
+
+        $timeline = array();
+
+        foreach( $lastChanges as $entry )
+        {
+            $timeline[ $entry['objectid'] ] = $entry;
+            $baseObject = new BaseObject( $entry['objectid']);
+            $baseObject->objectLoad();
+            $timeline[ $entry['objectid'] ]['type'] = $baseObject->getType();
+        }
+        $this->setTemplateVar('timeline', $timeline);
 	}
 	
 				
--- modules/cms-ui/themes/default/html/views/user/history.php
+++ modules/cms-ui/themes/default/html/views/user/history.php
@@ -18,15 +18,18 @@
 			<?php foreach($timeline as $list_key=>$list_value){ ?><?php extract($list_value) ?>
 				<tr class="data">
 					<td class="clickable">
-						<a target="_self" data-type="post" data-action="start" data-method="projectmenu" data-id="<?php echo $projectid ?>" data-extra="[]" data-data="{&quot;action&quot;:&quot;start&quot;,&quot;subaction&quot;:&quot;projectmenu&quot;,&quot;id&quot;:&quot;<?php echo $projectid ?>&quot;,&quot;token&quot;:&quot;<?php echo token() ?>&quot;,&quot;none&quot;:&quot;0&quot;}">
+						<a target="_self" data-type="open" data-action="project" data-method="history" data-id="<?php echo $projectid ?>" data-extra="[]" href="<?php echo Html::url('project','',$projectid,array()) ?>">
 							<span class="text"><?php echo nl2br(encodeHtml(htmlentities($projectname))); ?></span>
 							
 						</a>
 
 					</td>
-					<td title="<?php echo $filename ?>">
-						<span class="text"><?php echo nl2br(encodeHtml(htmlentities(Text::maxLength( $filename,30,'..',constant('STR_PAD_BOTH') )))); ?></span>
-						
+					<td title="<?php echo $filename ?>" class="clickable">
+						<a target="_self" data-type="open" data-action="<?php echo $type ?>" data-method="history" data-id="<?php echo $objectid ?>" data-extra="[]" href="<?php echo Html::url($type,'',$objectid,array()) ?>">
+							<span class="text"><?php echo nl2br(encodeHtml(htmlentities(Text::maxLength( $filename,30,'..',constant('STR_PAD_BOTH') )))); ?></span>
+							
+						</a>
+
 					</td>
 					<td>
 						<?php include_once( 'modules/template-engine/components/html/date/component-date.php') ?><?php component_date($lastchange_date) ?>
