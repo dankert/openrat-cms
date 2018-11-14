f5b71e0351b19aa3eacba36355e9e8df517af101
Jan Dankert <devnull@localhost>
2018-11-14 23:31:11 +0100

    Die Sprachdateien werden durch das CMS selbst aktualisiert, so dass hier nichts mehr per Hand aufgerufen werden muss.

--- dev-helper/language-files-writable.sh
+++ dev-helper/language-files-writable.sh
@@ -2,6 +2,6 @@
 
 for iso in de en es fr it ru cn; do
 
-	touch ../language/lang-$iso.php
-	chmod -v a+w ../language/lang-$iso.php
+	touch ../modules/language/lang-$iso.php
+	chmod -v a+w ../modules/language/lang-$iso.php
 done
