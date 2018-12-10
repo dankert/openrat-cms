58debcd2a05a9e30c28028d1e057980107a8752d
Jan Dankert <devnull@localhost>
2018-12-11 00:54:57 +0100

    Fix: Publisher->ok gibt es nicht.

--- modules/cms-core/action/FileAction.class.php
+++ modules/cms-core/action/FileAction.class.php
@@ -538,7 +538,7 @@ class FileAction extends ObjectAction
 		$this->file->publish();
 		$this->file->publish->close();
 		
-		$this->addNotice('file',$this->file->fullFilename,'PUBLISHED'.($this->file->publish->ok?'':'_ERROR'),$this->file->publish->ok,array(),$this->file->publish->log);
+		$this->addNotice('file',$this->file->fullFilename,'PUBLISHED',OR_NOTICE_OK,array(),$this->file->publish->log);
 	}
 
 
