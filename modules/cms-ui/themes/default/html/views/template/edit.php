ec80382983ba56787861a28738282db13cf04a2d
Jan Dankert <develop@jandankert.de>
2019-05-17 22:58:55 +0200

    Fix: Templates: Icons korrekt anzeigen, Quelltext in der Ansicht etwas kürzen.

--- modules/cms-ui/themes/default/html/views/template/edit.php
+++ modules/cms-ui/themes/default/html/views/template/edit.php
@@ -51,14 +51,14 @@
 			</tr>
 		</table>
 		<?php foreach($models as $list_key=>$list_value){ ?><?php extract($list_value) ?>
-			<fieldset class="toggle-open-close<?php echo '1'?" open":" closed" ?><?php echo '1'?" show":"" ?>"><legend class="on-click-open-close"><div class="arrow arrow-right on-closed"></div><div class="arrow arrow-down on-open"></div><?php echo $name ?></legend><div>
+			<fieldset class="toggle-open-close<?php echo '1'?" open":" closed" ?><?php echo '1'?" show":"" ?>"><legend class="on-click-open-close"><div class="arrow arrow-right on-closed"></div><div class="arrow arrow-down on-open"></div><?php echo $name ?></legend><div class="closable">
 				<div class="clickable">
-					<code><?php echo nl2br(encodeHtml(htmlentities($source))); ?></code>
+					<code><?php echo nl2br(encodeHtml(htmlentities(Text::maxLength( $source,200,'..',constant('STR_PAD_BOTH') )))); ?></code>
 					
 					<br/>
 					
 					<a class="or-form-button" target="_self" data-type="edit" data-action="" data-method="src" data-id="<?php echo OR_ID ?>" data-extra="{'modelid':'<?php echo $modelid ?>'}" href="<?php echo Html::url('','src','',array('modelid'=>$modelid)) ?>">
-						<img src="./modules/cms-ui/themes/default/images/icon/template.png" />
+						<i class="image-icon image-icon--action-template"></i>
 						
 						<span><?php echo nl2br(encodeHtml(htmlentities(lang(''.'edit'.'')))); ?></span>
 						
