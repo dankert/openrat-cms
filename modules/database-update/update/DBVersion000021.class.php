96848a806006b75c84dda12c5ec7dbaf9f932027
Jan Dankert <develop@jandankert.de>
2019-08-31 23:01:57 +0200

    Fix: fixed "Specified key was too long; max key length is 767 bytes". 150 Chars is enough for token and selector.

--- modules/database-update/update/DBVersion000021.class.php
+++ modules/database-update/update/DBVersion000021.class.php
@@ -17,9 +17,9 @@ class DBVersion000021 extends DbVersion
     {
         $this->addTable('auth');
 
-        $this->addColumn('auth','selector'     ,OR_DB_COLUMN_TYPE_VARCHAR ,255 ,null,OR_DB_COLUMN_NOT_NULLABLE);
+        $this->addColumn('auth','selector'     ,OR_DB_COLUMN_TYPE_VARCHAR ,150 ,null,OR_DB_COLUMN_NOT_NULLABLE);
         $this->addColumn('auth','userid'       ,OR_DB_COLUMN_TYPE_INT  ,0   ,null,OR_DB_COLUMN_NOT_NULLABLE);
-        $this->addColumn('auth','token'        ,OR_DB_COLUMN_TYPE_VARCHAR ,255 ,null,OR_DB_COLUMN_NOT_NULLABLE);
+        $this->addColumn('auth','token'        ,OR_DB_COLUMN_TYPE_VARCHAR ,150 ,null,OR_DB_COLUMN_NOT_NULLABLE);
         $this->addColumn('auth','token_algo'   ,OR_DB_COLUMN_TYPE_INT  ,0   ,0   ,OR_DB_COLUMN_NOT_NULLABLE);
         $this->addColumn('auth','expires'      ,OR_DB_COLUMN_TYPE_INT  ,0   ,null,OR_DB_COLUMN_NOT_NULLABLE);
         $this->addColumn('auth','create_date'  ,OR_DB_COLUMN_TYPE_INT  ,0   ,null,OR_DB_COLUMN_NOT_NULLABLE);
