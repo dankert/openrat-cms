6ecc6a1611b064ecf14a309f7c8054b0ec7b8ebf
Jan Dankert <develop@jandankert.de>
2020-10-21 23:32:28 +0200

    Fix: Magic quotes was removed a long time ago.

--- modules/cms/base/Startup.class.php
+++ modules/cms/base/Startup.class.php
@@ -128,14 +128,9 @@ class Startup {
 	{
 		// REGISTER_GLOBALS
 		// This feature has been DEPRECATED as of PHP 5.3.0 and REMOVED as of PHP 5.4.0.
-		if (ini_get('register_globals'))
-			Logger::warn("REGISTER_GLOBALS is active. For security reasons: DO NOT USE THIS!");
 
 		// MAGIC_QUOTES
 		// This feature has been DEPRECATED as of PHP 5.3.0 and REMOVED as of PHP 5.4.0.
-		// always returns FALSE as of PHP 5.4.0.
-		if (@get_magic_quotes_gpc() == 1) // function is deprecated since PHP 7.4
-			Logger::warn("MAGIC_QUOTES is active. For security reasons: DO NOT USE THIS!");
 	}
 
 
