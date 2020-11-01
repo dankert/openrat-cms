952657369e00a21b78ca9e66d0b73bb1dce795c6
Jan Dankert <develop@jandankert.de>
2020-11-01 22:31:13 +0100

    Fix: Method 'availableView' was missing.

--- modules/cms/action/BaseAction.class.php
+++ modules/cms/action/BaseAction.class.php
@@ -1,18 +1,32 @@
 <?php
 
 
-namespace cms\action {
+namespace cms\action;
+
+/**
+ */
+class BaseAction extends Action
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
 
-    /**
-     */
-    class BaseAction extends Action
-    {
+	/**
+	 * Gets all available View-Methods in this Action.
+	 */
+	public function availableView()
+	{
+		$viewMethods = array_map( function($methodName){
+			// Removing the 'View' from the end of the method name
+			return substr($methodName,0,strlen($methodName)-4);
+		}, array_filter(get_class_methods($this), function ($methodName) {
+			// Filter only View methods
+			return substr($methodName, -4, 4) == 'View';
+		}));
 
-        public function __construct()
-        {
-            parent::__construct();
-
-        }
-    }
+		$this->setTemplateVar('views', $viewMethods);
+	}
 }
