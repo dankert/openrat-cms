081b6f530f1453852d720137540810b80a27776f
Jan Dankert <develop@jandankert.de>
2021-03-09 08:27:30 +0100

    UI-Fix: Link to pages

--- modules/cms/ui/themes/default/html/views/template/info.php
+++ modules/cms/ui/themes/default/html/views/template/info.php
@@ -43,8 +43,13 @@
         </div>
         <div class="<?php echo O::escapeHtml('or-table-area') ?>"><?php echo O::escapeHtml('') ?>
           <table class="<?php echo O::escapeHtml('or-table') ?>"><?php echo O::escapeHtml('') ?>
+            <tr class="<?php echo O::escapeHtml('or-table-header') ?>"><?php echo O::escapeHtml('') ?>
+              <th><?php echo O::escapeHtml('') ?>
+                <span><?php echo O::escapeHtml(''.@O::lang('name').'') ?></span>
+              </th>
+            </tr>
             <?php foreach((array)@$pages as $pageid=>$name) {  ?>
-              <tr class="<?php echo O::escapeHtml('or-table-data') ?>"><?php echo O::escapeHtml('') ?>
+              <tr class="<?php echo O::escapeHtml('or-data') ?>"><?php echo O::escapeHtml('') ?>
                 <td class="<?php echo O::escapeHtml('or-act-clickable') ?>"><?php echo O::escapeHtml('') ?>
                   <a target="<?php echo O::escapeHtml('_self') ?>" data-type="<?php echo O::escapeHtml('open') ?>" data-action="<?php echo O::escapeHtml('page') ?>" data-method="<?php echo O::escapeHtml('') ?>" data-id="<?php echo O::escapeHtml(''.@$pageid.'') ?>" data-extra="<?php echo O::escapeHtml('[]') ?>" href="<?php echo O::escapeHtml('#/page/'.@$pageid.'') ?>" class="<?php echo O::escapeHtml('or-link') ?>"><?php echo O::escapeHtml('') ?>
                     <i class="<?php echo O::escapeHtml('or-image-icon or-image-icon--action-page') ?>"><?php echo O::escapeHtml('') ?></i>
