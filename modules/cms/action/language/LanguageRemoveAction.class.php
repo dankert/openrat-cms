fd730fb751daa9f949b76741ce8991b3457f2c93
Jan Dankert <dev@jandankert.de>
2023-02-14 21:48:16 +0100

    Fix: Be sure that there are at least 1 language and 1 model.

--- modules/cms/action/language/LanguageRemoveAction.class.php
+++ modules/cms/action/language/LanguageRemoveAction.class.php
@@ -4,17 +4,28 @@ use cms\action\LanguageAction;
 use cms\action\Method;
 use cms\model\Project;
 use language\Messages;
+use util\exception\ClientException;
+use util\exception\UIException;
+use util\exception\ValidationException;
 
 
 class LanguageRemoveAction extends LanguageAction implements Method {
 
     public function view() {
 
+
+		$project = Project::create( $this->language->projectid );
+
+		// There must be at least 1 language
+		if   ( count( $project->getLanguageIds() ) <= 1 )
+			throw new ClientException("there must be at least 1 language.");
+
 		$this->setTemplateVar('name'   ,$this->language->name   );
-    }
+	}
 
 
     public function post() {
+
 		if   ( $this->request->getRequiredNumber('confirm') ) {
 
 			$project = Project::create( $this->language->projectid );
@@ -25,6 +36,9 @@ class LanguageRemoveAction extends LanguageAction implements Method {
 				$this->language->delete();
 
 				$this->addNoticeFor( $this->language, Messages::DELETED );
+			}
+			else {
+				$this->addErrorFor( $this->language, Messages::NOTHING_DONE );
 			}
 		}
     }
--- modules/cms/action/model/ModelRemoveAction.class.php
+++ modules/cms/action/model/ModelRemoveAction.class.php
@@ -2,20 +2,39 @@
 namespace cms\action\model;
 use cms\action\Method;
 use cms\action\ModelAction;
+use cms\model\Project;
 use language\Messages;
+use util\exception\ClientException;
 
 
 class ModelRemoveAction extends ModelAction implements Method {
     public function view() {
 		$this->model->load();
 
+		$project = Project::create( $this->model->projectid );
+
+		// There must be at least 1 model
+		if   ( count( $project->getModelIds() ) <= 1 )
+			throw new ClientException('There must be at least 1 model.');
+
 		$this->setTemplateVar( 'name',$this->model->name );
     }
+
+
+
     public function post() {
+
 		if   ( $this->request->isTrue('confirm') )
 		{
-			$this->model->delete();
-			$this->addNoticeFor( $this->model, Messages::DONE );
+			$project = Project::create( $this->model->projectid );
+
+			// There must be at least 1 model
+			if   ( count( $project->getModelIds() ) > 1 ) {
+				$this->model->delete();
+				$this->addNoticeFor($this->model, Messages::DONE);
+			}else {
+				$this->addWarningFor( $this->model, Messages::NOTHING_DONE);
+			}
 		}
 		else
 		{
