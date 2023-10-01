973fb1efe5a17072ee942ead018d1a197fa006d1
Jan Dankert <develop@jandankert.de>
2023-10-01 02:59:13 +0200

    New: Fixing the year 2038 problem with 8-byte-integers for the unix timestamps.

---
+++ modules/cms/update/version/DBVersion000033.class.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace cms\update\version;
+
+use cms\base\Startup;
+use database\DbVersion;
+use database\Column;
+use security\Password;
+
+/**
+ * Migrate file values to content.
+ *
+ * @author dankert
+ *
+ */
+class DBVersion000033 extends DbVersion
+{
+    /**
+     * Modify date columns to bigint to fix the year 2038 problem.
+     */
+    public function update()
+    {
+    	foreach( [
+			$this->table('object' )->column('create_date'    ),
+		    $this->table('object' )->column('lastchange_date'),
+		    $this->table('object' )->column('published_date')->nullable(),
+		    $this->table('object' )->column('valid_from')->nullable(),
+		    $this->table('object' )->column('valid_to'  )->nullable(),
+    		$this->table('value'  )->column('date')->nullable(),
+    		$this->table('value'  )->column('lastchange_date'),
+    		$this->table('version')->column('installed'),
+    		$this->table('user'   )->column('password_expires')->nullable(),
+    		$this->table('user'   )->column('last_login')->nullable(),
+    		$this->table('user'   )->column('password_locked_until')->nullable(),
+    		$this->table('auth'   )->column('expires'),
+    		$this->table('auth'   )->column('create_date'),
+		] as $column ) {
+			$column->type( Column::TYPE_INT )->size( Column::SIZE_INT_BIG )->modify();
+		}
+	}
+}
+
